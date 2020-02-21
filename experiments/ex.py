e3a41d68957fac8c2f1a15e7943e0d44a3b0c8ab
Edward Grigoryan <edogrigqv2@gmail.com>
2020-02-21 16:17:56 +0000

    gitignore updated

--- experiments/ex.py
+++ experiments/ex.py
@@ -7,94 +7,18 @@ import io
 
 # tf.enable_eager_execution()
 
-from waymo_open_dataset.utils import range_image_utils
-from waymo_open_dataset.utils import transform_utils
-from waymo_open_dataset.utils import  frame_utils
-from waymo_open_dataset import dataset_pb2 as open_dataset
+# from waymo_open_dataset.utils import range_image_utils
+# from waymo_open_dataset.utils import transform_utils
+# from waymo_open_dataset.utils import  frame_utils
+# from waymo_open_dataset import dataset_pb2 as open_dataset
 import hub
 from PIL import Image
 
-# filenames = os.listdir('/home/edward/waymo/validation')[0:2]
-# filenames = ['/home/edward/waymo/validation/' + filename for filename in filenames]
-# print(filenames)
-# frame_count = 0
-
-# file_counter = 0
-# for filename in filenames:
-#     dataset = tf.data.TFRecordDataset(filename)
-#     # print('Frame: {}, File: {}'.format(frame_count, file_counter))
-#     file_counter += 1
-#     for data in dataset:
-#         frame_count += 1
-#         # frame = open_dataset.Frame()
-#         # frame.ParseFromString(bytearray(data.numpy()))
-
-# print('Total frames: {}'.format(frame_count))
-# print('Total files: {}'.format(file_counter))
-
-# arr = hub.array((frame_count, 6 * 1000 * 1000), name='edward/waymo-dataset:v2', backend='s3', dtype='uint8', chunk_size=(6, 6 * 1000 * 1000))
-
-# counter = 0
-# for filename in filenames:
-#     dataset = tf.data.TFRecordDataset(filename)
-#     for batch in dataset.batch(6):
-#         x = np.zeros(shape=(batch.shape[0], 6 * 1000 * 1000), dtype='uint8')
-#         print(batch.shape[0])
-#         for i in range(0, batch.shape[0]):
-#             data = bytearray(batch[i].numpy())
-#             l = len(data)
-#             x[i][4:4 + l] = data
-#             x[i][0] = l // 256 // 256 // 256
-#             x[i][1] = (l // 256 // 256) % 256
-#             x[i][2] = (l // 256) % 256
-#             x[i][3] = l % 256
-#         arr[counter:counter + batch.shape[0]] = x
-#         counter += batch.shape[0]
-
-# starting reading data to check integrity
-
-# arr2 = hub.load(name='edward/waymo-dataset:v2', backend='s3')
-
-# data = arr2[108]
-
-# l = data[0] * 256 * 256 * 256 + data[1] * 256 * 256 + data[2] * 256 + data[3]
-# x = data[4:4 + l]
-
-# frame = open_dataset.Frame()
-# frame.ParseFromString(bytearray(x))
-# image = frame.images[0]
-# img = Image.open(io.BytesIO(bytearray(image.image)))
-# print(np.array(img).shape)
-
-client = hub.s3('waymo-dataset-upload', aws_creds_filepath='.creds/aws.json').connect() 
-arr = client.array_open('v028/validation/images')
-
-# arr = hub.load(name='edward/validation-camera-images:v2', backend='s3', storage=S3(bucket='waymo-dataset-upload'))
+client = hub.gs('snark_waymo_open_dataset', creds_path='.creds/gs.json').connect() 
+arr = client.array_open('v029/validation/images')
 
 for i in range(0, 5):
     img = arr[10, i]
     print(img.shape)
     Image.fromarray(img, 'RGB').save(f'output/image-{i}.jpg')
 
-# arr = hub.array(shape=(100, 100, 100000), name='test6', chunk_size=(1, 1, 100000), storage=S3(bucket='waymo-dataset-upload'), compression='gzip')
-# arr[5,5] = np.ones(shape=(100000))
-# arr[6,6] = np.ones(shape=(100000))
-# print(arr[5, 5, 3])
-
-# bucket = hub.S3(bucket='waymo-dataset-upload')
-# arr = bucket.arraynew(shape=(5, 1920, 1080, 3), name='test10', dtype='uint8', chunk=(1, 1920, 1080, 3), compress='jpeg')
-# arr[2] = np.ones((1920, 1080, 3))
-# arr[3] = np.ones((1920, 1080, 3))
-# print(arr[3, 120, 120, 2])
-
-# arr = np.array([[1, 2], [3, 4]])
-# print(type(arr))
-# print(arr[1])
-# print(type(arr[1]))
-
-# bucket = hub.gs('waymo-dataset-upload', '~/google-creds.json').fs('~/bucket').connect()
-# arr = bucket.array_create('sample/sample/v2', shape=(100, 1920, 1080, 3), chunk=(1, 1920, 1080, 3), dtype='uint8', compress='jpeg', overwrite=True)
-# arr2 = bucket.array_open('sample/sample/v2')
-# arr[5] = 5 * np.ones((1920, 1080, 3), 'uint8')
-# #print(arr2[5, 1, 4, 2])
-# bucket.array_delete('sample/sample/v2')
