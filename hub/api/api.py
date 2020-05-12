48c6c1184dafb2c403329dcb768ab37dea2ce4fa
david.buniatyan@gmail.com <david.buniatyan@gmail.com>
2020-05-12 15:53:00 -0700

    s3 credentials update

--- hub/api/api.py
+++ hub/api/api.py
@@ -13,27 +13,39 @@ from hub.utils.store_control import StoreControlClient
 from hub.exceptions import FileSystemException, S3Exception
 from hub.config import CACHE_FILE_PATH
 
-def amazon_s3(bucket: Optional[str] = None, aws_creds_filepath: Optional[str] = None, aws_access_key_id: Optional[str] = None, aws_secret_access_key: Optional[str] = None) -> HubBackend:
+
+def amazon_s3(
+    bucket: Optional[str] = None,
+    aws_creds_filepath: Optional[str] = None,
+    aws_access_key_id: Optional[str] = None,
+    aws_secret_access_key: Optional[str] = None,
+) -> HubBackend:
     if aws_access_key_id is not None or aws_secret_access_key is not None:
         assert aws_access_key_id is not None
         assert aws_secret_access_key is not None
         assert bucket is not None
         assert aws_creds_filepath is None
-        return HubBackendImpl(AmazonS3(bucket, aws_access_key_id, aws_secret_access_key))
+        return HubBackendImpl(
+            AmazonS3(bucket, aws_access_key_id, aws_secret_access_key)
+        )
     elif aws_creds_filepath is not None:
         raise NotImplementedError()
-    else: 
+    else:
         config = StoreControlClient.get_config()
         if bucket is None:
-            bucket = config['BUCKET']
-        
-        aws_access_key_id = config['AWS_ACCESS_KEY_ID']
-        aws_secret_access_key = config['AWS_SECRET_ACCESS_KEY']
+            bucket = config["BUCKET"]
 
-        return HubBackendImpl(AmazonS3(bucket, aws_access_key_id, aws_secret_access_key))
+        aws_access_key_id = config["AWS_ACCESS_KEY_ID"]
+        aws_secret_access_key = config["AWS_SECRET_ACCESS_KEY"]
+
+        return HubBackendImpl(
+            AmazonS3(bucket, aws_access_key_id, aws_secret_access_key)
+        )
+
 
 # def GoogleGS(bucket: str, ...)
 # Hello World
+
 
 def filesystem(dir: str) -> HubBackend:
     dir = os.path.expanduser(dir)
@@ -41,12 +53,13 @@ def filesystem(dir: str) -> HubBackend:
 
     return HubBackendImpl(FileSystemStorage(dir))
 
+
 def bucket(backends: Union[HubBackend, Tuple[HubBackend, ...]]) -> HubBucket:
     if isinstance(backends, HubBackend):
         backends = [backends]
 
     backends: Tuple[HubBackendImpl, ...] = backends
-    
+
     storage: Storage = None
 
     for backend in backends:
@@ -54,8 +67,9 @@ def bucket(backends: Union[HubBackend, Tuple[HubBackend, ...]]) -> HubBucket:
             storage = backend.storage
         else:
             storage = RecursiveStorage(backend.storage, storage)
-    
+
     return HubBucketImpl(storage)
 
-# bucket = hub.AmazonS3() 
+
+# bucket = hub.AmazonS3()
 # bucket.array_create()
