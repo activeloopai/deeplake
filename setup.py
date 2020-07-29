80e70b1f29226b9dc02c5494698d384ad369bc6b
Davit Buniatyan <david.buniatyan@gmail.com>
2020-07-28 21:20:56 -0700

    publishable version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages, setup
 
 project = "hub"
-version = "0.9.0.2"
+version = "0.9.0.3"
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md")) as f:
