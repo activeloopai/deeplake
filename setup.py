da5dd18adc8a3d66ede6e32b099eb6a9b28cb495
Edward Grigoryan <edogrigqv2@gmail.com>
2020-09-20 14:53:50 +0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 
 project = "hub"
-VERSION = "0.12.6"
+VERSION = "0.12.7"
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md")) as f:
