8390b009ecc441d614e9c43f8794b858b389c143
Kristina <kristina@activeloop.ai>
2020-09-24 07:40:58 -0700

    Add BBox class

--- hub/features/bbox.py
+++ hub/features/bbox.py
@@ -1,9 +1,22 @@
 from typing import Tuple
 
-import hub.features.feature_connector as feature_connector
-
-FeatureConnector = feature_connector.FeatureConnector
+from hub.features.features import Tensor
 
 
-class BBox(FeatureConnector):
-    pass
+class BBox(Tensor):
+    def __init__(self, xmin: float, ymin: float, 
+                 xmax: float, ymax: float):
+        super(BBox, self).__init__(shape=(4,), dtype='float32')
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        self.validate()
+
+    def validate(self):
+        assert 0 < self.xmin < self.xmax 
+        assert 0 < self.ymin < self.ymax
+
+if __name__ == "__main__":
+    box = BBox(0.1, 0.3, 0.5, 0.7)
+    print(box.dtype)
