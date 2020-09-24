95b0b3f5369a5760e3b05c09fb5dac615b54f036
Kristina <kristina@activeloop.ai>
2020-09-24 07:40:34 -0700

    Add Image class

--- hub/features/image.py
+++ hub/features/image.py
@@ -1,17 +1,30 @@
 from typing import Tuple
 
-import hub.features.tensor as tensor
+from hub.features.features import Tensor
 
 
-class Image(tensor.Tensor):
-    def __init__(
-        shape: Tuple[int, ...] = None,
-        dtype=None,
-        encoding_format: str = None,
-        channels=None,
-    ):
-        super().__init__(shape, dtype)
+class Image(Tensor):
+    def __init__(self,
+                 shape: Tuple[int, ...] = None,
+                 dtype = None,
+                 encoding_format: str = None,
+                 channels = None,
+    ):        
+        self._set_dtype(dtype)
+        super(Image, self).__init__(shape, dtype)
+        self._set_channels(channels)
 
     @property
     def encoding_format(self):
         raise NotImplementedError()
+
+    def _set_dtype(self, dtype):
+        if dtype not in ('uint8', 'uint16'):
+            raise ValueError(f'Not acceptable dtype for {self.__class__.__name__}')
+        self.dtype = dtype
+
+    def _set_channels(self, channels):        
+        if channels and len(channels) != self.shape[-1]:
+            raise ValueError(f'Channels are incompatible with image shape {self.shape}')
+        self.channels = channels
+        
