b4c5f5a50ef2f897d50c6ffbbe32d844c7142612
AbhinavTuli <abhinav.tuli99@gmail.com>
2020-09-25 11:55:47 +0530

    fixed test

--- hub/features/tests/test_serilize_deserialize.py
+++ hub/features/tests/test_serilize_deserialize.py
@@ -2,7 +2,25 @@ from hub.features.features import Primitive,Tensor,FeatureDict
 from hub.features.serialize import serialize
 from hub.features.deserialize import deserialize
 
-def test_serialize_deserialize(t):
+def test_serialize_deserialize():
+    t = Tensor(
+        shape=(100, 200),
+        dtype={
+            "image": Tensor(shape=(300, 400, 3), dtype="uint8"),
+            "label": Tensor(
+                shape=(5000,),
+                dtype={
+                    "first": {
+                        "a": "<U20",
+                        "b": "uint32",
+                        "c": Tensor(shape=(13, 17), dtype="float32"),
+                    },
+                    "second": "float64",
+                },
+            ),
+        },
+    )
+
     original_result = tuple(t._flatten())
     original_paths = [r.path for r in original_result]
     original_shapes = [r.shape for r in original_result]
@@ -22,22 +40,4 @@ def test_serialize_deserialize(t):
 
 
 if __name__ == "__main__":
-    t = Tensor(
-        shape=(100, 200),
-        dtype={
-            "image": Tensor(shape=(300, 400, 3), dtype="uint8"),
-            "label": Tensor(
-                shape=(5000,),
-                dtype={
-                    "first": {
-                        "a": "<U20",
-                        "b": "uint32",
-                        "c": Tensor(shape=(13, 17), dtype="float32"),
-                    },
-                    "second": "float64",
-                },
-            ),
-        },
-    )
-    
-    test_serialize_deserialize(t)
+    test_serialize_deserialize()
