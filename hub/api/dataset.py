e9633cbb5028ca1f3ed7032ce1b0ceb998538920
AbhinavTuli <abhinav.tuli99@gmail.com>
2020-10-01 15:59:02 +0530

    bug fixes

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -42,98 +42,49 @@ class Dataset:
                 dtype=t.dtype,
             )
 
-    def _slice_split(self, slice_):
-        path = slice_[0]
-        assert isinstance(path, str)
-        slice_ = slice_[1:]
-        path = path if path.startswith("/") else "/" + path
-        return path, slice_
-    def _slice_split_tuple(self, slice_):
-        path=""
-        list_slice=[]
-        for sl in slice_:
-            if isinstance(sl, str):
-                path += sl if sl.startswith("/") else "/" + sl
-            elif isinstance(sl,int) or isinstance(sl,slice):
-                list_slice.append(sl)
-            else:
-                raise TypeError("type {} isn't supported in dataset slicing".format(type(sl)))
-        tuple_slice = tuple(list_slice)
-        if path=="":
-            raise ValueError("No path found in slice!")
-        return path, tuple_slice
-
-    def _slice_extract_info(self, slice_):
-        assert isinstance(slice_, slice)
-
-        if slice_.step is not None and slice_.step<0:       # negative step not supported
-            raise ValueError("Negative step not supported in dataset slicing")
-
-        offset=0
-
-        if slice_.start is not None:
-            if slice_.start<0:                 # make indices positive if possible
-                slice_.start+=self.num_samples
-                if slice_.start<0:
-                    raise IndexError('index out of bounds for dimension with length {}'.format(self.num_samples))
-
-            if slice_.start>=self.num_samples:
-                raise IndexError('index out of bounds for dimension with length {}'.format(self.num_samples))
-            offset=slice_.start
-
-        if slice_.stop is not None:
-            if slice_.stop<0:                   # make indices positive if possible
-                slice_.stop+=self.num_samples
-                if slice_.stop<0:
-                    raise IndexError('index out of bounds for dimension with length {}'.format(self.num_samples))    
-
-            if slice_.stop>=self.num_samples:
-                raise IndexError('index out of bounds for dimension with length {}'.format(self.num_samples))
-
-        if slice_.start is not None and slice_.stop is not None:
-            if slice_.stop<slice_.start:    # return empty
-                num=0
-            else:
-                num=slice_.stop-slice_.start
-        elif slice_.start is None and slice_.stop is not None:
-            num=slice_.stop
-        elif slice_.start is not None and slice_.stop is None:
-            num=self.num_samples-slice_.start
-        else:
-            num=self.num_samples
-
-        return num, offset, slice_
+    # def _slice_split(self, slice_):
+    #     path = slice_[0]
+    #     assert isinstance(path, str)
+    #     slice_ = slice_[1:]
+    #     path = path if path.startswith("/") else "/" + path
+    #     return path, slice_
 
 
     def __getitem__(self, slice_):
         if isinstance(slice_,int):             # return Dataset with single sample
+            # doesn't handle negative right now
             if slice_>=self.num_samples:
                 raise IndexError('index out of bounds for dimension with length {}'.format(self.num_samples))
             return Dataset(_tensors=self._tensors, token=self.token, num_samples=1, mode=self.mode , dtype=self.dtype, offset=self.offset+slice_)
 
         elif isinstance(slice_,slice):         # return Dataset with multiple samples
-            num, ofs, slice_=self._slice_extract_info(slice_)
+            num, ofs =slice_extract_info(slice_,self.num_samples)
             return Dataset(_tensors=self._tensors, token=self.token, num_samples=num, mode=self.mode , dtype=self.dtype, offset=self.offset+ofs)
             
         elif isinstance(slice_, str):   
-            slice_ = slice_ if slice_.startswith("/") else "/" + slice_     # return TensorView object
-            return TensorView(_tensors=self._tensors, token=self.token, num_samples=self.num_samples, mode=self.mode , dtype=self.dtype, offset=self.offset, path=slice_)
+            path= slice_ if slice_.startswith("/") else "/" + slice_     # return TensorView object
+
+            #add slice for original Dataset range
+            offset_slice=slice(self.offset,self.offset+self.num_samples)
+            return TensorView(_tensors=self._tensors, token=self.token, mode=self.mode , dtype=self.dtype, path=path, slice_=offset_slice)
             
         elif isinstance(slice_,tuple):        # return tensor view object
-            path, slice_ = self._slice_split_tuple(slice_)
-            if len(slice_)==0:
-                num=self.num_samples
-                ofs=0
-            elif isinstance(slice_[0],int):
-                num=1
-                ofs=slice_[0]
-            elif isinstance(slice_[0],slice):
-                num, ofs, temp_slice = self._slice_extract_info(slice_[0])
-                ls=list(slice_)
-                ls[0]=temp_slice
-                slice_=tuple(ls)
-            return TensorView(_tensors=self._tensors, token=self.token, num_samples=num, mode=self.mode , dtype=self.dtype, offset=self.offset+ofs, path=path, slice_=slice_)
+            path, slice_ = slice_split_tuple(slice_)
 
+            if len(slice_)==0:
+                slice_=slice(self.offset,self.offset+self.num_samples)
+            elif isinstance(slice_[0],int):
+                if slice_[0]>=self.num_samples:
+                    raise IndexError('index out of bounds for dimension with length {}'.format(self.num_samples))
+                ls=list(slice_)
+                ls[0]+=self.offset
+                slice_=tuple(ls)
+            elif isinstance(slice_[0],slice):
+                num, ofs = slice_extract_info(slice_[0],self.num_samples)
+                ls=list(slice_)
+                ls[0]=slice(self.offset+ofs, self.offset+ofs+num)
+                slice_=tuple(ls)
+            return TensorView(_tensors=self._tensors, token=self.token, mode=self.mode , dtype=self.dtype, path=path, slice_=slice_)
         else:
             raise TypeError("type {} isn't supported in dataset slicing".format(type(slice_)))
 
@@ -148,7 +99,7 @@ class Dataset:
             self._tensors[slice_][slice(self.offset,self.offset+self.num_samples)] = value
             
         elif isinstance(slice_,tuple):        # return tensor view object
-            path, slice_ = self._slice_split_tuple(slice_)
+            path, slice_ = slice_split_tuple(slice_)
             if len(slice_)==0:
                 slice_= slice(self.offset,self.offset+self.num_samples)
             elif isinstance(slice_[0],int):
@@ -180,3 +131,61 @@ def open(
     url: str = None, token=None, num_samples: int = None, mode: str = None
 ) -> Dataset:
     raise NotImplementedError()
+
+
+def slice_split_tuple(slice_):
+        path=""
+        list_slice=[]
+        for sl in slice_:
+            if isinstance(sl, str):
+                path += sl if sl.startswith("/") else "/" + sl
+            elif isinstance(sl,int) or isinstance(sl,slice):
+                list_slice.append(sl)
+            else:
+                raise TypeError("type {} isn't supported in dataset slicing".format(type(sl)))
+        tuple_slice = tuple(list_slice)
+        if path=="":
+            raise ValueError("No path found in slice!")
+        return path, tuple_slice
+
+
+def slice_extract_info(slice_, num_samples):
+    assert isinstance(slice_, slice)
+
+    if slice_.step is not None and slice_.step<0:       # negative step not supported
+        raise ValueError("Negative step not supported in dataset slicing")
+
+    offset=0
+
+    if slice_.start is not None:
+        if slice_.start<0:                 # make indices positive if possible
+            slice_.start+=num_samples
+            if slice_.start<0:
+                raise IndexError('index out of bounds for dimension with length {}'.format(num_samples))
+
+        if slice_.start>=num_samples:
+            raise IndexError('index out of bounds for dimension with length {}'.format(num_samples))
+        offset=slice_.start
+
+    if slice_.stop is not None:
+        if slice_.stop<0:                   # make indices positive if possible
+            slice_.stop+=num_samples
+            if slice_.stop<0:
+                raise IndexError('index out of bounds for dimension with length {}'.format(num_samples))    
+
+        if slice_.stop>=num_samples:
+            raise IndexError('index out of bounds for dimension with length {}'.format(num_samples))
+
+    if slice_.start is not None and slice_.stop is not None:
+        if slice_.stop<slice_.start:    # return empty
+            num=0
+        else:
+            num=slice_.stop-slice_.start
+    elif slice_.start is None and slice_.stop is not None:
+        num=slice_.stop
+    elif slice_.start is not None and slice_.stop is None:
+        num=num_samples-slice_.start
+    else:
+        num=num_samples
+
+    return num, offset
--- hub/api/tensorview.py
+++ hub/api/tensorview.py
@@ -1,43 +1,31 @@
 from hub.features import featurify, FeatureConnector, FlatTensor
 from hub.store.storage_tensor import StorageTensor
-
+# from hub.api.dataset import slice_split_tuple, slice_extract_info
 class TensorView:
     def __init__(
         self,
         token=None,
-        num_samples: int = None,
         mode: str = None,
         dtype=None,
         path=None,
         slice_=None,
         _tensors=None,
-        offset=0
     ):
         assert dtype is not None
-        assert num_samples is not None
+        # assert num_samples is not None
         assert mode is not None
         assert path is not None
         assert _tensors is not None
 
         self.token = token
         self.mode = mode
-        self.num_samples = num_samples
+        # self.num_samples = num_samples
         self.dtype: FeatureConnector = dtype
         # self._flat_tensors: Tuple[FlatTensor] = tuple(self.dtype._flatten())
         self._tensors = _tensors
         self.path=path
-        self.offset = offset
+        # self.offset = offset
         self.slice_=slice_
-        if self.slice_ is not None and len(self.slice_)!=0:
-            ls=list(self.slice_)
-            # we assign to ls instead of adding offset to prevent doubling offset                
-            if isinstance(ls[0],int):
-                ls[0]=self.offset
-            elif isinstance(ls[0],slice):
-                ls[0]=slice(self.offset,offset+num_samples)
-            self.slice_=tuple(ls)
-        else:
-            self.slice_=slice(self.offset,offset+num_samples)
 
     
     def numpy(self):
