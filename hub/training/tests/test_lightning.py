c3df46abbd3dcc1d8c6ef67c087d2ec058db2fd7
kristinagrig06 <kristina@activeloop.ai>
2020-10-16 12:16:07 +0400

    Test log saving with PyTorch Lightning Module

---
+++ hub/training/tests/test_lightning.py
@@ -0,0 +1,143 @@
+import os
+
+from logs import Track
+from hub.api.dataset import Dataset
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import pytorch_lightning as pl
+from pytorch_lightning.metrics.functional import accuracy
+from pytorch_lightning.callbacks import Callback
+
+
+class LitMNIST(pl.LightningModule):
+    
+    def __init__(self, data_dir='./', hidden_size=64, learning_rate=2e-4):
+
+        super().__init__()        
+        # Set our init args as class attributes        
+        self.tracker = Track(logs=LOGS)
+        self.data_dir = data_dir
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+
+        # Hardcode some dataset specific attributes
+        self.num_classes = 10
+        self.dims = (1, 28, 28)
+        channels, width, height = self.dims
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+        # Define PyTorch model
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(channels * width * height, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, self.num_classes)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return F.log_softmax(x, dim=1)
+
+    # @pl.property
+    # def tracker(self):
+    #     return Track(num_of_epochs=self.num_of_epochs)    
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)     
+
+        self.log('train_loss', loss, prog_bar=True)
+        self.log('train_acc', acc, prog_bar=True)   
+        return {'loss': loss, 'train_acc': acc}
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+
+        # Calling self.log will surface up scalars for you in TensorBoard
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', acc, prog_bar=True)        
+        return {'val_acc': acc, 'val_loss': loss}
+
+    def validation_epoch_end(self, outputs):   
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()        
+        self.tracker.track(Track().scalar, 'val', {
+            'loss': avg_loss, 
+            'acc' : avg_acc
+        }).iterate()   
+
+    def training_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
+        avg_acc = torch.stack([x['train_acc'] for x in outputs]).mean()
+        self.tracker.track(Track().scalar, 'train', {
+            'loss': avg_loss, 
+            'acc' : avg_acc
+        }).iterate()  
+
+    def training_end(self):
+        self.tracker.logs.commit()
+
+    def test_step(self, batch, batch_idx):
+        # Here we just reuse the validation_step for testing
+        return self.validation_step(batch, batch_idx)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    ####################
+    # DATA RELATED HOOKS
+    ####################
+
+    def prepare_data(self):
+        # download
+        MNIST(self.data_dir, train=True, download=True)
+        MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+
+        # Assign train/val datasets for use in dataloaders
+        if stage == 'fit' or stage is None:
+            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == 'test' or stage is None:
+            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=32)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=32)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=32)
+
+epochs = 3
+LOGS = Dataset(dtype={"train_acc": float, "train_loss": float, "val_acc": float, "val_loss": float},
+                            shape=(epochs,), url='./models', mode='w')
+model = LitMNIST()
+trainer = pl.Trainer(gpus=1, max_epochs=epochs, progress_bar_refresh_rate=20, callbacks=[])
+trainer.fit(model)
+
+trainer.test()
