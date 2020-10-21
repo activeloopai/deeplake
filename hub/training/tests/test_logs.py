9e98aca8e581973fc42294ede78626721feac66e
kristinagrig06 <kristina@activeloop.ai>
2020-10-21 22:49:53 +0400

    Change test

--- hub/training/tests/test_logs.py
+++ hub/training/tests/test_logs.py
@@ -1,16 +1,17 @@
 from hub.api.dataset import Dataset
 from torch import tensor
+import numpy as np
 
 
 def test_logs():
     logs = Dataset(dtype={"train_acc": float, "train_loss": float, 
                           "val_acc": float, "val_loss": float},
-                   shape=(2,), url='./models', mode='w')
+                   shape=(1,), url='./models', mode='w')
     metrics_1 = {'val_loss': tensor(1.21), 
                  'val_acc': tensor(0.5),
                  'train_loss': tensor(2.4),
                  'train_acc': tensor(0.75)}
     for key, value in metrics_1.items():
         logs[key][0] = value.item()
-    assert logs['val_loss'][0].numpy() == 1.21
-    assert logs['train_loss'][0].numpy() == 2.4
+    assert np.isclose(logs['val_loss'][0].numpy(), 1.21)
+    assert np.isclose(logs['train_loss'][0].numpy(), 2.4)
