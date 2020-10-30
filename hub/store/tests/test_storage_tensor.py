f9a3be20aadfe52c2d2ab3c53726dce39cd2390c
Davit Buniatyan <david.buniatyan@gmail.com>
2020-10-29 19:50:05 -0700

    removing comments

--- hub/store/tests/test_storage_tensor.py
+++ hub/store/tests/test_storage_tensor.py
@@ -26,7 +26,6 @@ def test_s3_open():
         shape=[50, 100, 100],
         dtype="int32",
     )
-    # tensor = StorageTensor("s3://snark-test/test_storage_tensor/test_s3_open", mode="r")
     tensor[25:35, 10:20, 0] = np.ones((10, 10), dtype="int32")
     assert tensor.shape == (50, 100, 100)
     assert tensor.chunks == (50, 100, 100)
@@ -41,9 +40,6 @@ def test_gcs_open():
         shape=[50, 100, 100],
         dtype="int32",
     )
-    # tensor = StorageTensor(
-    #    "gcs://snark-test/test_storage_tensor/test_gcs_open", mode="r"
-    # )
     tensor[25:35, 10:20, 0] = np.ones((10, 10), dtype="int32")
     assert tensor.shape == (50, 100, 100)
     assert tensor.chunks == (50, 100, 100)
