93695db786ef35797d1de655f42160ad2f7606d2
AbhinavTuli <abhinav.tuli99@gmail.com>
2020-11-05 00:55:36 +0530

    removed duplicated function

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -146,32 +146,6 @@ class Dataset:
                     raise NotHubDatasetToAppendException()
             return True
 
-    def _check_and_prepare_dir(self):
-        """Checks if input data is ok
-        Creates or overwrites dataset folder
-        Returns True dataset needs to be created opposed to read
-        """
-        fs, path, mode = self._fs, self._path, self.mode
-        exist_meta = fs.exists(posixpath.join(path, "meta.json"))
-        if exist_meta:
-            if "w" in mode:
-                fs.rm(path, recursive=True)
-                fs.makedirs(path)
-                return True
-            return False
-        else:
-            if "r" in mode:
-                raise HubDatasetNotFoundException()
-            exist_dir = fs.exists(path)
-            if not exist_dir:
-                fs.makedirs(path)
-            elif get_file_count(fs, path) > 0:
-                if "w" in mode:
-                    raise NotHubDatasetToOverwriteException()
-                else:
-                    raise NotHubDatasetToAppendException()
-            return True
-
     def _generate_storage_tensors(self):
         for t in self._flat_tensors:
             t: FlatTensor = t
