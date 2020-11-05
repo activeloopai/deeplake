676e299b2db3fa477943138790f7bd4283596319
kristinagrig06 <kristina@activeloop.ai>
2020-11-05 23:25:44 +0400

    Resolve issue with None in shape

--- hub/store/dynamic_tensor.py
+++ hub/store/dynamic_tensor.py
@@ -175,11 +175,12 @@ class DynamicTensor:
         real_shapes = self._dynamic_tensor[slice_[0]] if self._dynamic_tensor else None
         slice_ = self._get_slice(slice_, real_shapes)
         
-        expected_value_shape = tuple([len(range(*slice_shape.indices(self.shape[i])))                                       
-                                      for i, slice_shape in enumerate(slice_) 
-                                      if not isinstance(slice_shape, int) ])
-        if value.shape != expected_value_shape:
-            raise ValueShapeError(expected_value_shape, value.shape)
+        if None not in self.shape:
+            expected_value_shape = tuple([len(range(*slice_shape.indices(self.shape[i])))                                       
+                                        for i, slice_shape in enumerate(slice_) 
+                                        if not isinstance(slice_shape, int)])
+            if type(value) in (np.ndarray, list) and np.array(value).shape != expected_value_shape:
+                raise ValueShapeError(expected_value_shape, value.shape)
         self._storage_tensor[slice_] = value
 
     def get_shape(self, slice_):
