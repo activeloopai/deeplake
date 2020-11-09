6367c638cd2eb0e624801603e6ef09cddd9a91ff
kristinagrig06 <kristina@activeloop.ai>
2020-11-09 10:57:39 +0400

    Change format

--- hub/store/dynamic_tensor.py
+++ hub/store/dynamic_tensor.py
@@ -176,9 +176,13 @@ class DynamicTensor:
         slice_ = self._get_slice(slice_, real_shapes)
 
         if None not in self.shape and not all([isinstance(sh, int) for sh in slice_]):
-            expected_value_shape = tuple([len(range(*slice_shape.indices(self.shape[i])))                                       
-                                        for i, slice_shape in enumerate(slice_) 
-                                        if not isinstance(slice_shape, int)])
+            expected_value_shape = tuple(
+                [
+                    len(range(*slice_shape.indices(self.shape[i])))
+                    for i, slice_shape in enumerate(slice_)
+                    if not isinstance(slice_shape, int)
+                ]
+            )
             if isinstance(value, list):
                 value = np.array(value)
             if value.shape != expected_value_shape:
