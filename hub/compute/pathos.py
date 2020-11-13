a1e291d4f3e4a3188287471d108a3164eafe94a6
Davit Buniatyan <david.buniatyan@gmail.com>
2020-11-12 23:30:17 -0800

    pathos mvp

--- hub/compute/pathos.py
+++ hub/compute/pathos.py
@@ -3,10 +3,43 @@ from hub.utils import batch
 from hub.compute.transform import Transform
 
 try:
-    import pathos
-except:
+    from pathos.pools import ProcessPool
+    from pathos.pools import ThreadPool
+except Exception:
     pass
 
 
 class PathosTransform(Transform):
-    pass
+    def __init__(self, func, schema, ds):
+        Transform.__init__(self, func, schema, ds)
+        self.map = ThreadPool(nodes=8).map
+
+    def store(self, url, token=None):
+        """
+        mary chunks with compute
+        """
+        ds = hub.Dataset(
+            url, mode="w", shape=self._ds.shape, schema=self._schema, token=token, cache=False
+        )
+
+        # Chunkwise compute
+        batch_size = ds.chunksize
+
+        def batchify(ds):
+            return tuple(batch(ds, batch_size))
+
+        def batched_func(i_xs):
+            i, xs = i_xs
+            xs = [self._func(x) for x in xs]
+            self._transfer_batch(ds, i, xs)
+
+        batched = batchify(ds)
+
+        results = self.map(batched_func, enumerate(batched))
+        results = list(results)
+        return ds
+
+    def _transfer_batch(self, ds, i, results):
+        for j, result in enumerate(results):
+            for key in result:
+                ds[key, i * ds.chunksize + j] = result[key]
--- hub/compute/tests/test_pipeline.py
+++ hub/compute/tests/test_pipeline.py
@@ -44,7 +44,7 @@ def test_pipeline_ray():
     pass
 
 
-def benchmark(sample_size=100, width=100, channels=4, dtype="int8"):
+def benchmark(sample_size=100, width=1000, channels=4, dtype="int8"):
     numpy_arr = np.zeros((sample_size, width, width, channels), dtype=dtype)
     zarr_fs = zarr.zeros((sample_size, width, width, channels), dtype=dtype, store=zarr.storage.FSStore("./data/test/array"), overwrite=True)
     zarr_lmdb = zarr.zeros((sample_size, width, width, channels), dtype=dtype, store=zarr.storage.LMDBStore("./data/test/array2"), overwrite=True)
@@ -57,9 +57,9 @@ def benchmark(sample_size=100, width=100, channels=4, dtype="int8"):
         "./data/test/test_pipeline_basic_3", mode="w", shape=(sample_size,), schema=my_schema, cache=0
     )
 
-    #ds_fs_cache = hub.Dataset(
-    #    "./data/test/test_pipeline_basic_2", mode="w", shape=(sample_size,), schema=my_schema
-    #)
+    ds_fs_cache = hub.Dataset(
+        "./data/test/test_pipeline_basic_2", mode="w", shape=(sample_size,), schema=my_schema
+    )
     if False:
         print(f"~~~ Sequential write of {sample_size}x{width}x{width}x{channels} random arrays ~~~")
         for name, arr in [("Numpy", numpy_arr), ("Zarr FS", zarr_fs), 
@@ -69,8 +69,8 @@ def benchmark(sample_size=100, width=100, channels=4, dtype="int8"):
                 for i in range(sample_size):
                     arr[i] = (np.random.rand(width, width, channels) * 255).astype(dtype)
 
-    print(f"~~~ Pipeline {sample_size}x{width}x{width}x{channels} random arrays~~~")
-    for name, processes in [("single", 1), ("pathos", 10), ("ray", 10), ("green", 10), ("dask", 10)]:
+    print(f"~~~ Pipeline {sample_size}x{width}x{width}x{channels} random arrays ~~~")
+    for name, processes in [("single", 1), ("pathos", 10)]: # , ("ray", 10), ("green", 10), ("dask", 10)]:
         @hub.transform(schema=my_schema, scheduler=name, processes=processes)
         def my_transform(sample):
             return {
