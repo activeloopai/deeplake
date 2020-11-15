48a1138f3fa7438f2d332a63b9b2cd3ef236d92b
AbhinavTuli <abhinav.tuli99@gmail.com>
2020-11-15 13:18:38 +0530

    adding coco converter asserts

--- hub/api/tests/test_converters.py
+++ hub/api/tests/test_converters.py
@@ -20,8 +20,9 @@ def test_from_tfds_coco():
     with tfds.testing.mock_data(num_examples=5):
         ds = hub.Dataset.from_tfds('coco', num=5)
         res_ds = ds.store("./data/test_tfds/coco", length=5)  # mock data doesn't have length, so explicitly provided
-        # assert res_ds["label"].numpy().tolist() == [1, 9, 2, 5, 3]
-        #TODO add assert here
+        assert res_ds["image_filename"].numpy().tolist() == [b'f dhgfdgeichbdba', b'dhibdaajeghaefeijch', b'ghd h afjj igecea', b'iccbhgaaehgad', b'dahehcihidgaifeicd']
+        assert res_ds["image_id"].numpy().tolist() == [90, 38, 112, 194, 105]
+        assert res_ds["objects"].numpy()[0]["label"][0:5].tolist() == [12, 15, 33, 23, 12]
 
 @pytest.mark.skipif(not tensorflow_loaded(), reason="requires tensorflow to be loaded")
 def test_from_tensorflow():
