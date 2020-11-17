63612dd148342a93a7b183a0ae386a5f91c6b663
Davit Buniatyan <david.buniatyan@gmail.com>
2020-11-16 20:31:41 -0800

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -2,25 +2,29 @@ from hub import Dataset, features
 import numpy as np
 from hub import dev_mode
 
-# Tag is set {Username}/{Dataset}
-tag = "davitb/basic11"
+def main():
+    # Tag is set {Username}/{Dataset}
+    tag = "davitb/basic11"
 
-# Create dataset
-ds = Dataset(
-    tag,
-    shape=(4,),
-    schema={
-        "image": features.Tensor((512, 512), dtype="float"),
-        "label": features.Tensor((512, 512), dtype="float"),
-    },
-)
+    # Create dataset
+    ds = Dataset(
+        tag,
+        shape=(4,),
+        schema={
+            "image": features.Tensor((512, 512), dtype="float"),
+            "label": features.Tensor((512, 512), dtype="float"),
+        },
+    )
 
 
-# Upload Data
-ds["image"][:] = np.ones((4, 512, 512))
-ds["label"][:] = np.ones((4, 512, 512))
-ds.commit()
+    # Upload Data
+    ds["image"][:] = np.ones((4, 512, 512))
+    ds["label"][:] = np.ones((4, 512, 512))
+    ds.commit()
 
-# Load the data
-ds = Dataset(tag)
-print(ds["image"][0].numpy())
+    # Load the data
+    ds = Dataset(tag)
+    print(ds["image"][0].compute())
+
+if __name__ == "__main__":
+    main()
