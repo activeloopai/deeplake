e142c59a066995540ec8b9116eb08ceb5baa7b99
Davit Buniatyan <david.buniatyan@gmail.com>
2020-11-20 12:33:42 -0800

    test fix because of merge

--- hub/api/tests/test_converters.py
+++ hub/api/tests/test_converters.py
@@ -117,7 +117,7 @@ def test_to_pytorch():
     for i, batch in enumerate(ds):
         assert (batch["label"]["d"]["e"].numpy() == i * np.ones((5, 3))).all()
 
-        
+
 @pytest.mark.skipif(not pytorch_loaded(), reason="requires pytorch to be loaded")
 def test_from_pytorch():
     from torch.utils.data import Dataset
@@ -138,13 +138,8 @@ def test_from_pytorch():
             landmarks = 7 * np.ones((10, 10, 10))
             named = "testing text labels"
             sample = {
-                "data": {
-                    'image': image,
-                    'landmarks': landmarks
-                },
-                "labels": {
-                    "named": named
-                }
+                "data": {"image": image, "landmarks": landmarks},
+                "labels": {"named": named},
             }
 
             if self.transform:
@@ -153,11 +148,10 @@ def test_from_pytorch():
 
     tds = TestDataset()
     ds = hub.Dataset.from_pytorch(tds)
-    ds = ds.store("./data/
-                  /test1")
-    assert(ds["data", "image", 3].numpy() == 5 * np.ones((50, 50))).all()
-    assert(ds["data", "landmarks", 2].numpy() == 7 * np.ones((10, 10, 10))).all()
-    assert(ds["labels", "named", 5].numpy() == "testing text labels")
+    ds = ds.store("./data//test1")
+    assert (ds["data", "image", 3].numpy() == 5 * np.ones((50, 50))).all()
+    assert (ds["data", "landmarks", 2].numpy() == 7 * np.ones((10, 10, 10))).all()
+    assert ds["labels", "named", 5].numpy() == "testing text labels"
 
 
 @pytest.mark.skipif(not pytorch_loaded(), reason="requires pytorch to be loaded")
@@ -177,11 +171,10 @@ def test_to_from_pytorch():
         ds["label", "d", "e", i] = i * np.ones((5, 3))
     ds = ds.to_pytorch()
     out_ds = hub.Dataset.from_pytorch(ds)
-    res_ds = out_ds.store(
-        "./data/test_from_pytorch/test3"
-    )
+    res_ds = out_ds.store("./data/test_from_pytorch/test3")
     for i in range(10):
         assert (res_ds["label", "d", "e", i].numpy() == i * np.ones((5, 3))).all()
+
 
 if __name__ == "__main__":
     test_to_from_pytorch()
