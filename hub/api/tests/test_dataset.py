3dc301de85e3da9096972372312003f78b1ed322
Edward Grigoryan <edogrigqv2@gmail.com>
2020-11-24 10:59:15 +0400

    Moved bug tests from seperate folder into existing one

--- hub/api/tests/test_dataset.py
+++ hub/api/tests/test_dataset.py
@@ -181,6 +181,24 @@ def test_dataset_bug():
     )
 
 
+def test_dataset_bug_1(url="./data/test/dataset", token=None):
+    my_schema = {
+        "image": Tensor(
+            (None, 1920, 1080, None), "uint8", max_shape=(10, 1920, 1080, 4)
+        ),
+    }
+    ds = Dataset(url, token=token, shape=(10000,), mode="w", schema=my_schema)
+    ds["image", 1] = np.ones((2, 1920, 1080, 1))
+
+
+def test_dataset_bug_2(url="./data/test/dataset", token=None):
+    my_schema = {
+        "image": Tensor((100, 100), "uint8"),
+    }
+    ds = Dataset(url, token=token, shape=(10000,), mode="w", schema=my_schema)
+    ds["image", 0:1] = [np.zeros((100, 100))]
+
+
 @pytest.mark.skipif(not gcp_creds_exist(), reason="requires gcp credentials")
 def test_dataset_gcs():
     test_dataset("gcs://snark-test/test_dataset_gcs")
