ee976d30d84ee32339ff14003653d2a90288becd
pathanzafar702 <53207784+pathanzafar702@users.noreply.github.com>
2020-11-28 12:47:10 +0530

    Create candycrush.cpp

---
+++ candycrush.cpp
@@ -0,0 +1,48 @@
+// Time:  O((R * C)^2)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> candyCrush(vector<vector<int>>& board) {
+        const auto R = board.size(), C = board[0].size();
+        bool changed = true;
+        
+        while (changed) {
+            changed = false;
+            
+            for (int r = 0; r < R; ++r) {
+                for (int c = 0; c + 2 < C; ++c) {
+                    auto v = abs(board[r][c]);
+                    if (v != 0 && v == abs(board[r][c + 1]) && v == abs(board[r][c + 2])) {
+                        board[r][c] = board[r][c + 1] = board[r][c + 2] = -v;
+                        changed = true;
+                    }
+                }
+            }
+            
+            for (int r = 0; r + 2 < R; ++r) {
+                for (int c = 0; c < C; ++c) {
+                    auto v = abs(board[r][c]);
+                    if (v != 0 && v == abs(board[r + 1][c]) && v == abs(board[r + 2][c])) {
+                        board[r][c] = board[r + 1][c] = board[r + 2][c] = -v;
+                        changed = true;
+                    }
+                }
+            }
+
+            for (int c = 0; c < C; ++c) {
+                int empty_r = R - 1;
+                for (int r = R - 1; r >= 0; --r) {
+                    if (board[r][c] > 0) {
+                        board[empty_r--][c] = board[r][c];
+                    }
+                }
+                for (int r = empty_r; r >= 0; --r) {
+                    board[r][c] = 0;
+                }
+            }
+        }
+        
+        return board;
+    }
+};
