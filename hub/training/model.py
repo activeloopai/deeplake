32c670a91ae0d6234ffec8e9af67b69be7355735
kristinagrig06 <kristina@activeloop.ai>
2020-11-30 21:16:04 +0400

    Change torch state dict model storing

--- hub/training/model.py
+++ hub/training/model.py
@@ -27,10 +27,11 @@ if tensorflow_spec is not None:
 if pytorch_lightning_spec is not None:
     PYTORCH_LIGHTNING_MODEL_CLASSES = (pl.LightningModule,)
 
+
 class Model:
     def __init__(self, model=None, metainfo: Dict = dict()):
         """Creates model with given metainfo.
-        
+
         Parameters
         ----------
         model: PyTorch, Tensorflow or Pytorch Lightning model object
@@ -45,7 +46,7 @@ class Model:
         """ Model description
         """
         return self._metainfo.get("description") if self._metainfo else None
- 
+
     def load(self, model_path: str, token: str = None):
         """Loads a Pytorch or Tensorflow model
 
@@ -70,17 +71,17 @@ class Model:
         if model_path.startswith('s3://'):
             if not token:
                 token = os.environ['AWS_CONFIG_FILE']
-            fs, url = get_fs_and_path(model_path, token=token)  
-            url = os.path.join('s3://', url) 
+            fs, url = get_fs_and_path(model_path, token=token)
+            url = os.path.join('s3://', url)
         else:
-            fs, url = get_fs_and_path(model_path)   
-        if model_path.endswith('.pth') or model_path.endswith('.pt'): 
+            fs, url = get_fs_and_path(model_path)
+        if model_path.endswith('.pth') or model_path.endswith('.pt'):
             if 'torch' not in sys.modules:
                 raise ModuleNotFoundError("Unable to load a model. \
                                         Module 'torch' is not installed")
-            with fs.open(model_path, 'rb') as opened_file:  
-                self._model = torch.load(opened_file)
-        elif model_path.endswith('.h5'):    
+            with fs.open(model_path, 'rb') as opened_file:
+                self._model.load_state_dict(torch.load(opened_file))
+        elif model_path.endswith('.h5'):
             if 'tensorflow' not in sys.modules:
                 raise ModuleNotFoundError("Unable to load a model. \
                                         Module 'tensorflow' is not installed")
@@ -93,10 +94,9 @@ class Model:
                                         Module 'tensorflow' is not installed")
             self._model = tf.keras.models.load_model(model_path)
         else:
-            raise ValueError("Not supported model type")  
+            raise ValueError("Not supported model type")
 
-
-    def store(self, model_dir: str, token: str = None): 
+    def store(self, model_dir: str, token: str = None):
         """Saves an object to a file.
 
         Usage
@@ -121,17 +121,21 @@ class Model:
         if model_dir.startswith('s3://'):
             if not token:
                 token = os.environ['AWS_CONFIG_FILE']
-            fs, url = get_fs_and_path(model_dir, token=token)  
-            url = os.path.join('s3://', url) 
+            fs, url = get_fs_and_path(model_dir, token=token)
+            url = os.path.join('s3://', url)
         else:
             fs, url = get_fs_and_path(model_dir)
         model_class = self._model.__class__
-        if (("PYTORCH_MODEL_CLASSES" in globals() and issubclass(
-            model_class, PYTORCH_MODEL_CLASSES)) or ("PYTORCH_LIGHTNING_MODEL_CLASSES" in globals()
-            and issubclass(model_class, PYTORCH_LIGHTNING_MODEL_CLASSES))):
+        if ("PYTORCH_MODEL_CLASSES" in globals() and issubclass(model_class,
+                                                                PYTORCH_MODEL_CLASSES)):
             model_full_path = os.path.join(url, model_class.__name__ + ".pth")
             with fs.open(model_full_path, "wb") as opened_file:
                 torch.save(self._model.state_dict(), opened_file)
+        elif ("PYTORCH_LIGHTNING_MODEL_CLASSES" in globals()
+              and issubclass(model_class, PYTORCH_LIGHTNING_MODEL_CLASSES)):
+            model_full_path = os.path.join(url, model_class.__name__ + ".pth")
+            with fs.open(model_full_path, "wb") as opened_file:
+                torch.save(self._model.model.state_dict(), opened_file)
         elif "TENSORFLOW_MODEL_CLASSES" in globals() and issubclass(model_class,
                                                                     TENSORFLOW_MODEL_CLASSES):
             model_full_path = os.path.join(url, model_class.__name__ + '.h5')
