e2e9332c35619a5ec79df9220e673c86407eb928
kristinagrig06 <kristina@activeloop.ai>
2020-12-08 19:22:59 +0400

    Fix linting

--- hub/training/train.py
+++ hub/training/train.py
@@ -17,8 +17,14 @@ from hub.training.lightning_model import LitMNIST
 
 
 @ray.remote(num_gpus=1, num_cpus=4, max_calls=1)
-def train_model_tune(model: pl.LightningModule, dataloaders: Tuple, num_epochs: int = 3,
-                     logs_dir: str = './models/logs', model_output_dir: str = './models/', **kwargs):
+def train_model_tune(
+    model: pl.LightningModule,
+    dataloaders: Tuple,
+    num_epochs: int = 3,
+    logs_dir: str = "./models/logs",
+    model_output_dir: str = "./models/",
+    **kwargs
+):
     """Train Pytorch Lightning model using Ray
 
     Arguments:
@@ -28,17 +34,19 @@ def train_model_tune(model: pl.LightningModule, dataloaders: Tuple, num_epochs: 
     num_epochs: Number of epochs to be run
     logs_dir: Directory to which training metrics will be stored
     model_output_dir: Directory to which model state_dict will be saved
-    **kwargs: Arbitrary keyword arguments for model initialization. 
+    **kwargs: Arbitrary keyword arguments for model initialization.
     """
     # log_dataset = Dataset(dtype={"train_acc": float, "train_loss": float, "val_acc": float, "val_loss": float},
     #                       shape=(num_epochs,),
     #                       url = logs_dir,
-    #                       mode='w')    
+    #                       mode='w')
     model = model(**kwargs)
     model_obj = Model(model)
-    trainer = pl.Trainer(max_epochs=num_epochs, 
-                         progress_bar_refresh_rate=20, num_sanity_val_steps=0, 
-        )
+    trainer = pl.Trainer(
+        max_epochs=num_epochs,
+        progress_bar_refresh_rate=20,
+        num_sanity_val_steps=0,
+    )
     trainer.fit(model, dataloaders[0], val_dataloaders=dataloaders[1])
 
     if len(dataloaders) == 3:
@@ -47,8 +55,15 @@ def train_model_tune(model: pl.LightningModule, dataloaders: Tuple, num_epochs: 
     model_obj.store(model_output_dir)
 
 
-def fit(model: pl.LightningModule, dataloaders: Tuple, num_epochs: int = 3, model_output_dir: str = '.', 
-        num_gpus: int = 1, num_cpus: int = multiprocessing.cpu_count(), **kwargs):
+def fit(
+    model: pl.LightningModule,
+    dataloaders: Tuple,
+    num_epochs: int = 3,
+    model_output_dir: str = ".",
+    num_gpus: int = 1,
+    num_cpus: int = multiprocessing.cpu_count(),
+    **kwargs
+):
     """Fit dataloaders into model using remote Ray function
 
     Arguments:
@@ -62,16 +77,22 @@ def fit(model: pl.LightningModule, dataloaders: Tuple, num_epochs: int = 3, mode
     ray.init()
 
     g = train_model_tune.options(num_gpus=num_gpus, num_cpus=num_cpus)
-    ray.get(g.remote(model=model, dataloaders=dataloaders, model_output_dir=model_output_dir, **kwargs))
+    ray.get(
+        g.remote(
+            model=model,
+            dataloaders=dataloaders,
+            model_output_dir=model_output_dir,
+            **kwargs
+        )
+    )
 
 
 if __name__ == "__main__":
-    transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ])
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
 
-    data_dir='./'
+    data_dir = "./"
     mnist_full = MNIST(data_dir, train=True, transform=transform)
     mnist_train, mnist_val = random_split(mnist_full, [55000, 5000])
     mnist_test = MNIST(data_dir, train=False, transform=transform)
