a31c0e7f8c8b02d03c879991d3067f557810ea85
sparkingdark <debmastet335@gmail.com>
2020-12-12 12:27:18 +0530

    trying to solve linting

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -677,8 +677,7 @@ class Dataset:
     def from_directory(
         url, path_to_dir, image_shape, max_shape=(1920, 1080, 4), mode="w+"
     ):
-        """
-        This utility function is specific to create dataset from the categorical image dataset.
+        """This utility function is specific to create dataset from the categorical image dataset.
 
         """
         def get_ds_size(path_to_dir):
