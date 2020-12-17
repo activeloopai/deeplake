5f0898c018ce4f96b9a32f476e1b0cda56a1a16c
AbhinavTuli <abhinav.tuli99@gmail.com>
2020-12-18 02:04:47 +0530

    removed example

--- example_private.py
+++
@@ -1,8 +0,0 @@
-import hub
-import numpy as np
-schema = {
-    "abc" : "int32"
-}
-ds = hub.Dataset("abhinav/try_pvt21", schema=schema, mode="w", shape=(5,), public=False)
-ds["abc", 0] = 7
-ds.commit()
