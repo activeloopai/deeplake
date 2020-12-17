cd884f6c346efc60399c1a5c8dad7608e9203c1e
sparkingdark <debmastet335@gmail.com>
2020-12-17 10:27:43 +0530

    commenting

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -676,7 +676,10 @@ class Dataset:
 
     @staticmethod
     def from_directory(url, path_to_dir, image_shape, mode="w+"):
-        """This utility function is specific to create dataset from the categorical image dataset."""
+        """This utility function is specific to create dataset from the categorical image dataset.
+        ------------------------------------------------------------------------------------------
+        
+        """
 
         def get_ds_size(path_to_dir):
             ds = 1
