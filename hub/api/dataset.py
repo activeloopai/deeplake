225da88a88de2e92ba7bec75df1c86151b0063d6
sparkingdark <debomastet335@gmail.com>
2020-12-21 20:53:36 +0530

    done with the linting

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -88,7 +88,7 @@ class Dataset:
             token is the parameter to pass the credentials, it can be filepath or dict
         fs: optional
         fs_map: optional
-        path_to_description_file: Optional give path to a json file containing information about the author,license,description about the dataset. 
+        path_to_description_file: Optional give path to a json file containing information about the author,license,description about the dataset.
         cache: int, optional
             Size of the memory cache. Default is 64MB (2**26)
             if 0, False or None, then cache is not used
@@ -218,37 +218,38 @@ class Dataset:
 
     @property
     def path_to_description_file(self):
-        return self._path_to_description_file    
+        return self._path_to_description_file
 
     def _store_meta(self) -> dict:
         def make_description(path_to_description_file):
-            '''
+            """
             Utility function to create description,license everything about the dataset.
 
             parameter
             ---------
             path_to_description_file: give a json file containing description of the dataset
 
-            '''
+            """
             import json
-            
+
             f = open(path_to_description_file)
             data = json.load(f)
 
             return {
-                "description":data["description"],
-                "license":data["license"],
-                "citation":data["citation"]
+                "description": data["description"],
+                "license": data["license"],
+                "citation": data["citation"],
             }
+
         meta = {
             "shape": self.shape,
             "schema": hub.schema.serialize.serialize(self.schema),
             "version": 1,
         }
-        
-        if self.path_to_description_file!=None:
+
+        if self.path_to_description_file != None:
             meta.update(make_description(self.path_to_description_file))
-            
+
         self._fs_map["meta.json"] = bytes(json.dumps(meta), "utf-8")
         return meta
 
