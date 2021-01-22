efecb1d4d937cc129830126b56b2260f51720b44
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-01-22 14:09:56 +0530

    linting fix

--- examples/dataset_endpoint_minio.py
+++ examples/dataset_endpoint_minio.py
@@ -9,11 +9,13 @@ token = {
     "aws_access_key_id": "Q3AM3UQ867SPQQA43P2F",
     "aws_secret_access_key": "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
     "endpoint_url": "https://play.min.io:9000",
-    "region": "us-east-1"
+    "region": "us-east-1",
 }
 
 schema = {"abc": Tensor((100, 100, 3))}
-ds = hub.Dataset("s3://mybucket/random_dataset", token=token, shape=(10,), schema=schema)
+ds = hub.Dataset(
+    "s3://mybucket/random_dataset", token=token, shape=(10,), schema=schema
+)
 
 for i in range(10):
     ds["abc", i] = np.ones((100, 100, 3))
