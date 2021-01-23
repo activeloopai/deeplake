f3a24c586b372fbe20298eab0a71639bc8682009
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-01-23 13:29:07 +0530

    added tests for sharded datasetview

--- hub/api/tests/test_sharded_dataset.py
+++ hub/api/tests/test_sharded_dataset.py
@@ -1,5 +1,8 @@
+from hub.schema.features import SchemaDict
+from hub.exceptions import AdvancedSlicingNotSupported
 from hub.api.sharded_datasetview import ShardedDatasetView
 from hub import Dataset
+import pytest
 
 
 def test_sharded_dataset():
@@ -13,9 +16,19 @@ def test_sharded_dataset():
 
     ds[0]["first"] = 2.3
     assert ds[0]["second"].numpy() != 2.3
-    print(ds[30]["first"].numpy())
     assert ds[30]["first"].numpy() == 0
     assert len(ds) == 40
+    assert ds.shape == (40,)
+    assert type(ds.schema) == SchemaDict
+    assert ds.__repr__() == "ShardedDatasetView(shape=(40,))"
+    with pytest.raises(AdvancedSlicingNotSupported):
+        ds[5:8]
+    ds[4, "first"] = 3
+    for _ in ds:
+        pass
+
+    ds2 = ShardedDatasetView([])
+    assert ds2.identify_shard(5) == (0, 0)
 
 
 if __name__ == "__main__":
