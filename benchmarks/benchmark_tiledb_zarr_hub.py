b5904fcb324a7914a307aa2008c6d0bc205447b6
DebadityaPal <debaditya.pal6@gmail.com>
2021-02-04 21:12:29 +0530

    Put all benchmarks in one file

--- benchmarks/benchmark_tiledb_zarr_hub.py
+++ benchmarks/benchmark_tiledb_zarr_hub.py
@@ -4,6 +4,7 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
 
+import tiledb
 import zarr
 import hub
 import numpy as np
@@ -12,11 +13,49 @@ from time import time
 from hub.utils import Timer
 
 
-def time_zarr(dataset, batch_size=1):
-    ds = hub.Dataset(dataset)
+def time_tiledb(dataset, batch_size=1, split=None):
+    if os.path.exists(dataset.split("/")[1] + "_tileDB"):
+        ds_tldb = tiledb.open(dataset.split("/")[1] + "_tileDB")
+    else:
+        if split is not None:
+            ds = hub.Dataset(dataset + "_" + split)
+        else:
+            ds = hub.Dataset(dataset)
+        if not os.path.exists(dataset.split("/")[1] + "_tileDB"):
+            os.makedirs(dataset.split("/")[1] + "_tileDB")
+        ds_numpy = np.concatenate(
+            (
+                ds["image"].compute().reshape(ds.shape[0], -1),
+                ds["label"].compute().reshape(ds.shape[0], -1),
+            ),
+            axis=1,
+        )
+        ds_tldb = tiledb.from_numpy(dataset.split("/")[1] + "_tileDB", ds_numpy)
+
+    assert type(ds_tldb) == tiledb.array.DenseArray
+
+    with Timer("Time"):
+        counter = 0
+        t0 = time()
+        for batch in range(ds_tldb.shape[0] // batch_size):
+            x, y = (
+                ds_tldb[batch * batch_size : (batch + 1) * batch_size, :-1],
+                ds_tldb[batch * batch_size : (batch + 1) * batch_size, -1],
+            )
+            counter += 1
+            t1 = time()
+            print("Batch", counter, f"dt: {t1 - t0}")
+            t0 = t1
+
+
+def time_zarr(dataset, batch_size=1, split=None):
     if os.path.exists(dataset.split("/")[1] + "_zarr"):
         ds_zarr = zarr.open(dataset.split("/")[1] + "_zarr")
     else:
+        if split is not None:
+            ds = hub.Dataset(dataset + "_" + split)
+        else:
+            ds = hub.Dataset(dataset)
         store = zarr.DirectoryStore(dataset.split("/")[1] + "_zarr")
         shape = [
             ds["image"].shape[0],
@@ -44,7 +83,7 @@ def time_zarr(dataset, batch_size=1):
     with Timer("Time"):
         counter = 0
         t0 = time()
-        for batch in range(ds.shape[0] // batch_size):
+        for batch in range(ds_zarr.shape[0] // batch_size):
             x, y = (
                 ds_zarr[batch * batch_size : (batch + 1) * batch_size, :-1],
                 ds_zarr[batch * batch_size : (batch + 1) * batch_size, -1],
@@ -55,8 +94,11 @@ def time_zarr(dataset, batch_size=1):
             t0 = t1
 
 
-def time_hub(dataset, batch_size=1):
-    ds = hub.Dataset(dataset)
+def time_hub(dataset, batch_size=1, split=None):
+    if split is not None:
+        ds = hub.Dataset(dataset + "_" + split)
+    else:
+        ds = hub.Dataset(dataset)
 
     assert type(ds) == hub.api.dataset.Dataset
 
@@ -74,7 +116,7 @@ def time_hub(dataset, batch_size=1):
             t0 = t1
 
 
-datasets = ["activeloop/mnist", "hydp/places365_small_train"]
+datasets = ["activeloop/mnist"]
 batch_sizes = [7000, 70000]
 
 
@@ -86,13 +128,16 @@ if __name__ == "__main__":
             dataset = "_".join(dataset)
             data = hub.Dataset.from_tfds(dataset.split("/")[1], split=split)
         else:
+            split = None
             data = hub.Dataset.from_tfds(dataset.split("/")[1])
         data.store("./" + dataset.split("/")[1] + "_hub")
         for batch_size in batch_sizes:
             print("Dataset: ", dataset, "with Batch Size: ", batch_size)
+            print("Performance of TileDB")
+            time_tiledb(dataset, batch_size, split)
             print("Performance of Zarr")
-            time_zarr(dataset, batch_size)
+            time_zarr(dataset, batch_size, split)
             print("Performance of Hub (Stored on the Cloud):")
-            time_hub(dataset, batch_size)
+            time_hub(dataset, batch_size, split)
             print("Performance of Hub (Stored Locally):")
-            time_hub("./" + dataset.split("/")[1] + "_hub", batch_size)
+            time_hub("./" + dataset.split("/")[1] + "_hub", batch_size, split=None)
