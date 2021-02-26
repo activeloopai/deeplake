604f3c3d73ec49e65cea46297f3430e78be6ae2b
kristinagrig06 <kristina@activeloop.ai>
2021-02-26 23:47:46 +0400

    Modify Model class

--- hub/training/model.py
+++ hub/training/model.py
@@ -1,14 +1,7 @@
-"""
-License:
-This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
-If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
-"""
-
 import os
 import sys
-from typing import Union, Dict
-import json
-import tempfile
+from typing import Dict
+import time
 import io
 
 from hub.store.store import get_fs_and_path
@@ -22,11 +15,17 @@ tensorflow_spec = importlib.util.find_spec("tensorflow")
 if tensorflow_spec is not None:
     import tensorflow as tf
     import h5py
+pytorch_lightning_spec = importlib.util.find_spec("pytorch_lightning")
+if pytorch_lightning_spec is not None:
+    import pytorch_lightning as pl
+
 
 if torch_spec is not None:
     PYTORCH_MODEL_CLASSES = (torch.nn.Module, torch.nn.Sequential)
 if tensorflow_spec is not None:
     TENSORFLOW_MODEL_CLASSES = (tf.keras.Model, tf.keras.Sequential)
+if pytorch_lightning_spec is not None:
+    PYTORCH_LIGHTNING_MODEL_CLASSES = (pl.LightningModule,)
 
 
 class Model:
@@ -70,7 +69,11 @@ class Model:
         """
         if model_path.startswith("s3://"):
             if not token:
-                token = os.environ["AWS_CONFIG_FILE"]
+                token = {
+                    "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID"),
+                    "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
+                    "aws_default_region": os.environ.get("AWS_DEFAULT_REGION"),
+                }
             fs, url = get_fs_and_path(model_path, token=token)
             url = os.path.join("s3://", url)
         else:
@@ -102,7 +105,7 @@ class Model:
         else:
             raise ValueError("Not supported model type")
 
-    def store(self, model_dir: str, token: str = None):
+    def store(self, model_dir: str, token: str = None, epoch: int = None):
         """Saves an object to a file.
 
         Usage
@@ -126,29 +129,40 @@ class Model:
         """
         if model_dir.startswith("s3://"):
             if not token:
-                token = os.environ["AWS_CONFIG_FILE"]
+                token = {
+                    "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID"),
+                    "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
+                    "aws_default_region": os.environ.get("AWS_DEFAULT_REGION"),
+                }
             fs, url = get_fs_and_path(model_dir, token=token)
             url = os.path.join("s3://", url)
         else:
             fs, url = get_fs_and_path(model_dir)
         model_class = self._model.__class__
-        if "PYTORCH_MODEL_CLASSES" in globals() and issubclass(
-            model_class, PYTORCH_MODEL_CLASSES
+        if (
+            "PYTORCH_MODEL_CLASSES" in globals()
+            and issubclass(model_class, PYTORCH_MODEL_CLASSES)
+        ) or (
+            "PYTORCH_LIGHTNING_MODEL_CLASSES" in globals()
+            and issubclass(model_class, PYTORCH_LIGHTNING_MODEL_CLASSES)
         ):
-            model_full_path = os.path.join(url, model_class.__name__ + ".pth")
+            if epoch:
+                model_full_path = os.path.join(
+                    url, model_class.__name__ + f"_{epoch}.pth"
+                )
+            else:
+                model_full_path = os.path.join(
+                    url, model_class.__name__ + f"_{time.time()}.pth"
+                )
             with fs.open(model_full_path, "wb") as opened_file:
                 torch.save(self._model, opened_file)
         elif "TENSORFLOW_MODEL_CLASSES" in globals() and issubclass(
             model_class, TENSORFLOW_MODEL_CLASSES
         ):
-            try:
-                model_full_path = os.path.join(url, model_class.__name__ + ".h5")
-                io_h5 = io.BytesIO()
-                self._model.save(io_h5)
-                with fs.open(model_full_path, "wb") as opened_file:
-                    opened_file.write(io_h5.getbuffer())
-            except TypeError:
-                model_full_path = os.path.join(url, model_class.__name__ + ".tf")
-                self._model.save(model_full_path)
+            model_full_path = os.path.join(url, model_class.__name__ + ".h5")
+            io_h5 = io.BytesIO()
+            self._model.save(io_h5)
+            with fs.open(model_full_path, "wb") as opened_file:
+                opened_file.write(io_h5.getbuffer())
         else:
             raise ValueError(f"Unable to store a model of type {type(self._model)}")
