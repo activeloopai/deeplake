98d055b4b10f194c95c27a283a39f95030896157
kristinagrig06 <kristina@activeloop.ai>
2021-02-27 01:21:34 +0400

    Fix tests

--- hub/training/tests/test_model.py
+++ hub/training/tests/test_model.py
@@ -4,6 +4,9 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
 
+import os
+import glob
+import shutil
 import numpy as np
 import pytest
 from hub.utils import pytorch_loaded, tensorflow_loaded
@@ -26,21 +29,25 @@ if tensorflow_spec is not None:
 def test_store_load_torch():
     if torch_spec is None:
         raise ModuleNotFoundError("Module 'torch' is not installed")
+    model_dir = "./data/pytorch_test/"
+    shutil.rmtree(model_dir, ignore_errors=True)
+    os.makedirs(model_dir)
     model_arch = torch.nn.Sequential(
         torch.nn.Linear(1000, 100),
         torch.nn.ReLU(),
         torch.nn.Linear(100, 10),
     )
     model_init = Model(model_arch)
-    model_init.store("./data/")
+    model_init.store(model_dir)
     loaded_model = Model()
-    loaded_model.load(f"./data/{model_init._model.__class__.__name__}.pth")
+    loaded_model.load(glob.glob(f"{model_dir}*.pth")[0])
     assert repr(model_init._model) == repr(loaded_model._model)
-    for (p1, p2) in zip(
-        model_init._model.named_parameters(), loaded_model._model.named_parameters()
-    ):
-        assert p1[0] == p2[0]
-        assert torch.equal(p1[1].data, p2[1].data)
+    with torch.no_grad():
+        for (p1, p2) in zip(
+            model_init._model.named_parameters(), loaded_model._model.named_parameters()
+        ):
+            assert p1[0] == p2[0]
+            assert torch.equal(p1[1].data, p2[1].data)
 
 
 @pytest.mark.skipif(
@@ -52,6 +59,10 @@ def test_store_load_tf():
         raise ModuleNotFoundError("Module 'tensorflow' is not installed")
     ext = ".h5" if int((tf.__version__)[0]) < 2 else ".tf"
 
+    model_dir = "./data/tensorflow_test/"
+    shutil.rmtree(model_dir, ignore_errors=True)
+    os.makedirs(model_dir)
+
     def get_model():
         inputs = tf.keras.Input(shape=(32,))
         outputs = tf.keras.layers.Dense(1)(inputs)
@@ -62,9 +73,9 @@ def test_store_load_tf():
     model_arch = get_model()
     model_init = Model(model_arch)
     test_input = np.random.random((128, 32))
-    model_init.store("/tmp/")
+    model_init.store(model_dir)
     loaded_model = Model()
-    loaded_model.load(f"/tmp/{model_init._model.__class__.__name__}{ext}")
+    loaded_model.load(glob.glob(f"{model_dir}*.h5")[0])
     np.testing.assert_allclose(
         model_init._model.predict(test_input), loaded_model._model.predict(test_input)
     )
