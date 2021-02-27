69e5dad9b9d4f959c24ccab75b88b6fc2127f7ff
Debojyoti Chakraborty <debomastet335@gmail.com>
2021-02-27 13:00:26 +0000

    added versionz'

--- hub/cli/command.py
+++ hub/cli/command.py
@@ -6,9 +6,11 @@ If a copy of the MPL was not distributed with this file, You can obtain one at h
 
 import click
 
+import hub
 from hub import config
 from hub.log import configure_logger
 from hub.cli.auth import login, logout, register
+from hub.version import __version__
 
 
 @click.group()
@@ -22,18 +24,21 @@ from hub.cli.auth import login, logout, register
 def cli(host, verbose):
     configure_logger(verbose)
 
+@click.group()
+@click.version_option(__version__)
+@click.pass_context
+def cli(ctx):
+    pass
 
-@click.command()
-def version(version=1.22):
-    """Get the version of the hub package"""
-    click.echo(f"you are using the hub version:{version}")
+
+
 
 
 def add_commands(cli):
     cli.add_command(login)
     cli.add_command(register)
     cli.add_command(logout)
-    cli.add_command(version)
+    cli.add_command(cli)
 
 
 add_commands(cli)
