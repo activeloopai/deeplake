101a178487436ecce02a1752c36dd2c0d3e738e7
sparkingdark <debomastet335@gmail.com>
2021-02-27 12:18:12 +0530

    add more detailed error message to authenticationexception

--- hub/exceptions.py
+++ hub/exceptions.py
@@ -79,7 +79,7 @@ class HubException(ClickException):
 
 
 class AuthenticationException(HubException):
-    def __init__(self, message="Authentication failed. Please login again."):
+    def __init__(self, message="Authentication failed. Please login again. If you don't have a account , you can register at https://app.activeloop.ai"):
         super().__init__(message=message)
 
 
