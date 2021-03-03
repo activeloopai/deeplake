25ff65698ed875f9c38d363791073a31330c3b42
benchislett <chislett.ben@gmail.com>
2021-03-02 22:45:47 -0500

    Refactor compression benchmarks to runnable design
    Plan: each benchmark will export two functions, filename_setup and
    filename_run, which designate how the benchmark is to be run. setup will
    take any required parameters and return a processed param tuple to be
    passed as argument to the runner. The runner is designed to be as slim
    as possible so we only measure the crucial code. Then, we can externally
    call these functions and time/profile/benchmark on the runtime of the
    function call, allowing for a great increase of control. Further
    refactors along this design coming soon.

---
+++ benchmarks/benchmark_compress_hub.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+
+import hub
+
+
+def benchmark_compress_hub_setup(times, image_path="./images/compression_benchmark_image.png"):
+    img = Image.open(image_path)
+    arr = np.array(img)
+    ds = hub.Dataset(
+        "./data/bench_png_compression",
+        mode="w",
+        shape=times,
+        schema={"image": hub.schema.Image(arr.shape, compressor="png")},
+    )
+
+    batch = np.zeros((times,) + arr.shape, dtype="uint8")
+    for i in range(times):
+        batch[i] = arr
+
+    return (ds, times, batch)
+
+
+def benchmark_compress_hub_run(params):
+    ds, times, batch = params
+    ds["image", :times] = batch
