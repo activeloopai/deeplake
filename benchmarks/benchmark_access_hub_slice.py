85ac89877af553bb0928f6f90622d726c1510460
benchislett <chislett.ben@gmail.com>
2021-03-02 23:16:22 -0500

    Refactor benchmark_random_access into new format
    Improves `benchmark_access_hub_full.py` and uses that as a base for
    `benchmark_access_hub_slice.py` which replaces functionality from
    `benchmark_random_access.py` (now deleted).

--- benchmarks/benchmark_access_hub_slice.py
+++ benchmarks/benchmark_access_hub_slice.py
@@ -1,16 +1,16 @@
 from hub import dataset
 
 
-def benchmark_access_hub_full_setup(dataset_name, field=None):
+def benchmark_access_hub_slice_setup(dataset_name, slice_bounds, field=None):
     dset = Dataset(dataset_name, cache=False, storage_cache=False, mode="r")
 
     keys = dset.keys
     if field is not None:
         keys = (field,)
-    return (dset, keys)
+    return (dset, slice_bounds, keys)
 
 
-def benchmark_access_hub_full_run(params):
-    dset, keys = params
+def benchmark_access_hub_slice_run(params):
+    dset, slice_bounds, keys = params
     for k in keys:
         dset[k][slice_bounds[0], slice_bounds[1]].compute()
