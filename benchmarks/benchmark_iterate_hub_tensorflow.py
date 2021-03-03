b4a49daf7508c6c08e6da4f24400410e08bcb826
benchislett <chislett.ben@gmail.com>
2021-03-02 23:29:30 -0500

    Remove unused line in benchmark_iterate_hub TF

--- benchmarks/benchmark_iterate_hub_tensorflow.py
+++ benchmarks/benchmark_iterate_hub_tensorflow.py
@@ -5,9 +5,6 @@ def benchmark_iterate_hub_tensorflow_setup(dataset_name, batch_size, prefetch_fa
     dset = Dataset(dataset_name, cache=False, storage_cache=False, mode="r")
 
     loader = dset.to_tensorflow().batch(batch_size).prefetch(prefetch_factor)
-    loader = torch.utils.data.DataLoader(dset.to_pytorch(
-    ), batch_size=batch_size, prefetch_factor=prefetch_factor, num_workers=num_workers)
-
     return (loader,)
 
 
