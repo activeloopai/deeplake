47f962e98afcc1fb9518bf36113f4ed18ab88ecd
McCrearyD <mccreary@dyllan.ai>
2021-03-16 21:10:57 -0700

    return shape

--- hub/schema/auto/infer.py
+++ hub/schema/auto/infer.py
@@ -1,14 +1,46 @@
 import os
+from glob import glob
+
+__all__ = ['infer_schema_and_shape']
 
 
-def infer_schema(path):
+def _find_root(path):
+    """
+    find the root of the dataset within the given path.
+    the "root" is defined as being the path to a subdirectory within path that has > 1 folder/file (if applicable).
+
+    in other words, if there is a directory structure like:
+    dataset -
+        Images -
+            class1 -
+                img.jpg
+                ...
+            class2 -
+                img.jpg
+                ...
+            ...
+
+    the output of this function should be "dataset/Images/" as that is the root.
+    """
+
+    subs = glob(os.path.join(path, '*'))
+    if len(subs) > 1:
+        return path
+    return _find_root(subs[0])
+
+
+def infer_schema_and_shape(path):
     if not os.path.isdir(path):
         raise Exception('input path must be either a directory or file')
 
+    root = _find_root(path)
+
     schema = {}
+    shape = (1, )  # TODO
 
     # TODO: determine input type
+    # does it match an image classification dataset?
 
     # TODO: determine label type
 
-    return schema
+    return schema, shape
