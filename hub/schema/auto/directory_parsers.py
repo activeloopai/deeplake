a229132fcd9f4e7df7d2ee677603422e3be425a2
McCrearyD <mccreary@dyllan.ai>
2021-03-16 22:42:56 -0700

    utils

--- hub/schema/auto/directory_parsers.py
+++ hub/schema/auto/directory_parsers.py
@@ -1,7 +1,31 @@
+import os
+from glob import glob
+
 __all__ = ['get_parsers']
 
 _parsers = []
 _priorities = []
+
+IMAGE_EXTS = ['.jpg', '.png', '.jpeg']
+
+
+def get_children(path):
+    """helper function to glob the given directory"""
+
+    return glob(os.path.join(path, '*'))
+
+
+def get_ext(path):
+    ext = os.path.splitext(path)[-1]
+    return ext.lower()
+
+
+def files_are_of_extension(path, allowed_extensions):
+    """helper function that checks if all files within the given directory have extensions that are allowed"""
+
+    allowed_extensions = [ext.lower() for ext in allowed_extensions]
+    children = get_children(path)
+    return all([get_ext(child) in allowed_extensions for child in children])
 
 
 def get_parsers(priority_sort=True):
@@ -13,14 +37,14 @@ def get_parsers(priority_sort=True):
 
 def directory_parser(priority=0):
     """
-    a directory parser function is a function that takes in a path & returns a schema. 
+    a directory parser function is a function that takes in a path & returns a schema.
     these functions make it easier to extend the schema infer domain. functions should
     be as general as possible.
 
     Parameters
     ----------
     priority: int
-        an arbitrary number that the parsers will be sorted by 
+        an arbitrary number that the parsers will be sorted by
         (lower the number = higher the priority)
     """
     def decorate(fn):
@@ -33,4 +57,15 @@ def directory_parser(priority=0):
 
 @directory_parser(priority=0)
 def image_classification(path):
+    children = get_children(path)
+
+    # check if all children are directories
+    if not all([os.path.isdir(child) for child in children]):
+        return None
+
+    # check if children's contents are all image files
+    for child in children:
+        print(child)
+        print(files_are_of_extension(child, IMAGE_EXTS))
+
     return None
