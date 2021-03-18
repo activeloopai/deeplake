c1e1d6386164cb50db86a4099687f43bce77438a
McCrearyD <mccreary@dyllan.ai>
2021-03-18 16:41:05 -0700

    run test & delete hub_dir

--- hub/auto/tests/test_image_classification.py
+++ hub/auto/tests/test_image_classification.py
@@ -1,9 +1,36 @@
 import json
 import os
+import shutil
 import zipfile
 from pathlib import PosixPath
 
 import hub
+
+
+def assert_conversion(kaggle_tag, hub_dir):
+    """tries to create a dataset for the kaggle_tag & then convert it into hub format."""
+
+    dataset_store = get_dataset_store(kaggle_tag)
+    hub_dir = dataset_store / hub_dir / "hub"
+
+    # delete hub dataset so conversion test can be done
+    if hub_dir.is_dir():
+        print("hub_dir was found (%s), deleting..." % hub_dir)
+        shutil.rmtree(hub_dir)
+
+    _download_kaggle(kaggle_tag, dataset_store)
+
+    try:
+        ds = hub.Dataset.from_path(dataset_store / "Images")
+    except Exception:
+        assert False
+
+    print("dataset obj:", ds)
+    assert ds is not None
+
+    assert hub_dir.is_dir(), hub_dir
+
+    # TODO: check if the hub dataset was properly uploaded
 
 
 def get_dataset_store(tag):
@@ -20,8 +47,8 @@ def _exec_command(command):
     assert out == 0
 
 
-def _download_kaggle(tag):
-    dataset_store = get_dataset_store(tag)
+def _download_kaggle(tag, dataset_store):
+
     username = os.environ.get("KAGGLE_USERNAME")
     key = os.environ.get("KAGGLE_KEY")
     print("got credentials %s@%s" % (username, key))
@@ -38,5 +65,4 @@ def _download_kaggle(tag):
 
 def test_image_classification():
     kaggle_tag = "coloradokb/dandelionimages"
-    _download_kaggle(kaggle_tag)
-    # ds = hub.Dataset.from_path(dataset_store / 'Images'
+    assert_conversion(kaggle_tag, "Images")
