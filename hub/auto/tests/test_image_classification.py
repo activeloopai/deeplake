c375c421f62607b5dc67f9e15ad6027a54b6507f
McCrearyD <mccreary@dyllan.ai>
2021-03-18 15:59:39 -0700

    setup kaggle downloader for tests

---
+++ hub/auto/tests/test_image_classification.py
@@ -0,0 +1,29 @@
+import json
+import os
+import zipfile
+from pathlib import PosixPath
+
+dataset_store = PosixPath("PYTEST_TMPDIR/datasets")
+
+
+def _download_kaggle(tag):
+    username = os.environ.get("KAGGLE_USERNAME")
+    key = os.environ.get("KAGGLE_KEY")
+    print("got credentials %s@%s" % (username, key))
+    print('downloading kaggle dataset "%s" to %s' % (tag, dataset_store))
+    os.makedirs(dataset_store, exist_ok=True)
+    command = (
+        "cd %s && export KAGGLE_USERNAME=%s && export KAGLE_KEY=%s && kaggle datasets download -d %s"
+        % (dataset_store, username, key, tag)
+    )
+    print("using command", command)
+    print("-----------------")
+    out = os.system(command)
+    print("-----------------")
+    print("exit code:", out)
+    assert out == 0
+
+
+def test_image_classification():
+    kaggle_tag = "coloradokb/dandelionimages"
+    _download_kaggle(kaggle_tag)
