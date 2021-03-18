c4b075ce225bf25e4cf1d51f22a28b5e40de05e7
McCrearyD <mccreary@dyllan.ai>
2021-03-18 16:10:21 -0700

    fixed kaggle downloading & unzip

--- hub/auto/tests/test_image_classification.py
+++ hub/auto/tests/test_image_classification.py
@@ -6,22 +6,28 @@ from pathlib import PosixPath
 dataset_store = PosixPath("PYTEST_TMPDIR/datasets")
 
 
-def _download_kaggle(tag):
-    username = os.environ.get("KAGGLE_USERNAME")
-    key = os.environ.get("KAGGLE_KEY")
-    print("got credentials %s@%s" % (username, key))
-    print('downloading kaggle dataset "%s" to %s' % (tag, dataset_store))
-    os.makedirs(dataset_store, exist_ok=True)
-    command = (
-        "cd %s && export KAGGLE_USERNAME=%s && export KAGLE_KEY=%s && kaggle datasets download -d %s"
-        % (dataset_store, username, key, tag)
-    )
+def _exec_command(command):
     print("using command", command)
     print("-----------------")
     out = os.system(command)
     print("-----------------")
     print("exit code:", out)
     assert out == 0
+
+
+def _download_kaggle(tag):
+    username = os.environ.get("KAGGLE_USERNAME")
+    key = os.environ.get("KAGGLE_KEY")
+    print("got credentials %s@%s" % (username, key))
+    print('downloading kaggle dataset "%s" to %s' % (tag, dataset_store))
+    os.makedirs(dataset_store, exist_ok=True)
+    setup = "cd %s && export KAGGLE_USERNAME=%s && export KAGLE_KEY=%s &&" % (
+        dataset_store,
+        username,
+        key,
+    )
+    _exec_command("%s kaggle datasets download -d %s" % (setup, tag))
+    _exec_command("%s unzip *.zip" % (setup))
 
 
 def test_image_classification():
