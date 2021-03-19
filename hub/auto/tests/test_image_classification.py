5804234e98fd17dae46f5b42779a09f8c1f3d87a
McCrearyD <mccreary@dyllan.ai>
2021-03-18 17:32:38 -0700

    check if credentials are None

--- hub/auto/tests/test_image_classification.py
+++ hub/auto/tests/test_image_classification.py
@@ -52,11 +52,13 @@ def _exec_command(command):
 
 
 def _download_kaggle(tag, dataset_store):
-
     username = os.environ.get("KAGGLE_USERNAME")
     key = os.environ.get("KAGGLE_KEY")
+    assert username is not None
+    assert key is not None
     print("got credentials %s@%s" % (username, key))
     print('downloading kaggle dataset "%s" to %s' % (tag, dataset_store))
+
     os.makedirs(dataset_store, exist_ok=True)
     setup = "cd %s && export KAGGLE_USERNAME=%s && export KAGLE_KEY=%s &&" % (
         dataset_store,
