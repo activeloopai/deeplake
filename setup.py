f688ec368f4aa5570daa7ed7d0369cdab53e9a7a
Abhinav Tuli <42538472+AbhinavTuli@users.noreply.github.com>
2021-03-26 16:39:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 from setuptools import find_packages, setup
 
 project = "hub"
-VERSION = "1.3.0"
+VERSION = "1.3.2"
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
