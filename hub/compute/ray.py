c27527874d0c246811b3ef122d87a80611d4d73b
kristinagrig06 <kristina@activeloop.ai>
2021-04-22 14:59:35 +0400

    Num cpus in init

--- hub/compute/ray.py
+++ hub/compute/ray.py
@@ -5,6 +5,7 @@ If a copy of the MPL was not distributed with this file, You can obtain one at h
 """
 
 import sys
+import psutil
 from hub import Dataset
 from hub.api.datasetview import DatasetView
 from hub.utils import batchify
@@ -47,8 +48,9 @@ class RayTransform(Transform):
         if "ray" not in sys.modules:
             raise ModuleNotInstalledException("ray")
 
+        num_cpus = psutil.cpu_count(logical=False)
         if not ray.is_initialized():
-            ray.init(local_mode=True)
+            ray.init(num_cpus=num_cpus, local_mode=True)
 
     @remote
     def _func_argd(_func, index, _ds, schema, kwargs):
