eba9c724a31b8456d49f422831fbc89317a773f2
Diveafall <d.isayan@gmail.com>
2021-04-30 11:15:46 +0400

    added code snippet source to file

--- docs/source/_static/js/gtm.js
+++ docs/source/_static/js/gtm.js
@@ -1,3 +1,4 @@
+/** Source: https://developers.google.com/tag-manager/quickstart */
 (function (w, d, s, l, i) {
   w[l] = w[l] || [];
   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
