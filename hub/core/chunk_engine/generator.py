f05dec7e7fecbc99ca9be2fe9e3398422616877e
McCrearyD <mccreary@dyllan.ai>
2021-05-01 17:06:01 -0700

    chunk generator & some tests

---
+++ hub/core/chunk_engine/generator.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+DEFAULT_CHUNK_SIZE = 16000000
+
+
+def _assert_valid_piece(piece, chunk_size):
+    assert len(piece) > 0
+    assert len(piece) <= chunk_size
+
+
+def chunk(content_bytes, previous_num_bytes, chunk_size):
+    """
+    Generator function that chunks bytes.
+
+    Args:
+        content_bytes(bytes): Bytes object with the data to be chunked.
+        previous_num_bytes(int): The number of bytes in the previous chunk. This is helpful for filling the previous chunk before starting a new one.
+        chunk_size(int): Each individual chunk will be assigned this many bytes maximum.
+
+    Yields:
+        Each yield is a chunk of the `content_bytes`. Each chunk is of length (0, `chunk_size`].
+    """
+
+    if len(content_bytes) <= 0:
+        return
+
+    if previous_num_bytes is None:
+        bytes_left = 0
+    else:
+        bytes_left = chunk_size - previous_num_bytes
+
+    content_num_bytes = len(content_bytes)
+
+    if bytes_left < 0:
+        # TODO place in exceptions.py
+        raise Exception(
+            "previous chunk exceeded chunk_size. %i > %i" % (bytes_left, chunk_size)
+        )
+
+    # handle filling the rest of the previous chunk
+    total_bytes_yielded = 0
+    if bytes_left > 0:
+        content_bytes_piece = content_bytes[:bytes_left]
+        _assert_valid_piece(content_bytes_piece, chunk_size)
+        yield content_bytes_piece, 0
+        total_bytes_yielded += bytes_left
+
+        if bytes_left > content_num_bytes:
+            # if the previous chunk still isn't filled, that means there is no more
+            # data to write
+            return
+
+    num_chunks_to_create = max(1, int(np.floor(content_num_bytes / chunk_size)))
+    start_chunk = 1
+
+    # handle full chunk bytes
+    for piece_index, relative_chunk_index in enumerate(
+        range(start_chunk, num_chunks_to_create + start_chunk)
+    ):
+        start = piece_index * chunk_size + bytes_left
+        end = (piece_index + 1) * chunk_size + bytes_left
+        content_bytes_piece = content_bytes[start:end]
+        if total_bytes_yielded >= content_num_bytes:
+            # prevents empty pieces being generated
+            break
+        _assert_valid_piece(content_bytes_piece, chunk_size)
+        yield content_bytes_piece, relative_chunk_index
+        total_bytes_yielded += len(content_bytes_piece)
+
+    # handle leftover bytes
+    num_leftover_bytes = content_num_bytes - total_bytes_yielded
+    if num_leftover_bytes < 0:
+        # TODO place in exceptions.py
+        raise Exception("leftover bytes should never be negative")
+
+    if num_leftover_bytes > 0:
+        leftover_bytes = content_bytes[end:]
+        _assert_valid_piece(leftover_bytes, chunk_size)
+        yield leftover_bytes, relative_chunk_index + 1
---
+++ hub/core/chunk_engine/tests/test_chunk_generator.py
@@ -0,0 +1,156 @@
+from hub.core.chunk_engine.generator import chunk
+from hub.core.chunk_engine.tests.util import (
+    make_dummy_byte_array,
+    get_random_chunk_size,
+    get_random_num_samples,
+    get_random_partial,
+)
+
+
+def test_perfect_fit():
+    trials = 10
+
+    for _ in range(trials):
+        chunk_size = get_random_chunk_size()
+        num_samples = get_random_num_samples()
+
+        content = make_dummy_byte_array(chunk_size * num_samples)
+        gen = chunk(content, previous_num_bytes=None, chunk_size=chunk_size)
+
+        piece_count = 0
+        for piece, relative_chunk_index in gen:
+            expected_start = piece_count * chunk_size
+            expected_end = (piece_count + 1) * chunk_size
+            expected_bytes = content[expected_start:expected_end]
+
+            assert (
+                piece_count + 1 == relative_chunk_index
+            ), "relative chunk index needs to start @ 1 since 1 means to create a new chunk"
+            assert expected_bytes == piece, "expected bytes don't match returned piece"
+
+            piece_count += 1
+
+        assert (
+            piece_count == num_samples
+        ), "perfect fit means the number of pieces generated should be equal to the number of samples"
+
+
+def test_first_partial_chunk():
+    trials = 10
+
+    for _ in range(trials):
+        chunk_size = get_random_chunk_size()
+
+        # divide by 2 so part 2 doesn't fill all the way
+        content_length = get_random_partial(chunk_size // 2)
+
+        assert content_length < (chunk_size // 2), "test invalid"
+
+        content = make_dummy_byte_array(content_length)
+        gen = chunk(content, previous_num_bytes=None, chunk_size=chunk_size)
+
+        piece_count = 0
+        for piece, relative_chunk_index in gen:
+            assert (
+                piece_count + 1 == relative_chunk_index
+            ), "relative chunk index needs to start @ 1 since 1 means to create a new chunk"
+            assert piece == content
+            piece_count += 1
+
+        assert piece_count == 1
+
+        # part 2: try to write to a chunk (the one created in the previous part)
+        # that has been partially filled. but this doesn't fill the chunk all the way
+        # it is still partial
+        # basically, the edge case is: (content_length + old_content_length) < chunk_size)
+        old_content_length = content_length
+        content_length = get_random_partial(chunk_size // 2)
+        assert old_content_length + content_length < chunk_size
+
+        content = make_dummy_byte_array(content_length)
+        gen = chunk(
+            content, previous_num_bytes=old_content_length, chunk_size=chunk_size
+        )
+
+        piece_count = 0
+        for piece, relative_chunk_index in gen:
+            assert (
+                piece_count == relative_chunk_index
+            ), "relative chunk index needs to start @ 0 since 0 means to append to the previous chunk"
+            assert piece == content
+            piece_count += 1
+
+        assert piece_count == 1
+
+
+def test_nth_partial_chunk():
+    trials = 10
+
+    for _ in range(trials):
+        chunk_size = get_random_chunk_size()
+        n = get_random_num_samples()
+        partial_length = get_random_partial(chunk_size)
+        content_length = partial_length + (chunk_size * n)
+        assert partial_length < chunk_size, "test invalid"
+        assert content_length % chunk_size != 0
+
+        content = make_dummy_byte_array(content_length)
+        gen = chunk(content, previous_num_bytes=None, chunk_size=chunk_size)
+
+        piece_count = 0
+        previous_num_bytes = None
+        for piece, relative_chunk_index in gen:
+            expected_start = piece_count * chunk_size
+            expected_end = (piece_count + 1) * chunk_size
+            expected_bytes = content[expected_start:expected_end]
+
+            assert (
+                piece_count + 1 == relative_chunk_index
+            ), "relative chunk index needs to start @ 1 since 1 means to create a new chunk"
+            assert piece == expected_bytes, (
+                "piece # %i doesn't match expected content" % piece_count
+            )
+
+            previous_num_bytes = len(piece)
+            piece_count += 1
+
+        assert piece_count == n + 1  # plus 1 because partial
+        assert previous_num_bytes == partial_length, "invalid test"
+
+        # part 2: need to add more content after the nth partial
+        old_partial_length = partial_length
+        old_n = n
+        n = get_random_num_samples()
+        partial_length = get_random_partial(chunk_size)
+        content_length = partial_length + (chunk_size * n)
+        assert partial_length < chunk_size, "test invalid"
+        assert content_length % chunk_size != 0
+
+        content = make_dummy_byte_array(content_length)
+        gen = chunk(
+            content, previous_num_bytes=previous_num_bytes, chunk_size=chunk_size
+        )
+
+        piece_count = 0
+        bytes_left = chunk_size - previous_num_bytes
+        is_first = True
+        for piece, relative_chunk_index in gen:
+            if is_first:
+                expected_bytes = content[:bytes_left]
+            else:
+                expected_start = (piece_count - 1) * chunk_size + bytes_left
+                expected_end = (piece_count) * chunk_size + bytes_left
+                expected_bytes = content[expected_start:expected_end]
+
+            assert (
+                piece_count == relative_chunk_index
+            ), "relative chunk index needs to start @ 0 since 0 means to append to the previous chunk"
+            assert piece == expected_bytes, (
+                "piece # %i doesn't match expected content" % piece_count
+            )
+
+            piece_count += 1
+            is_first = False
+
+        extra_chunks = 1 if partial_length + old_partial_length <= chunk_size else 2
+        assert piece_count == n + extra_chunks
