6be285f521816fc96cb781eef94ae55a17e28fed
benchislett <chislett.ben@gmail.com>
2021-05-03 11:10:46 -0400

    Initialize util folder for generic helpers
    Current addition includes a placeholder for composing python
    `slice` objects

---
+++ hub/util/slice.py
@@ -0,0 +1,12 @@
+def merge_slices(existing_slice: slice, new_slice: slice) -> slice:
+    """ 
+    Return a slice representing the composition of the given slices.
+
+    Parameters
+    ----------
+    existing_slice:
+        The existing slice on the object being updated.
+    new_slice:
+        The new slice to be applied to the existing slice.
+    """
+    return existing_slice  # TODO: implement this
