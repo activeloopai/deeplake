06d507642d97696dc780891da792d290ab1f49f1
benchislett <chislett.ben@gmail.com>
2021-05-05 16:55:02 -0400

    Update merge_slices tests to use pytest-cases

--- hub/util/tests/test_slice.py
+++ hub/util/tests/test_slice.py
@@ -1,46 +1,31 @@
 from hub.util.slice import merge_slices
+from pytest_cases import parametrize_with_cases
 
 
-RANGE = range(100)
+class MergeSlicesCases:
+    def case_first_longer(self):
+        return slice(0, 4), slice(1, 3)
+
+    def case_second_longer(self):
+        return slice(0, 4), slice(1, 7)
+
+    def case_start_none(self):
+        return slice(None, 4), slice(1, 7)
+
+    def case_stop_first_none(self):
+        return slice(1, None), slice(3, 7)
+
+    def case_stop_second_none(self):
+        return slice(2, 5), slice(1, None)
+
+    def case_stop_both_none(self):
+        return slice(1, None), slice(2, None)
+
+    def case_irregular_steps(self):
+        return slice(4, 101, 2), slice(10, 20, 3)
 
 
-def test_merge_slices_first_longer():
-    first = slice(0, 4)
-    second = slice(1, 3)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
-
-
-def test_merge_slices_second_longer():
-    first = slice(0, 4)
-    second = slice(1, 7)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
-
-
-def test_merge_slices_start_none():
-    first = slice(None, 4)
-    second = slice(1, 7)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
-
-
-def test_merge_slices_stop_both_none():
-    first = slice(1, None)
-    second = slice(2, None)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
-
-
-def test_merge_slices_stop_first_none():
-    first = slice(1, None)
-    second = slice(3, 7)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
-
-
-def test_merge_slices_stop_second_none():
-    first = slice(2, 5)
-    second = slice(1, None)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
-
-
-def test_merge_slices_irregular_steps():
-    first = slice(4, 101, 2)
-    second = slice(10, 20, 3)
-    assert RANGE[first][second] == RANGE[merge_slices(first, second)]
+@parametrize_with_cases('first,second', cases=MergeSlicesCases)
+def test_merge_slices(first: slice, second: slice):
+    r = range(100)
+    assert r[first][second] == r[merge_slices(first, second)]
