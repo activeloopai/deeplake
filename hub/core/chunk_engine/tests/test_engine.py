812b6f12b2269645792a86a6435763823804de70
McCrearyD <mccreary@dyllan.ai>
2021-05-06 11:55:29 -0700

    add some batched tests

--- hub/core/chunk_engine/tests/test_engine.py
+++ hub/core/chunk_engine/tests/test_engine.py
@@ -1,13 +1,40 @@
 import numpy as np
 from hub.core.chunk_engine import read, chunk_and_write_array, MemoryProvider
 
+import pytest
+
 dummy_compressor = lambda x: x
 dummy_decompressor = lambda x: x
 
 chunk_size = 10
 
 
-def run_test(storage, a_in, cache_chain=[]):
+# shape,batched
+ARRAY_SPECS = (
+    # unbatched
+    ((1,), False),
+    ((100,), False),
+    ((100, 100), False),
+    ((100, 100, 3), False),
+    ((3, 100, 100), False),
+    ((3, 100, 100, 1), False),
+    # batched
+    ((1, 1), True),
+    ((10, 1), True),
+    ((1, 100), True),
+    ((10, 100), True),
+    ((1, 100, 100), True),
+    ((10, 100, 100), True),
+    ((1, 100, 100, 3), True),
+    ((10, 100, 100, 3), True),
+    ((1, 3, 100, 100), True),
+    ((10, 3, 100, 100), True),
+    ((1, 3, 100, 100, 1), True),
+    ((10, 3, 100, 100, 1), True),
+)
+
+
+def run_test(storage, a_in, batched, cache_chain=[]):
     chunk_and_write_array(
         a_in,
         "tensor",
@@ -15,7 +42,7 @@ def run_test(storage, a_in, cache_chain=[]):
         chunk_size,
         storage,
         cache_chain,
-        batched=False,
+        batched=batched,
     )
 
     for cache in cache_chain:
@@ -23,22 +50,22 @@ def run_test(storage, a_in, cache_chain=[]):
             len(cache.mapper.keys()) == 0
         ), "write_array(...) should implicitly flush the cache (clear & migrate everything to storage)"
 
-    print(storage.mapper.keys())
-
     a_out = read("tensor", 0, dummy_decompressor, storage, cache_chain)
     np.testing.assert_array_equal(a_in, a_out)
 
 
-def test_no_cache():
+@pytest.mark.parametrize("shape,batched", ARRAY_SPECS)
+def test_no_cache(shape, batched):
     storage = MemoryProvider()
-    a_in = np.random.uniform(size=(100))
+    a_in = np.random.uniform(size=shape)
 
-    run_test(storage, a_in)
+    run_test(storage, a_in, batched=batched)
 
 
-def test_with_cache_chain():
+@pytest.mark.parametrize("shape,batched", ARRAY_SPECS)
+def test_with_cache_chain(shape, batched):
     storage = MemoryProvider()
     cache_chain = [MemoryProvider()]
 
-    a_in = np.random.uniform(size=(100))
-    run_test(storage, a_in, cache_chain=cache_chain)
+    a_in = np.random.uniform(size=shape)
+    run_test(storage, a_in, batched=batched, cache_chain=cache_chain)
