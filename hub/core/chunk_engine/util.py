4369cde292bc43a352e630e25635d3db859cf613
McCrearyD <mccreary@dyllan.ai>
2021-05-06 15:50:07 -0700

    add `normalize_and_batchify` util with tests

--- hub/core/chunk_engine/util.py
+++ hub/core/chunk_engine/util.py
@@ -9,3 +9,14 @@ def array_to_bytes(array: np.ndarray) -> bytes:
 def index_map_entry_to_bytes(map_entry: dict):
     # TODO: don't use pickle, encode `map_entry` into array
     return pickle.dumps(map_entry)
+
+
+def normalize_and_batchify_shape(array: np.ndarray, batched: bool) -> np.ndarray:
+    # if the first axis is of length 1, but batched is true, it is only a single sample & squeeze will remove it
+    actually_batched = batched and array.shape[0] != 1
+    array = array.squeeze()
+    if not actually_batched:
+        array = np.expand_dims(array, axis=0)
+    if len(array.shape) == 1:
+        array = np.expand_dims(array, axis=0)
+    return array
