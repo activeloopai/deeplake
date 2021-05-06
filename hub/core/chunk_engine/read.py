4e4cc9fc0ce5034fc85efb2204bf319cf5c1f48e
McCrearyD <mccreary@dyllan.ai>
2021-05-06 15:11:17 -0700

    read from cache before storage & check for _incomplete chunks

--- hub/core/chunk_engine/read.py
+++ hub/core/chunk_engine/read.py
@@ -47,8 +47,10 @@ def read(
         for chunk_index in range(start_chunk, end_chunk + 1):
             # TODO read from caches first
             chunk_key = os.path.join(key, ("c%i" % chunk_index))
-            chunk = read_from_storage(chunk_key, storage)
-            decompressed_chunk = decompressor(chunk)
+
+            decompressed_chunk = read_decompressed_bytes_with_caching(
+                chunk_key, cache_chain, storage, decompressor, check_incomplete=True
+            )
             b.extend(decompressed_chunk)
 
         a = np.frombuffer(b, dtype=dtype)
@@ -61,19 +63,58 @@ def read(
     return samples
 
 
-def read_from_cache(key: str, cache_chain: List[MemoryProvider]) -> bool:
+def read_decompressed_bytes_with_caching(
+    key, cache_chain, storage, decompressor, check_incomplete=False
+):
+    if len(cache_chain) <= 0:
+        # TODO: move into exceptions.py
+        raise Exception("At least one cache inside of `cache_chain` is required.")
+
+    b = read_and_decompress_from_cache(
+        key, cache_chain, decompressor, check_incomplete=check_incomplete
+    )
+
+    if b is None:
+        b = read_and_decompress_from_storage(
+            key, storage, decompressor, check_incomplete=check_incomplete
+        )
+
+    return b
+
+
+def read_and_decompress_from_cache(
+    key: str, cache_chain: List[MemoryProvider], decompressor, check_incomplete=False
+) -> bool:
     # try to read key from cache, return data if success, else None
 
+    # TODO: move "_incomplete" to util
+    incomplete_key = key + "_incomplete"
+
     # TODO: cross-cache storage (maybe the data doesn't fit in 1 cache, should we do so partially?)
-    for cache_provider in cache_chain:
-        try:
-            data = cache[key]
-            return data
-        except:
-            pass
+    for cache in cache_chain:
+        if key in cache.mapper:
+            return decompressor(cache[key])
+        if check_incomplete and incomplete_key in cache.mapper:
+            # incomplete is not compressed
+            return cache[incomplete_key]
 
     return None
 
 
-def read_from_storage(key: str, storage: MemoryProvider):
-    return storage[key]
+def read_and_decompress_from_storage(
+    key: str, storage: MemoryProvider, decompressor, check_incomplete=False
+):
+    if key in storage.mapper:
+        return decompressor(storage[key])
+
+    incomplete_key = key + "_incomplete"
+    if check_incomplete and incomplete_key in storage.mapper:
+        # incomplete is not compressed
+        # TODO: move "_incomplete" to util
+        return storage[incomplete_key]
+
+    # TODO: move to exceptions.py
+    keys = [key]
+    if check_incomplete:
+        keys.append(incomplete_key)
+    raise Exception("Could not find key(s) `%s` in storage." % str(keys))
