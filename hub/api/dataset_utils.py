9c01eb823dcc452d81cb46f79d89cd2f860536c8
kristinagrig06 <kristina@activeloop.ai>
2021-05-07 18:07:01 +0400

    Add __get_compressor function

---
+++ hub/api/dataset_utils.py
@@ -0,0 +1,39 @@
+import numcodecs
+from codecs import PngCodec, Lz4, NumPy, Zstd, JpegCodec
+
+
+def _get_compressor(compressor: str, **kwargs):
+    """Get compressor object
+
+    Example:
+        compressor = _get_compressor('lz4', acceleration=2)
+
+    Args:
+        compressor (str): lowercase name of the required compressor
+
+    Returns:
+        Codec object providing compression
+
+    Raises:
+        ValueError: if the name of compressor isn't in ['lz4', 'zstd', 'numpy', 'png', 'jpeg']
+    """
+    if compressor is None:
+        return None
+    elif compressor.lower() == "lz4":
+        acceleration = kwargs.get("acceleration", numcodecs.lz4.DEFAULT_ACCELERATION)
+        return Lz4(acceleration)
+    elif compressor.lower() == "zstd":
+        level = kwargs.get("level", numcodecs.zstd.DEFAULT_CLEVEL)
+        return Zstd(level)
+    elif compressor.lower() == "numpy":
+        return NumPy()
+    elif compressor.lower() == "png":
+        single_channel = kwargs.get("single_channel", True)
+        return PngCodec(single_channel=single_channel)
+    elif compressor.lower() == "jpeg":
+        single_channel = kwargs.get("single_channel", True)
+        return JpegCodec(single_channel=single_channel)
+    else:
+        raise ValueError(
+            f"Wrong compressor: {compressor}, only LZ4, PNG and ZSTD are supported"
+        )
