7d19b7b04c023d924b02835a795475d2266156b9
McCrearyD <mccreary@dyllan.ai>
2021-05-10 13:08:21 -0700

    remove generator of decompressed chunks

--- hub/core/chunk_engine/read.py
+++ hub/core/chunk_engine/read.py
@@ -42,44 +42,35 @@ def read_array(
     length = meta["length"]
 
     samples = []
-    all_same_shape = True
-    last_shape = None
 
     for index_entry in index_map[index]:
         shape = index_entry["shape"]
 
-        # TODO: make this more concise
-        if last_shape is not None and last_shape != shape:
-            all_same_shape = False
+        chunks = []
+        for chunk_name in index_entry["chunk_names"]:
+            chunk_key = os.path.join(key, "chunks", chunk_name)
+            raw_chunk = storage[chunk_key]
 
-        # TODO: put this in a separate function/class, ideally that caches decompressed chunks
-        def decompressed_chunks_generator():
-            for chunk_name in index_entry["chunk_names"]:
-                chunk_key = os.path.join(key, "chunks", chunk_name)
-                raw_chunk = storage[chunk_key]
-
-                if compression.subject == "chunk":
-                    if chunk_name in index_entry["incomplete_chunk_names"]:
-                        chunk = raw_chunk
-                    else:
-                        # TODO: different `meta["version"]`s may have different compressor maps
-                        chunk = compression.decompress(raw_chunk)
-                else:
+            if compression.subject == "chunk":
+                if chunk_name in index_entry["incomplete_chunk_names"]:
                     chunk = raw_chunk
+                else:
+                    # TODO: backwards compatibility: different `meta["version"]`s may have different compressor maps
+                    # TODO: cache decompressed chunks
+                    chunk = compression.decompress(raw_chunk)
+            else:
+                chunk = raw_chunk
 
-                yield chunk
+            chunks.append(chunk)
 
         b = unchunk(
-            list(decompressed_chunks_generator()),
+            chunks,
             index_entry["start_byte"],
             index_entry["end_byte"],
         )
 
         a = np.frombuffer(b, dtype=dtype)
-        last_shape = shape
         samples.append(a.reshape(shape))
 
-    if all_same_shape:
-        return np.array(samples, dtype=dtype)
-
-    return samples
+    # TODO: dynamic shapes
+    return np.array(samples, dtype=dtype)
