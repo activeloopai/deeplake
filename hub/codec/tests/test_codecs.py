709b7c771f57f9534fbcb574ddb002d1b5e2da54
kristinagrig06 <kristina@activeloop.ai>
2021-05-10 16:25:56 +0400

    Parametrize tests

--- hub/codec/tests/test_codecs.py
+++ hub/codec/tests/test_codecs.py
@@ -1,19 +1,38 @@
+from typing import Union
 import pytest
 import numpy as np
 
-from hub.codec import PngCodec, NumPy, Lz4, Zstd, JpegCodec
+from hub.codec import NumPy, Lz4, Zstd, JpegCodec, PngCodec
+
+IMG_ARRAY_SHAPES = ((30, 30, 3), (2, 30, 30, 3), (30, 30, 1))
+GENERIC_ARRAY_SHAPES = ((20, 1), (1, 20), ((60, 60)))
+ARRAY_DTYPES = (
+    "uint8",
+    "uint16",
+    "float16",
+    "float32",
+)
+LZ4_ACCELERATIONS = (1, 50, 100)
+ZSTD_LEVELS = (1, 11, 22)
+
+
+def check_equals_decoded(
+    actual_array: np.ndarray, codec: Union[PngCodec, NumPy, Lz4, Zstd, JpegCodec]
+):
+    bytes_ = codec.encode(actual_array)
+    decoded_array = codec.decode(bytes_)
+    assert (actual_array == decoded_array).all()
 
 
 @pytest.mark.parametrize("from_config", [False, True])
-def test_png_codec(from_config: bool) -> None:
+@pytest.mark.parametrize("shape", IMG_ARRAY_SHAPES)
+def test_png_codec(from_config: bool, shape: tuple) -> None:
     codec = PngCodec()
     if from_config:
         config = codec.get_config()
         codec = PngCodec.from_config(config)
-    arr = np.ones((1000, 1000, 3), dtype="uint8")
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert (arr == arr_).all()
+    arr = np.ones(shape, dtype="uint8")
+    check_equals_decoded(arr, codec)
 
 
 @pytest.mark.parametrize("single_channel", [False, True])
@@ -28,24 +47,21 @@ def test_png_codec_config(single_channel: bool) -> None:
 def test_png_codec_single_channel(single_channel: bool) -> None:
     codec = PngCodec(single_channel)
     if single_channel:
-        arr = np.ones((1000, 1000, 1), dtype="uint8")
+        arr = np.ones((100, 100, 1), dtype="uint8")
     else:
-        arr = np.ones((1000, 1000), dtype="uint8")
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert (arr == arr_).all()
+        arr = np.ones((100, 100), dtype="uint8")
+    check_equals_decoded(arr, codec)
 
 
 @pytest.mark.parametrize("from_config", [False, True])
-def test_jpeg_codec(from_config: bool) -> None:
+@pytest.mark.parametrize("shape", IMG_ARRAY_SHAPES)
+def test_jpeg_codec(from_config: bool, shape: tuple) -> None:
     codec = JpegCodec()
     if from_config:
         config = codec.get_config()
         codec = JpegCodec.from_config(config)
-    arr = np.ones((1000, 1000, 3), dtype="uint8")
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert (arr == arr_).all()
+    arr = np.ones(shape, dtype="uint8")
+    check_equals_decoded(arr, codec)
 
 
 @pytest.mark.parametrize("single_channel", [False, True])
@@ -60,33 +76,30 @@ def test_jpeg_codec_config(single_channel: bool) -> None:
 def test_jpeg_codec_single_channel(single_channel: bool) -> None:
     codec = JpegCodec(single_channel)
     if single_channel:
-        arr = np.ones((1000, 1000, 1), dtype="uint8")
+        arr = np.ones((100, 100, 1), dtype="uint8")
     else:
-        arr = np.ones((1000, 1000), dtype="uint8")
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert (arr == arr_).all()
+        arr = np.ones((100, 100), dtype="uint8")
+    check_equals_decoded(arr, codec)
 
 
-def test_lz4() -> None:
-    codec = Lz4(acceleration=3)
-    arr = np.random.rand(3, 100)
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert np.all(arr == arr_)
+@pytest.mark.parametrize("acceleration", LZ4_ACCELERATIONS)
+@pytest.mark.parametrize("shape", GENERIC_ARRAY_SHAPES)
+def test_lz4(acceleration: int, shape: tuple) -> None:
+    codec = Lz4(acceleration=acceleration)
+    arr = np.random.rand(*shape)
+    check_equals_decoded(arr, codec)
 
 
-def test_numpy() -> None:
+@pytest.mark.parametrize("shape", GENERIC_ARRAY_SHAPES)
+def test_numpy(shape: tuple) -> None:
     codec = NumPy()
-    arr = np.random.rand(3, 100)
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert np.all(arr == arr_)
+    arr = np.random.rand(*shape)
+    check_equals_decoded(arr, codec)
 
 
-def test_zstd() -> None:
-    codec = Zstd(level=1)
-    arr = np.random.rand(3, 100)
-    bytes_ = codec.encode(arr)
-    arr_ = codec.decode(bytes_)
-    assert np.all(arr == arr_)
+@pytest.mark.parametrize("level", ZSTD_LEVELS)
+@pytest.mark.parametrize("shape", GENERIC_ARRAY_SHAPES)
+def test_zstd(level: int, shape: tuple) -> None:
+    codec = Zstd(level=level)
+    arr = np.random.rand(*shape)
+    check_equals_decoded(arr, codec)
