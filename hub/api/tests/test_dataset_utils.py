b403058d21aef9c993b1a0eb2e19611416257d6c
kristinagrig06 <kristina@activeloop.ai>
2021-05-11 14:08:55 +0400

    Add get_compressor tests

---
+++ hub/api/tests/test_dataset_utils.py
@@ -0,0 +1,16 @@
+import pytest
+from hub.api.dataset_utils import _get_compressor
+
+CODECS = ("lz4", "zstd", "numpy", "png", "jpeg")
+
+
+@pytest.mark.parametrize("codec_name", CODECS)
+def test_get_compressor(codec_name: str) -> None:
+    compressor = _get_compressor(codec_name)
+    assert compressor.__name__ == codec_name
+
+
+def test_unavailable_codec():
+    codec_name = "zip"
+    with pytest.raises(ValueError):
+        _get_compressor(codec_name)
