1db82dee59320e6ebda3955a50dabba11b35c54c
McCrearyD <mccreary@dyllan.ai>
2021-05-12 18:24:19 -0700

    fixed list failure when key empty

--- hub/core/storage/s3/s3_mapper.py
+++ hub/core/storage/s3/s3_mapper.py
@@ -115,6 +115,8 @@ class S3Mapper(MutableMapping):
         try:
             # TODO boto3 list_objects only returns first 1000 objects
             items = self.client.list_objects_v2(Bucket=self.bucket, Prefix=self.path)
+            if items["KeyCount"] <= 0:
+                return []
             items = items["Contents"]
             names = [item["Key"] for item in items]
             # removing the prefix from the names
