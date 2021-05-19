e91cb0e25bb60386d967f2a3f617e1e642c19722
benchislett <chislett.ben@gmail.com>
2021-05-18 20:35:30 -0400

    Linting fixes

--- hub/util/path.py
+++ hub/util/path.py
@@ -5,6 +5,13 @@ import os
 def local_provider_from_path(path: str):
     """Construct a LocalProvider if given a valid local path, or return None
 
+    Arguments:
+        path (str): Path that may or may not indicate a local directory
+
+    Returns:
+        If the given path could point to a directory, return the LocalProvider.
+        If the given path does not look like a local path, return None.
+
     Raises:
         ValueError: If the given path is a local path to a file
     """
