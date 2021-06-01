aa98425c141c8897f256c3f74b17b53637df1db7
McCrearyD <mccreary@dyllan.ai>
2021-06-01 16:53:03 -0700

    implement failure test cases for tensor

--- hub/core/tests/test_tensor_failures.py
+++ hub/core/tests/test_tensor_failures.py
@@ -5,7 +5,9 @@ from hub.core.meta.tensor_meta import tensor_meta_from_array
 from hub.core.tensor import add_samples_to_tensor, create_tensor
 
 from hub.tests.common import TENSOR_KEY
-from hub.util.exceptions import TensorMetaMismatchError
+from hub.util.exceptions import TensorAlreadyExistsError, TensorDoesNotExistError, TensorMetaInvalidValue, TensorMetaMismatchError
+
+
 
 
 @pytest.mark.xfail(raises=TensorMetaMismatchError, strict=True)
@@ -26,6 +28,26 @@ def test_shape_length_mismatch(memory_storage):
     add_samples_to_tensor(a2, TENSOR_KEY, memory_storage, batched=False)
 
 
-# TODO: failure case where `create_tensor` is not used
-# TODO: failure case where `create_tensor` is used twice for same key
-# TODO: failure case where `chunk_size <= 0`
+@pytest.mark.xfail(raises=TensorDoesNotExistError, strict=True)
+def test_tensor_does_not_exist(memory_storage):
+    a1 = np.arange(10)
+    add_samples_to_tensor(a1, TENSOR_KEY, memory_storage, batched=False)
+
+
+@pytest.mark.xfail(raises=TensorAlreadyExistsError, strict=True)
+def test_tensor_already_exists(memory_storage):
+    a1 = np.arange(10)
+    create_tensor(TENSOR_KEY, memory_storage, tensor_meta_from_array(a1, batched=False))
+    create_tensor(TENSOR_KEY, memory_storage, tensor_meta_from_array(a1, batched=False))
+
+
+@pytest.mark.xfail(raises=TensorMetaInvalidValue, strict=True)
+@pytest.mark.parametrize("chunk_size", [0, -1, -100])
+def test_invalid_chunk_sizes(memory_storage, chunk_size):
+    create_tensor(TENSOR_KEY, memory_storage, {"dtype": "int", "chunk_size": chunk_size})
+
+
+@pytest.mark.xfail(raises=TensorMetaInvalidValue, strict=True)
+@pytest.mark.parametrize("dtype", [1, False, "floatf", "intj", "foo", "bar"])
+def test_invalid_dtypes(memory_storage, dtype):
+    create_tensor(TENSOR_KEY, memory_storage, {"dtype": dtype, "chunk_size": 4})
