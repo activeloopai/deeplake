a93257fecc7d4b1371848de071f24f8ff7ed9beb
McCrearyD <mccreary@dyllan.ai>
2021-06-07 23:43:12 -0700

    implement basic image classifiaction ingestion

--- hub/auto/converter.py
+++ hub/auto/converter.py
@@ -1,14 +1,19 @@
-from hub.util.path import find_root
+import numpy as np
 from pathlib import Path
 import os
 import glob
 from typing import Dict, List, Sequence, Tuple
 
+from tqdm import tqdm
+
 from hub.api.dataset import Dataset
 from hub.auto.load import load
+from hub.util.path import find_root
 
 
 IMAGES_TENSOR_NAME = "images"
+LABELS_TENSOR_NAME = "labels"
+LABEL_NAMES_META_KEY = "class_names"
 
 
 def _get_file_paths(directory: Path) -> Sequence[str]:
@@ -38,12 +43,21 @@ class Converter:
         return tuple(class_names)
 
 
-    def from_image_classification(self, ds: Dataset):
+    def from_image_classification(self, ds: Dataset, use_tqdm: bool=False):
         class_names = self.get_class_names()
 
-        for file_path in self._file_paths:
+        ds.create_tensor(IMAGES_TENSOR_NAME, htype="image")
+        ds.create_tensor(LABELS_TENSOR_NAME, htype="class_label", extra_meta={LABEL_NAMES_META_KEY: class_names})
+
+        iterator = tqdm(self._file_paths, desc="Ingesting image classification dataset", total=len(self._file_paths), disable=not use_tqdm)
+        for file_path in iterator:
             image = load(file_path, symbolic=False)
             class_name = _class_name_from_path(file_path)
-            label = class_names.index(class_name)
-            print(class_name, label, image.shape)
+            label = np.array([class_names.index(class_name)])  # TODO: should be able to pass just an integer to `tensor.append`
+
+            ds[IMAGES_TENSOR_NAME].append(image)
+            ds[LABELS_TENSOR_NAME].append(label)
+
+        ds.flush()
+        return ds
 
--- hub/auto/tests/test_ingestion.py
+++ hub/auto/tests/test_ingestion.py
@@ -5,4 +5,8 @@ from hub.auto.tests.common import get_dummy_data_path
 def test_image_classification():
     path = get_dummy_data_path("image_classification")
     ds = from_path(path)
+
     assert ds.keys() == ("images", "labels")
+    assert ds.images.numpy().shape == (3, 900, 900, 3)
+    assert ds.labels.numpy().shape == (3, 1)
+    assert ds.labels.meta["class_names"] == ("class0", "class1", "class2")
