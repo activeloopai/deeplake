56b5557f67b034060035e8dce9a941a7ccbbaf17
McCrearyD <mccreary@dyllan.ai>
2021-06-09 09:45:16 -0700

    update docstrings

--- hub/auto/ingest.py
+++ hub/auto/ingest.py
@@ -9,42 +9,45 @@ from hub.util.exceptions import KaggleDatasetAlreadyDownloadedError
 import warnings
 
 
-DEFAULT_LOCAL_PATH = "./datasets/"
-
-HUB_DATASET_SUBDIR = "hub"
-UNSTRUCTURED_DATASET_SUBDIR = "unstructured"
-
-
 def _dataset_has_tensors(**kwargs):
     ds = Dataset(**kwargs, mode="r")
     return len(ds.keys()) > 0
 
 
-def _warn_kwargs(**kwargs):
+def _warn_kwargs(caller: str, **kwargs):
     if _dataset_has_tensors(**kwargs):
         warnings.warn("Dataset already exists, skipping ingestion and returning a read-only Dataset.")
         return  # no other warnings should print
 
     if "mode" in kwargs:
-        warnings.warn("Mode should not be passed to `Dataset.from_path`. Using write mode.")
+        warnings.warn("Argument `mode` should not be passed to `%s`. Ignoring and using `mode=\"write\"`." % caller)
+
+    if "path" in kwargs:
+        # TODO: generalize warns
+        warnings.warn("Argument `path` should not be passed to `%s`. Ignoring and using `destination`." % caller)
 
 
-def from_path(unstructured_path: str, **kwargs):
-    """Creates a hub dataset from unstructured data.
+def from_path(source: str, destination: str, **kwargs):
+    """Copies unstructured data from `source` and structures/sends it to `destination`.
 
     Note:
-        This copies the data into hub format.
-        Be careful when using this with large datasets.
+        Be careful when providing sources to large datasets!
+        This method copies data from `source` to `destination`.
+        To be safe, you should assume the size of your dataset will consume 3-5x more space than expected.
 
     Args:
-        path (str): Path to the data to be converted
+        source (str): Path to where the unstructured dataset is stored.
+        destination (str): Path to where the structured data will be stored.
+        **kwargs: Args will be passed into `hub.Dataset`.
 
     Returns:
-        A Dataset instance whose path points to the hub formatted
-        copy of the data.
+        A read-only `hub.Dataset` instance pointing to the structured data.
     """
 
-    _warn_kwargs(**kwargs)
+    # TODO: test that ingestion methods ALWAYS return read-only datasets.
+
+    _warn_kwargs("from_path", **kwargs)
+    kwargs["path"] = destination
 
     # TODO: check for incomplete ingestion
     # TODO: try to resume progress for incomplete ingestion
@@ -53,7 +56,7 @@ def from_path(unstructured_path: str, **kwargs):
         return Dataset(**kwargs, mode="r")
 
     ds = Dataset(**kwargs, mode="w")
-    converter = Converter(unstructured_path)
+    converter = Converter(source)
     converter.from_image_classification(ds)
 
     # TODO: opt-in delete unstructured data after ingestion
@@ -61,44 +64,40 @@ def from_path(unstructured_path: str, **kwargs):
     return Dataset(**kwargs, mode="r")
 
 
-def from_kaggle(tag: str, path: str=None, local_path: str=DEFAULT_LOCAL_PATH, kaggle_credentials: dict={}, **kwargs):
-    # TODO: docstring
-    # TODO: make sure path and local path are not equal
-    # TODO: make path variable names more obvious
+def from_kaggle(tag: str, source: str, destination: str, kaggle_credentials: dict={}, **kwargs):
 
-    """Downloads a kaggle dataset and creates a new hub dataset. Calls `from_path` on the kaggle dataset.
+    """Downloads the kaggle dataset with the given `tag` to this local machine, then that data is structured and copied into `destination`.
 
     Note:
-        This downloads data from kaggle and then copies it into hub, be careful when providing tags to large datasets.
+        Be careful when providing tags to large datasets!
+        This method downloads data from kaggle to the calling machine's local storage.
+        To be safe, you should assume the size of the kaggle dataset being downloaded will consume 3-5x more space than expected.
 
     Args:
         tag (str): Kaggle dataset tag. Example: `"coloradokb/dandelionimages"` points to https://www.kaggle.com/coloradokb/dandelionimages
-        path (str): Path to where the hub dataset will be uploaded. Passed into `hub.Dataset(path=path)`. See `hub.Dataset` for more information.
-            If not provided, `path` will be set to `DEFAULT_LOCAL_PATH`/`tag`/`HUB_DATASET_SUBDIR`.
-        local_path (str): Local path to a directory where the kaggle dataset will be downloaded/unzipped.
-            If not provided, `local_path` will be set to `DEFAULT_LOCAL_PATH`/`tag`/`HUB_DATASET_SUBDIR`.
-        **kwargs: Args will be passed into `Dataset`.
+        source (str): Path to where the unstructured kaggle dataset will be downloaded/unzipped.
+        destination (str): Path to where the structured data will be stored.
+        kaggle_credentials (dict): TODO:
+        **kwargs: Args will be passed into `hub.Dataset`.
 
     Returns:
-        A new `hub.Dataset` that is created at `path`.
+        A read-only `hub.Dataset` instance pointing to the structured data.
     """
 
-    kaggle_download_path = os.path.join(local_path, tag, UNSTRUCTURED_DATASET_SUBDIR)
-    if not path:
-        path = os.path.join(local_path, tag, HUB_DATASET_SUBDIR)
-        # TODO: warning?
+    # TODO: make sure source and destination paths are not equal
+    # TODO: make sure `destination` is an EMPTY directory
+    # TODO: make sure `source` is a local path ONLY
 
-    kwargs["path"] = path
-    _warn_kwargs(**kwargs)
+    _warn_kwargs("from_kaggle", **kwargs)
 
     if _dataset_has_tensors(**kwargs):
         return Dataset(**kwargs, mode="r")
 
     try:
-        download_kaggle_dataset(tag, local_path=kaggle_download_path, kaggle_credentials=kaggle_credentials)
+        download_kaggle_dataset(tag, local_path=source, kaggle_credentials=kaggle_credentials)
     except KaggleDatasetAlreadyDownloadedError as e:
         warnings.warn(e.message)
 
-    ds = from_path(kaggle_download_path, **kwargs)
+    ds = from_path(source=source, destination=destination, **kwargs)
 
     return ds
