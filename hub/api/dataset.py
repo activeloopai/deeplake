b5fae64a0fbf5ac9d3795b345fe665aede50cf0a
kristinagrig06 <kristina@activeloop.ai>
2021-06-09 13:33:02 +0400

    Add compression to tensor meta

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -95,6 +95,7 @@ class Dataset:
         htype: Optional[str] = None,
         chunk_size: Optional[int] = None,
         dtype: Optional[str] = None,
+        compression: Optional[str] = None,
         extra_meta: Optional[dict] = None,
     ):
         """Creates a new tensor in a dataset.
@@ -124,7 +125,9 @@ class Dataset:
         ds_meta["tensors"].append(name)
         self.meta = ds_meta
 
-        tensor_meta = default_tensor_meta(htype, chunk_size, dtype, extra_meta)
+        tensor_meta = default_tensor_meta(
+            htype, chunk_size, dtype, compression, extra_meta
+        )
         tensor = Tensor(name, self.storage, tensor_meta=tensor_meta)
         self.tensors[name] = tensor
 
