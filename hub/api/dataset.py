9e70819672b4a90307ecfeeecffb620bb1d17f36
McCrearyD <mccreary@dyllan.ai>
2021-06-10 15:11:42 -0700

    create/load dataset functions

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -5,7 +5,7 @@ from typing import Callable, Dict, Optional, Union
 from hub.api.tensor import Tensor
 from hub.constants import DEFAULT_MEMORY_CACHE_SIZE, DEFAULT_LOCAL_CACHE_SIZE, MB
 from hub.core.dataset import dataset_exists
-from hub.core.meta.dataset_meta import DatasetMeta 
+from hub.core.meta.dataset_meta import create_dataset_meta, load_dataset_meta
 from hub.core.meta.tensor_meta import default_tensor_meta
 from hub.core.tensor import tensor_exists
 from hub.core.typing import StorageProvider
@@ -63,10 +63,14 @@ class Dataset:
         )
         self.tensors: Dict[str, Tensor] = {}
 
-        self.meta = DatasetMeta(get_dataset_meta_key(), self.storage)
         if dataset_exists(self.storage):
+            self.meta = load_dataset_meta(get_dataset_meta_key(), self.storage)
             for tensor_name in self.meta.tensors:
                 self.tensors[tensor_name] = Tensor(tensor_name, self.storage)
+            
+        else:
+            self.meta = create_dataset_meta(get_dataset_meta_key(), self.storage)
+
 
     # TODO len should consider slice
     def __len__(self):
--- hub/core/meta/dataset_meta.py
+++ hub/core/meta/dataset_meta.py
@@ -1,27 +1,13 @@
 from hub.core.meta.meta import CallbackDict, CallbackList, Meta
-import hub
-
 from hub.core.typing import StorageProvider
-from hub.util.keys import get_dataset_meta_key
 
 
-def create_dataset_meta(key: str, storage: StorageProvider):
+def create_dataset_meta(key: str, storage: StorageProvider) -> Meta:
     required_meta = {
         "tensors": CallbackList,
-        "custom_meta": CallbackDict
     }
 
     return Meta(key, storage, required_meta)
 
-def load_dataset_meta(key: str, storage: StorageProvider):
+def load_dataset_meta(key: str, storage: StorageProvider) -> Meta:
     return Meta(key, storage)
-
-#class DatasetMeta(Meta):
-#    def __init__(self, **kwargs):
-#        required_meta = {
-#            "tensors": CallbackList(self._write),
-#            "custom_meta": CallbackDict(self._write)
-#        }
-#
-#        print(kwargs)
-#        super().__init__(required_meta=required_meta, **kwargs)
