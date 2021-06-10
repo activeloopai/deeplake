d60817c75fe5fc92ae745d7d85c31972d819db50
McCrearyD <mccreary@dyllan.ai>
2021-06-10 04:07:54 -0700

    remove old dataset methods

--- hub/core/meta/dataset_meta.py
+++ hub/core/meta/dataset_meta.py
@@ -6,13 +6,6 @@ from hub.core.typing import StorageProvider
 from hub.util.keys import get_dataset_meta_key
 
 
-def write_dataset_meta(storage: StorageProvider, meta: dict):
-    storage[get_dataset_meta_key()] = json.dumps(meta)
-
-def read_dataset_meta(storage: StorageProvider) -> dict:
-    return json.loads(storage[get_dataset_meta_key()])
-
-
 class CallbackList(list):
     def __init__(self, write: Callable):
         self.write = write
