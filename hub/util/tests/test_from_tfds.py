07ffb8a6c9f617028a7fe6d42457b100aadc7b78
Shashank Agarwal <shashank@shankee.com>
2021-06-11 15:26:40 +0530

    Fix tests

--- hub/util/tests/test_from_tfds.py
+++ hub/util/tests/test_from_tfds.py
@@ -2,7 +2,6 @@ import numpy as np
 import pytest
 
 from hub.util.check_installation import tfds_installed  # type: ignore
-from hub.util.from_tfds import from_tfds, from_tfds_to_path  # type: ignore
 
 requires_tfds = pytest.mark.skipif(
     not tfds_installed(), reason="requires tensorflow_datasets to be installed"
@@ -11,6 +10,7 @@ requires_tfds = pytest.mark.skipif(
 
 @requires_tfds
 def test_from_tfds_to_path(local_storage):
+    from hub.util.from_tfds import from_tfds, from_tfds_to_path  # type: ignore
     if local_storage is None:
         pytest.skip()
     hub_ds = from_tfds_to_path(
@@ -29,7 +29,7 @@ def test_from_tfds_to_path(local_storage):
 @requires_tfds
 def test_from_tfds(ds):
     import tensorflow_datasets as tfds  # type: ignore
-
+    from hub.util.from_tfds import from_tfds, from_tfds_to_path  # type: ignore
     tfds_ds = tfds.load("mnist", split="train").batch(1).take(10)
     from_tfds(tfds_ds=tfds_ds, ds=ds)
     for i, example in enumerate(tfds_ds):
