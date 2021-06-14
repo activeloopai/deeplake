4806e3814ef235c977f84d7b1396754edd99de37
McCrearyD <mccreary@dyllan.ai>
2021-06-13 21:58:45 -0700

    fix syntax

--- hub/api/tests/test_update_tensor.py
+++ hub/api/tests/test_update_tensor.py
@@ -11,6 +11,7 @@ def test_update_fixed_tensor(ds: Dataset):
     assert image.shape.lower == (28, 28, 3)
     assert image.shape.upper == (28, 28, 3)
 
+    # fixed samples
     image[5] = np.arange(28 * 28 * 3).reshape((28, 28, 3))
 
     assert len(image) == 20
@@ -28,6 +29,7 @@ def test_update_dynamic_tensor(ds: Dataset):
     assert image.shape.lower == (28, 28, 3)
     assert image.shape.upper == (28, 28, 3)
 
+    # dynamic samples
     image[5] = np.arange(10 * 30 * 3).reshape((10, 30, 3))
 
     assert len(image) == 20
@@ -73,8 +75,8 @@ def test_update_tensor_slice_with_list(ds: Dataset):
 
     # dynamic samples
     image[5:10] = [
-        *np.arange(3 * 10 * 30 * 3).reshape((3, 10, 30, 5))
-        * np.arange(2 * 90 * 3 * 2).reshape((2, 90, 3, 2))
+        *np.arange(3 * 10 * 30 * 3).reshape((3, 10, 30, 5)),
+        *np.arange(2 * 90 * 3 * 2).reshape((2, 90, 3, 2)),
     ]
 
     assert len(image) == 20
