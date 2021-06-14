1a95fc5bfe80ee5db9c924853344d4b3b4f632f4
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-06-14 12:24:09 +0530

    removed redundant function

--- setup.py
+++ setup.py
@@ -1,27 +1,9 @@
+from hub.util.get_property import get_property
 import os
-import re
 
 from setuptools import find_packages, setup
 
 project_name = "hub"
-
-
-def get_property(prop, project):
-    """
-    Get a property from the `project_name/__init__.py` file.
-
-    Example:
-        `get_property("__version__", "hub")`,
-        returns the __version__ variable set inside `hub/__init__.py`.
-    """
-
-    fname = project + "/__init__.py"
-    result = re.search(
-        # find variable with name `prop` in the `project + __init__.py` file.
-        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
-        open(fname).read(),
-    )
-    return result.group(1)
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
