5f098916d76f90c37ddfff54e90d73c0d5f52d55
McCrearyD <mccreary@dyllan.ai>
2021-06-14 00:01:44 -0700

    update docstring & add common configs for `htypes`

--- hub/core/tensor.py
+++ hub/core/tensor.py
@@ -40,10 +40,12 @@ def create_tensor(
         key (str): Key for where the chunks, index_meta, and tensor_meta will be located in `storage` relative to it's root.
         storage (StorageProvider): StorageProvider that all tensor data is written to.
         htype (str): Htype is how the default tensor metadata is defined.
+        **kwargs: `htype` defaults can be overridden by passing any of the compatible parameters.
+            To see all `htype`s and their correspondent arguments, check out `hub/htypes.py`.
 
     Raises:
         TensorAlreadyExistsError: If a tensor defined with `key` already exists.
-    """  # TODO: update docstring about **kwargs (custom meta too)
+    """
 
     if tensor_exists(key, storage):
         raise TensorAlreadyExistsError(key)
--- hub/htypes.py
+++ hub/htypes.py
@@ -1,15 +1,21 @@
+from typing import Dict
 from hub.constants import DEFAULT_CHUNK_SIZE
 
 
 DEFAULT_HTYPE = "generic"
 DEFAULT_DTYPE = "float64"
 
-HTYPE_CONFIGURATIONS = {
-    DEFAULT_HTYPE: {"dtype": DEFAULT_DTYPE, "chunk_size": DEFAULT_CHUNK_SIZE},
-    "image": {"dtype": "uint8", "chunk_size": DEFAULT_CHUNK_SIZE},
+HTYPE_CONFIGURATIONS: Dict[str, Dict] = {
+    DEFAULT_HTYPE: {"dtype": DEFAULT_DTYPE},
+    "image": {"dtype": "uint8"},
     "class_label": {
         "dtype": "uint32",
-        "chunk_size": DEFAULT_CHUNK_SIZE,
         "class_names": [],
     },
 }
+
+# these configs are added to every `htype`
+COMMON_CONFIGS = {"chunk_size": DEFAULT_CHUNK_SIZE, "custom_meta": {}}
+
+for config in HTYPE_CONFIGURATIONS.values():
+    config.update(COMMON_CONFIGS)
