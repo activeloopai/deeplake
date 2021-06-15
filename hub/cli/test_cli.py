6d9091eb4d7bcfaded537fe856d6be6c47abcb91
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-06-15 13:29:05 +0530

    uodating cli strings in test

--- hub/cli/test_cli.py
+++ hub/cli/test_cli.py
@@ -17,8 +17,8 @@ def test_cli_auth():
     assert result.exit_code == 0
     assert (
         result.output
-        == "Log in using Activeloop credentials.\nIf you don't have an account register by using 'hub register' "
-        "command or by going to https://app.activeloop.ai/register.\n\nSuccessfully logged in to Hub.\n"
+        == "Login to Activeloop Hub using your credentials.\nIf you don't have an account, register by using 'activeloop register' command or by going to "
+        "https://app.activeloop.ai/register.\n\nSuccessfully logged in to Activeloop Hub.\n"
     )
 
     result = runner.invoke(logout)
