22b1c512aee0703cbc11a3c67806495da694f6fd
McCrearyD <mccreary@dyllan.ai>
2021-06-15 16:52:22 -0700

    add write_array docstring and update write_bytes

--- hub/core/chunk_engine/write.py
+++ hub/core/chunk_engine/write.py
@@ -18,7 +18,17 @@ def write_array(
     tensor_meta: TensorMeta,
     index_meta: IndexMeta,
 ):
-    # TODO: docstring (note that `array` is assumed to be batched)
+    """Chunk and write an array to storage, also updates `index_meta`/`tensor_meta`. The provided array is treated as a batched of samples.
+
+    Args:
+        array (np.ndarray): Batched array to be chunked/written. 
+        key (str): Key for where the index_meta and tensor_meta are located in `storage` relative to it's root.
+            A subdirectory is created under this `key` (defined in `constants.py`), which is where the chunks will be
+            stored.
+        storage (StorageProvider): StorageProvider for storing the chunks, index_meta, and tensor_meta.
+        tensor_meta (TensorMeta): TensorMeta object that will be written to.
+        index_meta (IndexMeta): IndexMeta object that will be written to.
+    """
 
     # TODO: get the tobytes function from meta
     tobytes = row_wise_to_bytes
@@ -57,7 +67,7 @@ def write_bytes(
     index_meta: IndexMeta,
     extra_sample_meta: dict = {},
 ):
-    """Chunk and write bytes to storage, then update `index_meta`. The provided bytes are treated as a single sample.
+    """Chunk and write bytes to storage, also updates `index_meta`/`tensor_meta`. The provided bytes are treated as a single sample.
 
     Args:
         b (memoryview): Bytes (as memoryview) to be chunked/written. `b` is considered to be 1 sample and will be
@@ -66,7 +76,8 @@ def write_bytes(
             A subdirectory is created under this `key` (defined in `constants.py`), which is where the chunks will be
             stored.
         storage (StorageProvider): StorageProvider for storing the chunks, index_meta, and tensor_meta.
-        index_meta (IndexMeta): IndexMeta object that will be written to to keep track of the written chunk(s).
+        tensor_meta (TensorMeta): TensorMeta object that will be written to.
+        index_meta (IndexMeta): IndexMeta object that will be written to.
         extra_sample_meta (dict): By default `chunk_names`, `start_byte`, and `end_byte` are written, however
             `IndexMeta.add_entry` supports more parameters than this. Anything passed in this dict will also be used
             to call `IndexMeta.add_entry`.
