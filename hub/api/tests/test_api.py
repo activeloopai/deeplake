a9a544bcf100762611b0de67652b43451a26bae1
McCrearyD <mccreary@dyllan.ai>
2021-06-16 11:24:55 -0700

    add np dtype / py dtype to tests

--- hub/api/tests/test_api.py
+++ hub/api/tests/test_api.py
@@ -260,15 +260,23 @@ def test_shape_property(memory_ds):
 def test_dtype(memory_ds: Dataset):
     tensor = memory_ds.create_tensor("tensor")
     dtyped_tensor = memory_ds.create_tensor("dtyped_tensor", dtype="uint8")
+    np_dtyped_tensor = memory_ds.create_tensor("np_dtyped_tensor", dtype=np.float)
+    py_dtyped_tensor = memory_ds.create_tensor("py_dtyped_tensor", dtype=float)
 
     assert tensor.meta.dtype == None
     assert dtyped_tensor.meta.dtype == "uint8"
+    assert np_dtyped_tensor == "float64"
+    assert py_dtyped_tensor == "float64"
 
     tensor.append(np.ones((10, 10), dtype="float32"))
     dtyped_tensor.append(np.ones((10, 10), dtype="uint8"))
+    np_dtyped_tensor.append(np.ones((10, 10), dtype="float64"))
+    py_dtyped_tensor.append(np.ones((10, 10), dtype="float64"))
 
     assert tensor.meta.dtype == "float32"
     assert dtyped_tensor.meta.dtype == "uint8"
+    assert np_dtyped_tensor == "float64"
+    assert py_dtyped_tensor == "float64"
 
 
 @pytest.mark.xfail(raises=TensorMetaMismatchError, strict=True)
