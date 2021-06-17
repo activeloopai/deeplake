eb1602e9be88abebdbc5715e3390335e2c0e945c
McCrearyD <mccreary@dyllan.ai>
2021-06-16 23:07:08 -0700

    write compress/decompress array

---
+++ hub/util/compress.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from PIL import Image
+from io import BytesIO
+
+
+SUPPORTED_COMPRESSIONS = ["png", "jpeg"]
+
+
+def compress_array(array: np.ndarray, compression: str) -> bytes:
+    if compression in SUPPORTED_COMPRESSIONS:
+        img = Image.fromarray(array.astype("uint8"), "RGB")
+        out = BytesIO()
+        img.save(out, compression)
+        out.seek(0)
+        return out.read()
+    else:
+        raise Exception()  # TODO
+
+
+def decompress_array(buffer: bytes, compression: str) -> np.ndarray:
+    if compression in SUPPORTED_COMPRESSIONS:
+        img = Image.open(BytesIO(buffer))
+        return np.array(img)
+    else:
+        raise Exception()  # TODO
---
+++ hub/util/tests/test_compression.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.core.fromnumeric import compress
+import pytest
+from hub.util.compress import SUPPORTED_COMPRESSIONS, compress_array, decompress_array
+import os
+from hub.tests.common import get_dummy_data_path
+
+
+parametrize_compressions = pytest.mark.parametrize(
+    "compression", SUPPORTED_COMPRESSIONS
+)
+
+
+@parametrize_compressions
+def test_file(compression):
+    # TODO
+    pass
+
+
+@parametrize_compressions
+def test_array(compression):
+    array = np.zeros((100, 100, 3), dtype="uint8")  # TODO: handle non-uint8
+    compressed_buffer = compress_array(array, compression)
+
+    assert len(compressed_buffer) < len(
+        array.tobytes()
+    ), "Compressed buffer should be smaller than uncompressed buffer."
+
+    decompressed_array = decompress_array(compressed_buffer, compression)
+    np.testing.assert_array_equal(array, decompressed_array)
