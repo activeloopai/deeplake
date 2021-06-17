355e255be14b0bfdfa341a7d4aa839ff832a9447
McCrearyD <mccreary@dyllan.ai>
2021-06-16 18:01:34 -0700

    add __str__ to load and test in isolation

--- hub/util/load.py
+++ hub/util/load.py
@@ -2,7 +2,7 @@ import re
 import numpy as np
 import pathlib
 import exiftool  # type: ignore
-from typing import Callable, Dict, List, Union
+from typing import Callable, Dict, List, Tuple, Union
 from hub.util.exceptions import (
     HubAutoUnsupportedFileExtensionError,
     SampleCorruptedError,
@@ -21,52 +21,63 @@ class SymbolicSample:
         self._array = None
 
     @property
-    def name(self):
-        return self.path.split("/")[-1]
+    def was_read(self):
+        return self._array is not None
 
     @property
-    def shape(self):
-        # TODO: raise good exception if `numpy` wasn't called
+    def array(self) -> np.ndarray:
+        self.read()
+        return self._array
+
+    @property
+    def shape(self) -> Tuple[int, ...]:
+        self.read()
         return self._array.shape
 
     @property
-    def dtype(self):
-        # TODO: raise good exception if `numpy` wasn't called
+    def dtype(self) -> str:
+        self.read()
         return self._array.dtype.name
 
     @property
     def compression(self) -> str:
-        # TODO: raise exception if `numpy` wasn't called
+        # TODO: raise exception if `read` wasn't called
+        self.read()
         return self._compression
 
-    def raw_bytes(self):
+    def raw_bytes(self) -> bytes:
         with open(self.path, "rb") as image_file:
             return image_file.read()
 
-    def numpy(self) -> np.ndarray:
-        # TODO: explain this
-        if self._array is not None:
-            return self._array
+    def read(self):
+        if self._array is None:
+            # TODO: explain this
+            if self._array is not None:
+                return self._array
 
-        suffix = self.path.suffix.lower()
+            suffix = self.path.suffix.lower()
 
-        if suffix in IMAGE_SUFFIXES:
-            img = Image.open(self.path)
+            if suffix in IMAGE_SUFFIXES:
+                img = Image.open(self.path)
 
-            # TODO: mention in docstring that if this loads correctly, the meta is assumed to be valid
-            try:
-                self._array = np.array(img)
-            except:
-                # TODO: elaborate on why it corrupted
-                raise SampleCorruptedError(self.path)
+                # TODO: mention in docstring that if this loads correctly, the meta is assumed to be valid
+                try:
+                    self._array = np.array(img)
+                except:
+                    # TODO: elaborate on why it corrupted
+                    raise SampleCorruptedError(self.path)
 
-            # TODO: set meta values
-            self._compression = img.format
-            return self._array
+                # TODO: set meta values
+                self._compression = img.format
+                return self._array
 
-        raise HubAutoUnsupportedFileExtensionError(self._suffix, SUPPORTED_SUFFIXES)
+            raise HubAutoUnsupportedFileExtensionError(self._suffix, SUPPORTED_SUFFIXES)
 
-    # TODO: __str__
+    def __str__(self):
+        if self.was_read is None:
+            return f"SymbolicSample(was_read=False, path={self.path})"
+
+        return f"SymbolicSample(was_read=True, shape={self.shape}, compression='{self.compression}', dtype='{self.dtype}' path={self.path})"
 
 
 def load(path: Union[str, pathlib.Path]) -> SymbolicSample:
--- hub/util/tests/test_load.py
+++ hub/util/tests/test_load.py
@@ -1,6 +1,29 @@
+import os
+from hub.tests.common import get_dummy_data_path
 import hub
 
 
 def test_load():
+    # TODO: make test fixtures for these paths
+    path = get_dummy_data_path("compressed_images")
+    cat_path = os.path.join(path, "cat.jpeg")
+    flower_path = os.path.join(path, "flower.png")
+
     # TODO: hub.load
-    assert False
+    cat = hub.load(cat_path)
+    flower = hub.load(flower_path)
+
+    assert not cat.was_read
+    assert not flower.was_read
+
+    assert cat.shape == (900, 900, 3)
+    assert cat.was_read, "`was_read` should be true after reading any properties"
+    assert cat.compression == "JPEG"
+    assert cat.dtype == "uint8"
+    assert cat.array.shape == (900, 900, 3)
+
+    assert flower.shape == (513, 464, 4)
+    assert flower.was_read, "`was_read` should be true after reading any properties"
+    assert flower.compression == "PNG"
+    assert flower.dtype == "uint8"
+    assert flower.array.shape == (513, 464, 4)
