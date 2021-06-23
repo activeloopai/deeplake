48578fbbb98ad9dfce068396788f0fd411a1c48e
david.buniatyan@gmail.com <david.buniatyan@gmail.com>
2021-06-22 18:04:17 -0700

    added init py

--- hub/__init__.py
+++ hub/__init__.py
@@ -3,7 +3,7 @@ from .api.load import load
 from .core.transform.transform import transform  # type: ignore
 from .util.bugout_reporter import hub_reporter
 
-__version__ = "2.0a3"
+__version__ = "2.0a7"
 
 hub_reporter.tags.append(f"version:{__version__}")
 hub_reporter.system_report(publish=True)
