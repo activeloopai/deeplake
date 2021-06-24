b9afdaf6e82d7b60f7c57119a00e1b67dbc63667
dhiganthrao <dhiganth18037@ece.ssn.edu.in>
2021-06-24 09:22:41 +0530

    Updated test_cli.py to pass on pytest.

--- hub/cli/test_cli.py
+++ hub/cli/test_cli.py
@@ -16,8 +16,8 @@ def test_cli_auth():
     password = os.getenv("ACTIVELOOP_HUB_PASSWORD")
     result = runner.invoke(login, f"-u {username} -p {password}")
     assert result.exit_code == 0
-    assert result.output == "Successfully logged in to Activeloop Hub.\n"
+    assert result.output == "Successfully logged in to Activeloop.\n"
 
     result = runner.invoke(logout)
     assert result.exit_code == 0
-    assert result.output == "Logged out of Activeloop Hub.\n"
+    assert result.output == "Logged out of Activeloop.\n"
