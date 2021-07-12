9e7434f4133405e392c9ff13e03e75aeb38b7cbe
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-07-12 10:19:57 +0000

    windows always uses pytorch_old now

--- hub/integrations/__init__.py
+++ hub/integrations/__init__.py
@@ -1,7 +1,14 @@
+from hub.util.exceptions import WindowsSharedMemoryError
+
+
 try:
+    import os
+
+    if os.name == "nt":
+        raise WindowsSharedMemoryError
     from multiprocessing import shared_memory
     from .pytorch import dataset_to_pytorch
-except ImportError:
+except (ImportError, WindowsSharedMemoryError):
     from .pytorch_old import dataset_to_pytorch
 
 from .tensorflow import dataset_to_tensorflow
