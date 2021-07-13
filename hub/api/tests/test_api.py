cb4ea21852ccc0cae11d354973140bcf840c37b2
Fariz Rahman <farizrahman4u@gmail.com>
2021-07-14 03:57:13 +0530

    revert test_api.py

--- hub/api/tests/test_api.py
+++ hub/api/tests/test_api.py
@@ -1,3 +1,4 @@
+from hub.constants import UNCOMPRESSED
 import numpy as np
 import pytest
 import uuid
@@ -6,10 +7,7 @@ import os
 from hub.api.dataset import Dataset
 from hub.core.tests.common import parametrize_all_dataset_storages
 from hub.tests.common import assert_array_lists_equal
-from hub.util.exceptions import (
-    TensorDtypeMismatchError,
-    TensorInvalidSampleShapeError,
-)
+from hub.util.exceptions import TensorDtypeMismatchError, TensorInvalidSampleShapeError
 from hub.client.client import HubBackendClient
 from hub.client.utils import has_hub_testing_creds
 from click.testing import CliRunner
@@ -187,7 +185,8 @@ def test_empty_samples(ds: Dataset):
     actual_list = tensor.numpy(aslist=True)
     expected_list = [a1, *a2, a3, *a4]
 
-    assert tensor.meta.sample_compression is None
+    assert tensor.meta.sample_compression == UNCOMPRESSED
+    assert tensor.meta.chunk_compression == UNCOMPRESSED
 
     assert len(tensor) == 16
     assert tensor.shape_interval.lower == (16, 0, 0, 2)
@@ -197,7 +196,7 @@ def test_empty_samples(ds: Dataset):
 
     # test indexing individual empty samples with numpy while looping, this may seem redundant but this was failing before
     for actual_sample, expected in zip(ds, expected_list):
-        actual = actual_sample["with_empty"].numpy()
+        actual = actual_sample.with_empty.numpy()
         np.testing.assert_array_equal(actual, expected)
 
 
@@ -218,27 +217,30 @@ def test_scalar_samples(ds: Dataset):
     tensor.append(-99)
     tensor.append(np.array(4))
 
-    tensor.append(np.int16(4))
+    with pytest.raises(TensorDtypeMismatchError):
+        tensor.append(np.int16(4))
 
     with pytest.raises(TensorDtypeMismatchError):
         tensor.append(np.float32(4))
 
-    tensor.append(np.uint8(3))
+    with pytest.raises(TensorDtypeMismatchError):
+        tensor.append(np.uint8(3))
 
     tensor.extend([10, 1, 4])
     tensor.extend([1])
     tensor.extend(np.array([1, 2, 3], dtype=MAX_INT_DTYPE))
 
-    tensor.extend(np.array([4, 5, 33], dtype="int16"))
+    with pytest.raises(TensorDtypeMismatchError):
+        tensor.extend(np.array([4, 5, 33], dtype="int16"))
 
-    assert len(tensor) == 16
+    assert len(tensor) == 11
 
-    expected = np.array([5, 10, -99, 4, 4, 3, 10, 1, 4, 1, 1, 2, 3, 4, 5, 33])
+    expected = np.array([5, 10, -99, 4, 10, 1, 4, 1, 1, 2, 3])
     np.testing.assert_array_equal(tensor.numpy(), expected)
 
     assert tensor.numpy(aslist=True) == expected.tolist()
 
-    assert tensor.shape == (16,)
+    assert tensor.shape == (11,)
 
     # len(shape) for a scalar is `()`. len(shape) for [1] is `(1,)`
     with pytest.raises(TensorInvalidSampleShapeError):
@@ -255,7 +257,6 @@ def test_sequence_samples(ds: Dataset):
 
     tensor.append([1, 2, 3])
     tensor.extend([[4, 5, 6]])
-    ds.clear_cache()
 
     assert len(tensor) == 2
 
@@ -383,7 +384,7 @@ def test_shape_property(memory_ds):
 
 
 def test_htype(memory_ds: Dataset):
-    image = memory_ds.create_tensor("image", htype="image", sample_compression="png")
+    image = memory_ds.create_tensor("image", htype="image")
     bbox = memory_ds.create_tensor("bbox", htype="bbox")
     label = memory_ds.create_tensor("label", htype="class_label")
     video = memory_ds.create_tensor("video", htype="video")
@@ -426,20 +427,16 @@ def test_dtype(memory_ds: Dataset):
     np_dtyped_tensor.append(np.ones((10, 10), dtype=MAX_FLOAT_DTYPE))
     py_dtyped_tensor.append(np.ones((10, 10), dtype=MAX_FLOAT_DTYPE))
 
-    # test auto upcasting
-    np_dtyped_tensor.append(np.ones((10, 10), dtype="float32"))
-    py_dtyped_tensor.append(np.ones((10, 10), dtype="float32"))
-
-    with pytest.raises(TensorDtypeMismatchError):
-        tensor.append(np.ones((10, 10), dtype="float64"))
-
-    with pytest.raises(TensorDtypeMismatchError):
-        dtyped_tensor.append(np.ones((10, 10), dtype="uint64") * 256)
-
     assert tensor.dtype == np.float32
     assert dtyped_tensor.dtype == np.uint8
     assert np_dtyped_tensor.dtype == MAX_FLOAT_DTYPE
     assert py_dtyped_tensor.dtype == MAX_FLOAT_DTYPE
+
+
+@pytest.mark.xfail(raises=TensorDtypeMismatchError, strict=True)
+def test_dtype_mismatch(memory_ds: Dataset):
+    tensor = memory_ds.create_tensor("tensor", dtype="float16")
+    tensor.append(np.ones(100, dtype="uint8"))
 
 
 @pytest.mark.xfail(raises=TypeError, strict=True)
@@ -474,55 +471,13 @@ def test_hub_cloud_dataset():
     ds.delete()
 
 
-def test_iter_perf(memory_ds: Dataset):
-    orig_searchsorted = np.searchsorted
-    call_count = {"n": 0}
-    callers = []
-
-    def searchsorted(*args, **kwargs):
-        import inspect
-
-        callers.append(inspect.stack()[1][3])
-        call_count["n"] += 1
-        return orig_searchsorted(*args, **kwargs)
-
-    ds = memory_ds
-    ds.create_tensor("x")
-    ds.create_tensor("y")
-    for _ in range(10):
-        ds["x"].append(np.zeros((10, 10)))
-        ds["y"].append(np.ones((10, 10)))
-
-    np.searchsorted = searchsorted
-    for i, sub_ds in enumerate(ds):
-        np.testing.assert_array_equal(sub_ds["x"].numpy(), np.zeros((10, 10)))
-        np.testing.assert_array_equal(sub_ds["y"].numpy(), np.ones((10, 10)))
-
-    assert call_count["n"] == 40
-
-    np.searchsorted = orig_searchsorted
-
-
-def test_array_interface(memory_ds: Dataset):
-    tensor = memory_ds.create_tensor("tensor")
-    x = np.random.random((32, 32))
-    tensor.append(x)
-    arr1 = np.array(tensor)
-    arr2 = np.array(tensor)
-    np.testing.assert_array_equal(x, arr1[0])
-    np.testing.assert_array_equal(x, arr2[0])
-    assert arr1.__array_interface__["data"][0] == arr1.__array_interface__["data"][0]
-    tensor.append(x)
-    np.testing.assert_array_equal(tensor.numpy(), np.concatenate([arr1, arr2]))
-
-
 @parametrize_all_dataset_storages
 def test_hub_dataset_suffix_bug(ds):
     # creating dataset with similar name but some suffix removed from end
     ds2 = Dataset(ds.path[:-1])
     ds2.delete()
 
-
+    
 def test_empty_dataset():
     with CliRunner().isolated_filesystem():
         ds = Dataset("test")
