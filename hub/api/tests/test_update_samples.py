dcf108443d61c62d55bff3f36a60e5e1b8e60b85
McCrearyD <mccreary@dyllan.ai>
2021-07-20 14:00:03 -0700

    write tests for trivial + non-trivial update

---
+++ hub/api/tests/test_update_samples.py
@@ -0,0 +1,85 @@
+from typing import Callable
+from hub.tests.common import assert_array_lists_equal
+from hub.api.dataset import Dataset
+import numpy as np
+
+
+def _add_dummy_mnist(ds_generator: Callable):
+    ds = ds_generator()
+
+    ds.create_tensor("images")
+    ds.create_tensor("labels")
+
+    ds.images.extend(np.ones((10, 28, 28)))
+    ds.labels.extend(np.ones(10))
+
+    return ds
+
+
+def _make_update_assert_equal(ds_generator: Callable, tensor_name: str, index, value):
+    """Updates a tensor and checks that the data is as expected.
+
+    Example update:
+        >>> ds.tensor[0:5] = 1
+
+    Args:
+        ds_generator (Callable): Function that returns a new dataset object with each call.
+        tensor_name (str): Name of the tensor to be updated.
+        index (Any): Any value that can be used as an index for an access/modifier operation (`ds.tensor[index] = value`).
+        value (Any): Any value that can be used as a value for an access/modifier operation (`ds.tensor[index] = value`).
+    """
+
+    ds = ds_generator()
+    assert len(ds) == 10
+
+    tensor = ds[tensor_name]
+    expected = tensor.numpy()
+
+    # make updates
+    tensor[index] = value
+    expected[index] = value
+
+    # persistence
+    ds = ds_generator()
+    tensor = ds[tensor_name]
+
+    assert_array_lists_equal(tensor.numpy(), expected)
+
+    # make sure no new values are recorded
+    ds = ds_generator()
+    assert len(ds) == 10
+
+
+def test_trivial(local_ds_generator):
+    gen = local_ds_generator
+
+    _add_dummy_mnist(gen)
+
+    _make_update_assert_equal(gen, "images", 0, np.ones((28, 28)) * 5)
+    _make_update_assert_equal(gen, "labels", 0, 5)
+
+    _make_update_assert_equal(gen, "images", -1, np.ones((28, 28)) * 75)
+    _make_update_assert_equal(gen, "labels", -5, 99)
+
+
+def test_non_trivial(local_ds_generator):
+    gen = local_ds_generator
+    _add_dummy_mnist(gen)
+
+    # incoming samples are same shape as expected
+    _make_update_assert_equal(
+        gen, "images", slice(0, 3), np.arange(3 * 28 * 28).reshape((3, 28, 28))
+    )
+    _make_update_assert_equal(gen, "labels", slice(0, 5), [1, 2, 3, 4, 5])
+
+    # incoming samples are broadcasted
+    _make_update_assert_equal(gen, "images", slice(0, 3), np.zeros((28, 28)))
+    _make_update_assert_equal(gen, "labels", slice(0, 5), 3)
+
+
+def test_failures(memory_ds):
+    # TODO: test bad shape broadcasting
+
+    # TODO: test inplace operators (these shouldn't happen yet)
+
+    pass
