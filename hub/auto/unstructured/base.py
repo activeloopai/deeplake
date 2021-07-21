695e01d77503584de4ff09f2fe2661a989344871
Eshan <thisiseshan@gmail.com>
2021-07-22 01:59:55 +0530

    add docstrings

--- hub/auto/unstructured/base.py
+++ hub/auto/unstructured/base.py
@@ -7,6 +7,15 @@ class UnstructuredDataset(ABC):
     def __init__(self, source: str):
         self.source = Path(find_root(source))
 
+    """Initializes an unstructured dataset.
+    Args:
+        source (str): The full path to the dataset.
+            Can be a Hub cloud path of the form hub://username/datasetname. To write to Hub cloud datasets, ensure that you are logged in to Hub (use 'activeloop login' from command line)
+            Can be a s3 path of the form s3://bucketname/path/to/dataset. Credentials are required in either the environment or passed to the creds argument.
+            Can be a local file system path of the form ./path/to/dataset or ~/path/to/dataset or path/to/dataset.
+            Can be a memory path of the form mem://path/to/dataset which doesn't save the dataset but keeps it in memory instead. Should be used only for testing as it does not persist.
+    """
+
     @abstractmethod
     def structure(ds, use_progress_bar: bool = True, **kwargs):
         pass
--- hub/auto/unstructured/image_classification.py
+++ hub/auto/unstructured/image_classification.py
@@ -45,6 +45,15 @@ def _set_name_from_path(path: Path) -> str:
 
 class ImageClassification(UnstructuredDataset):
     def __init__(self, source: str):
+        """Convert an unstructured dataset to a structured dataset.
+        Args:
+            source (str): The full path to the dataset.
+                Can be a Hub cloud path of the form hub://username/datasetname. To write to Hub cloud datasets, ensure that you are logged in to Hub (use 'activeloop login' from command line)
+                Can be a s3 path of the form s3://bucketname/path/to/dataset. Credentials are required in either the environment or passed to the creds argument.
+                Can be a local file system path of the form ./path/to/dataset or ~/path/to/dataset or path/to/dataset.
+                Can be a memory path of the form mem://path/to/dataset which doesn't save the dataset but keeps it in memory instead. Should be used only for testing as it does not persist.
+        """
+
         # TODO: should support any `StorageProvider`. right now only local files can be converted
         super().__init__(source)
 
