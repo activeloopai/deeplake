f4ef763178dea2dc6cfc23c674eefc56a3f8ed8b
McCrearyD <mccreary@dyllan.ai>
2021-07-21 10:41:26 -0700

    add dynamic tests

--- hub/api/tests/test_update_samples.py
+++ hub/api/tests/test_update_samples.py
@@ -32,7 +32,7 @@ def _make_update_assert_equal(ds_generator: Callable, tensor_name: str, index, v
     assert len(ds) == 10
 
     tensor = ds[tensor_name]
-    expected = tensor.numpy()
+    expected = tensor.numpy(aslist=True)
 
     # make updates
     tensor[index] = value
@@ -42,40 +42,60 @@ def _make_update_assert_equal(ds_generator: Callable, tensor_name: str, index, v
     ds = ds_generator()
     tensor = ds[tensor_name]
 
-    assert_array_lists_equal(tensor.numpy(), expected)
+    assert_array_lists_equal(tensor.numpy(aslist=True), expected)
 
     # make sure no new values are recorded
     ds = ds_generator()
     assert len(ds) == 10
 
 
-def test_same_shape(local_ds_generator):
+def test(local_ds_generator):
     gen = local_ds_generator
 
+    # TODO: test with compression too
     _add_dummy_mnist(gen())
 
     # update single sample
-    _make_update_assert_equal(gen, "images", 0, np.ones((28, 28)) * 5)
-    _make_update_assert_equal(gen, "labels", 0, 5)
-    _make_update_assert_equal(gen, "images", -1, np.ones((28, 28)) * 75)
+    _make_update_assert_equal(gen, "images", 0, np.ones((28, 25)) * 5)  # new shape
+    _make_update_assert_equal(gen, "images", -1, np.ones((28, 28)) * 75)  # same shape
+    _make_update_assert_equal(
+        gen, "images", -1, np.ones((0, 0))
+    )  # empty sample (new shape)
     _make_update_assert_equal(gen, "labels", -5, 99)
+    _make_update_assert_equal(gen, "labels", 0, 5)
 
     # update a range of samples
+    x = np.arange(3 * 28 * 28).reshape((3, 28, 28))
+    _make_update_assert_equal(gen, "images", slice(0, 3), x)  # same shapes
     _make_update_assert_equal(
-        gen, "images", slice(0, 3), np.arange(3 * 28 * 28).reshape((3, 28, 28))
-    )
+        gen, "images", slice(3, 5), np.zeros((2, 5, 28))
+    )  # new shapes
+    _make_update_assert_equal(
+        gen, "images", slice(3, 5), np.zeros((2, 0, 0))
+    )  # empty samples (new shape)
     _make_update_assert_equal(gen, "labels", slice(0, 5), [1, 2, 3, 4, 5])
 
+    # update a range of samples with dynamic samples
+    _make_update_assert_equal(
+        gen,
+        "images",
+        slice(7, 10),
+        [
+            np.ones((28, 50)) * 5,
+            np.ones((0, 5)),
+            np.ones((1, 1)) * 10,
+        ],
+    )
 
-def test_new_shapes(local_ds_generator):
+
+def test_pre_indexed_tensor(local_ds_generator):
     gen = local_ds_generator
     _add_dummy_mnist(gen())
 
     # TODO: test updating a tensor that has already been indexed into. example:
     # t = ds.tensor[5:10]
     # t[0] = ...
-
-    # TODO: test updating a tensor with an empty sample
+    raise NotImplementedError
 
 
 def test_failures(memory_ds):
