408e30b353ea276ecf3e3fa4f72ab35a2cab63fa
Eshan <thisiseshan@gmail.com>
2021-07-21 19:30:57 +0530

    add fixtures to kaggle_tests

--- hub/auto/tests/test_kaggle.py
+++ hub/auto/tests/test_kaggle.py
@@ -1,31 +1,26 @@
-from hub import Dataset
-from hub.auto.tests.common import get_dummy_data_path
+from hub.api.dataset import Dataset
 from hub.auto.unstructured.image_classification import ImageClassification
 from hub.auto.unstructured.kaggle import download_kaggle_dataset
 import os
-import hub
 
 
-def test_ingestion_simple():
-    local = "./datasets/source/kaggle/simple"
-    hub_path = "./datasets/destination/kaggle/simple"
-    download_kaggle_dataset(
-        "andradaolteanu/birdcall-recognition-data", local_path=local
-    )
-    ds = hub.Dataset(hub_path)
-    unstructured = ImageClassification(source=local)
+def test_ingestion_simple(local_ds: Dataset):
+    ds = local_ds
+    kaggle_path = os.path.join(ds.path, "unstructured_kaggle_data_simple")
+    download_kaggle_dataset("andradaolteanu/birdcall-recognition-data", kaggle_path)
+    unstructured = ImageClassification(source=kaggle_path)
     unstructured.structure(ds, image_tensor_args={"sample_compression": "jpeg"})
+
     assert list(ds.tensors.keys()) == ["images", "labels"]
     assert ds.images[9].numpy().shape == (570, 570, 3)
     assert ds.labels.numpy().shape == (10,)
 
 
-def test_ingestion_sets():
-    local = "./datasets/source/kaggle/sets"
-    hub_path = "./datasets/destination/kaggle/sets"
-    download_kaggle_dataset("thisiseshan/bird-classes", local_path=local)
-    ds = hub.Dataset(hub_path)
-    unstructured = ImageClassification(source=local)
+def test_ingestion_sets(local_ds: Dataset):
+    ds = local_ds
+    kaggle_path = os.path.join(ds.path, "unstructured_kaggle_data_sets")
+    download_kaggle_dataset("thisiseshan/bird-classes", local_path=kaggle_path)
+    unstructured = ImageClassification(source=kaggle_path)
     unstructured.structure(ds, image_tensor_args={"sample_compression": "png"})
     assert list(ds.tensors.keys()) == [
         "test/images",
