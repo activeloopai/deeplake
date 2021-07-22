9500028d589ae0d191d2cd5a9a3b56fbec254358
Eshan <thisiseshan@gmail.com>
2021-07-23 01:29:05 +0530

    fix windows kaggle_test error-4

--- hub/auto/unstructured/image_classification.py
+++ hub/auto/unstructured/image_classification.py
@@ -93,7 +93,7 @@ class ImageClassification(UnstructuredDataset):
             images_tensor_name = os.path.join(set_name, IMAGES_TENSOR_NAME)
             labels_tensor_name = os.path.join(set_name, LABELS_TENSOR_NAME)
             images_tensor_map[set_name] = images_tensor_name.replace("\\", "/")
-            labels_tensor_map[set_name] = (labels_tensor_name.replace("\\", "/"),)
+            labels_tensor_map[set_name] = labels_tensor_name.replace("\\", "/")
 
             # TODO: infer sample_compression
             ds.create_tensor(
