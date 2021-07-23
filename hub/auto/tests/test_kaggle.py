93be06a291b136268d94c997ab1fbe60a7be2f69
Eshan <thisiseshan@gmail.com>
2021-07-23 22:52:50 +0530

    add duplicate zip file test

--- hub/auto/tests/test_kaggle.py
+++ hub/auto/tests/test_kaggle.py
@@ -2,6 +2,7 @@ from hub.api.dataset import Dataset
 from hub.auto.unstructured.image_classification import ImageClassification
 from hub.auto.unstructured.kaggle import download_kaggle_dataset
 from hub.util.exceptions import KaggleDatasetAlreadyDownloadedError
+from hub.tests.common import get_dummy_data_path
 import pytest
 import os
 import hub
@@ -46,3 +47,10 @@ def test_kaggle_Exception(local_ds: Dataset):
     with pytest.raises(KaggleDatasetAlreadyDownloadedError):
         download_kaggle_dataset("thisiseshan/bird-classes", local_path=kaggle_path)
         download_kaggle_dataset("thisiseshan/bird-classes", local_path=kaggle_path)
+
+
+def test_kaggle_Exception():
+    path = get_dummy_data_path("tests_auto/test_kaggle_exception")
+
+    with pytest.raises(KaggleDatasetAlreadyDownloadedError):
+        download_kaggle_dataset("thisiseshan/bird-classes", local_path=path)
