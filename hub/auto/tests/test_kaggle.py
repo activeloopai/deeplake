809ad6dce3624db4766fd8f7a72858a5844e1596
Eshan <thisiseshan@gmail.com>
2021-07-23 18:01:57 +0530

    add test for consecutive dataset downloads from kaggle

--- hub/auto/tests/test_kaggle.py
+++ hub/auto/tests/test_kaggle.py
@@ -1,6 +1,8 @@
 from hub.api.dataset import Dataset
 from hub.auto.unstructured.image_classification import ImageClassification
 from hub.auto.unstructured.kaggle import download_kaggle_dataset
+from hub.util.exceptions import KaggleDatasetAlreadyDownloadedError
+import pytest
 import os
 import hub
 
@@ -35,3 +37,12 @@ def test_ingestion_sets(local_ds: Dataset):
     assert ds["train/images"].numpy().shape == (3, 200, 200, 3)
     assert ds["train/labels"].numpy().shape == (3,)
     assert ds["train/labels"].meta.class_names == ("class0", "class1", "class2")
+
+
+def test_kaggle_Exception(local_ds: Dataset):
+    ds = local_ds
+    kaggle_path = os.path.join(ds.path, "unstructured_kaggle_data")
+
+    with pytest.raises(KaggleDatasetAlreadyDownloadedError):
+        download_kaggle_dataset("thisiseshan/bird-classes", local_path=kaggle_path)
+        download_kaggle_dataset("thisiseshan/bird-classes", local_path=kaggle_path)
