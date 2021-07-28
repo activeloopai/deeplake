f77f0fa41085cb68cb6fbddfa1a8e463a0dda672
McCrearyD <mccreary@dyllan.ai>
2021-07-28 16:14:12 -0700

    add another test for bp encoder updates

--- hub/core/meta/encode/tests/test_byte_positions_encoder_updates.py
+++ hub/core/meta/encode/tests/test_byte_positions_encoder_updates.py
@@ -3,9 +3,20 @@ from hub.core.meta.encode.byte_positions import BytePositionsEncoder
 from .common import assert_encoded
 
 
+def _validate_bp(enc: BytePositionsEncoder):
+    """Helps validate test initial and terminal states."""
+
+    expected = np.array(enc._encoded)
+    enc._post_process_state(0)
+    actual = enc._encoded
+
+    np.testing.assert_array_equal(expected, actual)
+
+
 
 def test_update_no_change():
     enc = BytePositionsEncoder(np.array([[8, 0, 10], [4, 88, 15]]))
+    _validate_bp(enc)
 
     enc[0] = 8
     assert_encoded(enc, [[8, 0, 10], [4, 88, 15]])
@@ -28,76 +39,21 @@ def test_update_no_change():
     assert enc.num_samples == 16
 
 
-# def test_update_squeeze_trivial():
-#     enc = BytePositionsEncoder(np.array([[28, 0, 2], [100, 100, 3], [28, 0, 5]]))
-# 
-#     enc[3] = (28, 0)
-#     assert_encoded(enc, [[28, 0, 5]])
-# 
-#     assert enc.num_samples == 6
-# 
-# 
-# def test_update_squeeze_complex():
-#     enc = BytePositionsEncoder(
-#         np.array([[10, 10, 1], [28, 0, 2], [100, 100, 3], [28, 0, 5], [10, 10, 7]])
-#     )
-# 
-#     enc[3] = (28, 0)
-#     assert_encoded(enc, [[10, 10, 1], [28, 0, 5], [10, 10, 7]])
-# 
-#     assert enc.num_samples == 8
-# 
-# 
-# def test_update_move_up():
-#     enc = BytePositionsEncoder(np.array([[101, 100, 0], [100, 101, 5]]))
-# 
-#     enc[1] = (101, 100)
-#     assert_encoded(enc, [[101, 100, 1], [100, 101, 5]])
-# 
-#     enc[2] = (101, 100)
-#     assert_encoded(enc, [[101, 100, 2], [100, 101, 5]])
-# 
-#     assert enc.num_samples == 6
-# 
-# 
-# def test_update_move_down():
-#     enc = BytePositionsEncoder(np.array([[101, 100, 5], [100, 101, 10]]))
-# 
-#     enc[5] = (100, 101)
-#     assert_encoded(enc, [[101, 100, 4], [100, 101, 10]])
-# 
-#     enc[4] = (100, 101)
-#     assert_encoded(enc, [[101, 100, 3], [100, 101, 10]])
-# 
-#     enc[3] = (100, 101)
-#     assert_encoded(enc, [[101, 100, 2], [100, 101, 10]])
-# 
-#     assert enc.num_samples == 11
-# 
-# 
-# def test_update_replace():
-#     enc = BytePositionsEncoder(np.array([[100, 100, 0]]))
-#     enc[0] = (100, 101)
-#     assert enc.num_samples == 1
-# 
-# 
-# def test_update_split_up():
-#     enc = BytePositionsEncoder(np.array([[100, 101, 5]]))
-# 
-#     enc[0] = (101, 100)
-#     assert_encoded(enc, [[101, 100, 0], [100, 101, 5]])
-# 
-# 
-# def test_update_split_down():
-#     enc = BytePositionsEncoder(np.array([[100, 101, 5]]))
-# 
-#     enc[5] = (101, 100)
-#     assert_encoded(enc, [[100, 101, 4], [101, 100, 5]])
-# 
-# 
-# def test_update_split_middle():
-#     enc = BytePositionsEncoder(np.array([[28, 0, 5]]))
-# 
-#     enc[3] = (100, 100)
-#     assert_encoded(enc, [[28, 0, 2], [100, 100, 3], [28, 0, 5]])
-# 
+def test_update_squeeze_trivial():
+    enc = BytePositionsEncoder(np.array([[4, 0, 9], [2, 40, 10], [4, 42, 29]]))
+    _validate_bp(enc)
+
+    enc[10] = 4
+    assert_encoded(enc, [[4, 0, 29]])
+
+    assert enc.num_samples == 30
+
+
+def test_update_squeeze_complex():
+    enc = BytePositionsEncoder(np.array([[2, 0, 5], [4, 12, 9], [2, 28, 10], [4, 30, 29], [2, 106, 100]]))
+    _validate_bp(enc)
+
+    enc[10] = 4
+    assert_encoded(enc, [[2, 0, 5], [4, 12, 29], [2, 108, 100]])
+
+    assert enc.num_samples == 101
