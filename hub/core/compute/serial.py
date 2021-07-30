36a1c2fd3340f95e2d88bd84ab22ac30c80fd3f3
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-07-30 22:41:08 +0530

    fixed serial provider

--- hub/core/compute/serial.py
+++ hub/core/compute/serial.py
@@ -2,8 +2,8 @@ from hub.core.compute.provider import ComputeProvider
 
 
 class SerialProvider(ComputeProvider):
-    def __init__(self, workers):
-        self.workers = workers
+    def __init__(self):
+        pass
 
     def map(self, func, iterable):
         return map(func, iterable)
