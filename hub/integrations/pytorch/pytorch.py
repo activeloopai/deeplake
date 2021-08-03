6c326dd7495e31093ea9ceefe44cbdb9726a4ec9
Fariz Rahman <farizrahman4u@gmail.com>
2021-08-03 22:07:08 +0530

    cast torch tensor

--- hub/integrations/pytorch/pytorch.py
+++ hub/integrations/pytorch/pytorch.py
@@ -263,7 +263,7 @@ class TorchDataset:
             value = value.astype("int32")
         elif value.dtype == "uint32" or value.dtype == "uint64":
             value = value.astype("int64")
-        return torch.tensor(value)  # type: ignore
+        return torch.tensor(value, dtype=chunk_engine.tensor_meta.dtype)  # type: ignore
 
     def _get_data_from_chunks(
         self,
