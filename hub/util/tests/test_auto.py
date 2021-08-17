d62db99ed496e0ed87962221e10611921ade836b
Eshan <thisiseshan@gmail.com>
2021-08-17 18:29:21 +0530

    test ci message

--- hub/util/tests/test_auto.py
+++ hub/util/tests/test_auto.py
@@ -38,8 +38,4 @@ def test_ingestion_summary():
     ingestion_summary(skipped_path, [], 1)
     sys.stdout = sys.__stdout__
 
-    if ingest_summary_skipped.getvalue() in (
-        "\n\nIngesiton Complete. No files were skipped.\n\n\n\n",
-        "\n============================= Ingestion Summary =============================\n\nIngesiton Complete. No files we skipped.\n\n\n\n",
-    ):
-        pass
+    assert ingest_summary_skipped.getvalue() == "test"
