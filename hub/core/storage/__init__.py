16d2546b241ebbbf6b360dd23d32fa5788b0dd44
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-08-18 19:59:00 +0530

    import fix

--- hub/core/storage/__init__.py
+++ hub/core/storage/__init__.py
@@ -2,11 +2,11 @@ from hub.core.storage.s3 import S3Provider
 from hub.core.storage.memory import MemoryProvider
 from hub.core.storage.local import LocalProvider
 from hub.core.storage.provider import StorageProvider
-from hub.core.storage.lru_cache import LRUCache
-from hub.core.storage.prefetch_lru_cache import PrefetchLRUCache
-from hub.core.storage.shuffle_lru_cache import ShuffleLRUCache
 
 try:
     from hub.core.storage.shared_memory import SharedMemoryProvider
 except ModuleNotFoundError:
     pass
+from hub.core.storage.lru_cache import LRUCache
+from hub.core.storage.prefetch_lru_cache import PrefetchLRUCache
+from hub.core.storage.shuffle_lru_cache import ShuffleLRUCache
