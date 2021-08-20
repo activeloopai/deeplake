186b301b62f0d917d4eecba3f1a3e56a4ed70250
McCrearyD <mccreary@dyllan.ai>
2021-08-20 10:14:29 -0700

    update test sizes

--- hub/api/tests/test_tiling.py
+++ hub/api/tests/test_tiling.py
@@ -1,5 +1,4 @@
 from hub.util.exceptions import CannotInferTilesError
-from hub.tests.common import MAX_INT_DTYPE
 import pytest
 import numpy as np
 from hub.constants import KB
@@ -10,11 +9,11 @@ def test_initialize_large_samples(local_ds_generator, compression):
     ds = local_ds_generator()
 
     # keep max chunk size default, this test should run really fast since we barely fill in any data
-    ds.create_tensor("tensor", dtype=MAX_INT_DTYPE, sample_compression=compression)
+    ds.create_tensor("tensor", dtype="int32", sample_compression=compression)
 
     ds.tensor.append_empty((10, 10, 3))  # small
-    ds.tensor.append_empty((1000, 1000, 3))  # large
-    ds.tensor.append(np.ones((10, 10, 3), dtype=MAX_INT_DTYPE))  # small
+    ds.tensor.append_empty((10000, 10000, 3))  # large (1.2GB)
+    ds.tensor.append(np.ones((10, 10, 3), dtype="int32"))  # small
     ds.tensor.extend_empty((5, 10, 10, 3))  # small
 
     ds = local_ds_generator()
@@ -30,14 +29,14 @@ def test_initialize_large_samples(local_ds_generator, compression):
         ds.tensor[1, -100:-50, -100:-50, :].numpy(), np.zeros((50, 50, 3))
     )
 
-    # update large sample (only filling in 60KB of data)
+    # update large sample (only filling in 10KB of data)
     ds = local_ds_generator()
-    ds.tensor[1, 50:100, 50:100, 0] = np.ones((1, 50, 50, 1), dtype=MAX_INT_DTYPE)
-    ds.tensor[1, 50:100, 50:100, 1] = np.ones((1, 50, 50, 1), dtype=MAX_INT_DTYPE) * 2
-    ds.tensor[1, 50:100, 50:100, 2] = np.ones((1, 50, 50, 1), dtype=MAX_INT_DTYPE) * 3
+    ds.tensor[1, 50:100, 50:100, 0] = np.ones((1, 50, 50, 1), dtype="int32")
+    ds.tensor[1, 50:100, 50:100, 1] = np.ones((1, 50, 50, 1), dtype="int32") * 2
+    ds.tensor[1, 50:100, 50:100, 2] = np.ones((1, 50, 50, 1), dtype="int32") * 3
 
     ds = local_ds_generator()
-    expected = np.ones((50, 50, 3), dtype=MAX_INT_DTYPE)
+    expected = np.ones((50, 50, 3), dtype="int32")
     expected[:, :, 1] *= 2
     expected[:, :, 2] *= 3
     np.testing.assert_array_equal(ds.tensor[1, 50:100, 50:100, :].numpy(), expected)
@@ -46,14 +45,15 @@ def test_initialize_large_samples(local_ds_generator, compression):
 @pytest.mark.parametrize("compression", [None, "png"])
 def test_populate_full_large_sample(local_ds_generator, compression):
     ds = local_ds_generator()
+
     ds.create_tensor(
         "large",
-        dtype=MAX_INT_DTYPE,
+        dtype="int32",
         sample_compression=compression,
         max_chunk_size=16 * KB,
     )
 
-    ds.large.append_empty((500, 500))  # 2MB, 125 chunks (uncompressed)
+    ds.large.append_empty((500, 500))  # 1MB, ~63 chunks (uncompressed)
 
     assert ds.large.shape == (1, 500, 500)
 
@@ -64,7 +64,7 @@ def test_populate_full_large_sample(local_ds_generator, compression):
         last_y = 0
         for x in range(0, 500, 50):
             for y in range(0, 500, 50):
-                patch = np.ones((50, 50), dtype=MAX_INT_DTYPE) * patch_count
+                patch = np.ones((50, 50), dtype="int32") * patch_count
                 ds.large[last_x:x, last_y:y] = patch
                 last_y = y
             last_x = x
@@ -72,9 +72,9 @@ def test_populate_full_large_sample(local_ds_generator, compression):
     ds = local_ds_generator()
 
     if compression is None:
-        assert ds.large.num_chunks == 125
+        assert ds.large.num_chunks == 64
     else:
-        assert ds.large.num_chunks < 125
+        assert ds.large.num_chunks < 63
 
     # check data
     patch_count = 0
@@ -82,7 +82,7 @@ def test_populate_full_large_sample(local_ds_generator, compression):
     last_y = 0
     for x in range(0, 500, 50):
         for y in range(0, 500, 50):
-            expected_patch = np.ones((50, 50), dtype=MAX_INT_DTYPE) * patch_count
+            expected_patch = np.ones((50, 50), dtype="int32") * patch_count
             actual_patch = ds.large[last_x:x, last_y:y].numpy()
             np.testing.assert_array_equal(expected_patch, actual_patch)
             last_y = y
