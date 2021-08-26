8da9f103ea8f829b4065087b270107d272d04149
Fariz Rahman <farizrahman4u@gmail.com>
2021-08-26 12:20:58 +0530

    possible mac fix

--- hub/core/serialize.py
+++ hub/core/serialize.py
@@ -123,7 +123,9 @@ def deserialize_chunk(
         encoded byte positions as numpy array,
         chunk data as memoryview.
     """
-    byts = memoryview(byts)
+    incoming_mview = isinstance(byts, memoryview)
+    if not incoming_mview:
+        byts = memoryview(byts)
 
     enc_dtype = np.dtype(hub.constants.ENCODING_DTYPE)
     itemsize = enc_dtype.itemsize
@@ -163,7 +165,8 @@ def deserialize_chunk(
         offset += byte_positions_nbytes
     # Read data
     data = byts[offset:]
-
+    if incoming_mview:
+        data = memoryview(bytes(data))
     return version, shape_info, byte_positions, data
 
 
