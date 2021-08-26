0998b8cf263f3e5b7f1a9ccb6bc1620dc69d5398
Fariz Rahman <farizrahman4u@gmail.com>
2021-08-26 12:39:41 +0530

    rem ba

--- hub/core/storage/lru_cache.py
+++ hub/core/storage/lru_cache.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, Set, Union
 from hub.core.storage.provider import StorageProvider
 
 
-def _get_nbytes(obj: Union[bytes, memoryview, bytearray, Cachable]):
+def _get_nbytes(obj: Union[bytes, memoryview, Cachable]):
     if isinstance(obj, Cachable):
         return obj.nbytes
     return len(obj)
@@ -87,7 +87,7 @@ class LRUCache(StorageProvider):
                 )
             return item
 
-        if isinstance(item, (bytes, memoryview, bytearray)):
+        if isinstance(item, (bytes, memoryview)):
             obj = expected_class.frombuffer(item)
 
             if isinstance(obj, CachableCallback):
