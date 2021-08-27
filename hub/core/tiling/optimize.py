8ed34e4069145f50aa2cb17914bbcc719e632150
McCrearyD <mccreary@dyllan.ai>
2021-08-27 12:48:15 -0700

    refactor to use tensor_meta as much as possible

--- hub/core/tiling/optimize.py
+++ hub/core/tiling/optimize.py
@@ -1,4 +1,5 @@
-from hub.core.compression import COMPRESSION_FACTORS, get_compression_factor
+from hub.util.tiles import approximate_num_bytes
+from hub.core.compression import get_compression_factor
 from hub.core.meta.tensor_meta import TensorMeta
 from hub.constants import KB
 from typing import Tuple
@@ -12,14 +13,17 @@ COST_THRESHOLD = 800 * KB  # TODO: make smaller with better optimizers
 INTERM_DTYPE = np.dtype(np.uint32)
 
 
-def _cost(tile_shape: Tuple[int, ...], dtype: np.dtype, tile_target_bytes: int, compression_factor: float) -> int:
+
+# def _cost(tile_shape: Tuple[int, ...], dtype: np.dtype, tile_target_bytes: int, compression_factor: float) -> int:
+def _cost(tile_shape: Tuple[int, ...], tensor_meta: TensorMeta) -> int:
     # TODO: docstring
 
-    actual_bytes = np.prod(tile_shape) * dtype.itemsize // compression_factor
-    abs_cost = abs(tile_target_bytes - actual_bytes)
+    actual_bytes = approximate_num_bytes(tile_shape, tensor_meta)
+    print("actual", actual_bytes)
+    abs_cost = abs(tensor_meta.max_chunk_size - actual_bytes)
 
     # higher cost for when `actual_bytes` is smaller than `target_bytes`
-    if actual_bytes < tile_target_bytes:
+    if actual_bytes < tensor_meta.max_chunk_size:
         return abs_cost * 2
 
     return abs_cost
@@ -27,21 +31,20 @@ def _cost(tile_shape: Tuple[int, ...], dtype: np.dtype, tile_target_bytes: int, 
 
 def _downscale(tile_shape: np.ndarray, sample_shape: Tuple[int, ...]) -> np.ndarray:
     # TODO: docstring
-    
+
     return np.minimum(tile_shape, sample_shape).astype(INTERM_DTYPE)
 
 
 def _propose_tile_shape(
-    sample_shape: Tuple[int, ...], dtype: np.dtype, max_chunk_size: int, compression_factor: float
+    sample_shape: Tuple[int, ...], tensor_meta: TensorMeta
 ) -> np.ndarray:
     # TODO: docstring (use args)
 
-    dtype_num_bytes = dtype.itemsize
+    dtype_num_bytes = np.dtype(tensor_meta.dtype).itemsize
     ndims = len(sample_shape)
 
-    # TODO: factor in sample/chunk compression (ALSO DO THIS BEFORE MERGING)
-    tile_target_entries = max_chunk_size // dtype_num_bytes
-    tile_target_entries *= compression_factor
+    tile_target_entries = tensor_meta.max_chunk_size // dtype_num_bytes
+    tile_target_entries *= get_compression_factor(tensor_meta)
     tile_target_length = int(tile_target_entries ** (1.0 / ndims))
 
     # Tile length must be at least 16, so we don't round too close to zero
@@ -51,17 +54,17 @@ def _propose_tile_shape(
 
 
 def _optimize_tile_shape(
-    sample_shape: Tuple[int, ...], dtype: np.dtype, max_chunk_size: int, compression_factor: float
+    sample_shape: Tuple[int, ...], tensor_meta: TensorMeta
 ) -> Tuple[int, ...]:
     # TODO: docstring
 
-    tile_shape = _propose_tile_shape(sample_shape, dtype, max_chunk_size, compression_factor)
+    tile_shape = _propose_tile_shape(sample_shape, tensor_meta)
     downscaled_tile_shape = _downscale(tile_shape, sample_shape)
     frozen_dims_mask = tile_shape != downscaled_tile_shape
 
     # iterate until we find a tile shape close to max chunk size
     proposed_tile_shape = downscaled_tile_shape
-    while _cost(proposed_tile_shape, dtype, max_chunk_size, compression_factor) > COST_THRESHOLD:
+    while _cost(proposed_tile_shape, tensor_meta) > COST_THRESHOLD:
         # TODO!
         
         break
@@ -70,14 +73,14 @@ def _optimize_tile_shape(
 
 
 def _validate_tile_shape(
-    tile_shape: Tuple[int, ...], dtype: np.dtype, max_chunk_size: int, compression_factor: float
+    tile_shape: Tuple[int, ...], tensor_meta: TensorMeta
 ):
     # TODO: docstring
 
-    cost = _cost(tile_shape, dtype, max_chunk_size, compression_factor)
+    cost = _cost(tile_shape, tensor_meta)
     if cost > COST_THRESHOLD:
         raise Exception(
-            f"Cost too large ({cost}) for tile shape {tile_shape} and max chunk size {max_chunk_size}"
+            f"Cost too large ({cost}) for tile shape {tile_shape} and max chunk size {tensor_meta.max_chunk_size}. Dtype={tensor_meta.dtype}"
         )  # TODO: exceptions.py
 
 
@@ -88,6 +91,6 @@ def optimize_tile_shape(sample_shape: Tuple[int, ...], tensor_meta: TensorMeta):
     max_chunk_size = tensor_meta.max_chunk_size
     compression_factor = get_compression_factor(tensor_meta)
 
-    tile_shape = _optimize_tile_shape(sample_shape, dtype, max_chunk_size, compression_factor)
-    _validate_tile_shape(tile_shape, dtype, max_chunk_size, compression_factor)
+    tile_shape = _optimize_tile_shape(sample_shape, tensor_meta)
+    _validate_tile_shape(tile_shape, tensor_meta)
     return tile_shape
