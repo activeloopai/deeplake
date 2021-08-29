d609206b28fb5073348ed072e25c4896f674d146
Fariz Rahman <farizrahman4u@gmail.com>
2021-08-29 15:08:10 +0400

    Tensor inplace ops (#1136)

--- hub/core/tensor.py
+++ hub/core/tensor.py
@@ -8,7 +8,7 @@ from hub.core.sample import Sample, SampleValue  # type: ignore
 from hub.core.chunk_engine import ChunkEngine
 from hub.api.info import load_info
 from hub.util.keys import get_tensor_meta_key, tensor_exists, get_tensor_info_key
-from hub.util.casting import get_incompatible_dtype
+from hub.util.casting import get_incompatible_dtype, intelligent_cast
 from hub.util.shape_interval import ShapeInterval
 from hub.util.exceptions import (
     TensorDoesNotExistError,
@@ -54,6 +54,18 @@ def create_tensor(
     storage[meta_key] = meta  # type: ignore
 
 
+def _inplace_op(f):
+    op = f.__name__
+
+    def inner(tensor, other):
+        tensor.chunk_engine.update(tensor.index, other, op)
+        if not tensor.index.is_trivial():
+            tensor._skip_next_setitem = True
+        return tensor
+
+    return inner
+
+
 class Tensor:
     def __init__(
         self,
@@ -87,6 +99,9 @@ class Tensor:
         self.chunk_engine = ChunkEngine(self.key, self.storage)
         self.index.validate(self.num_samples)
         self.info = load_info(get_tensor_info_key(self.key), self.storage)
+
+        # An optimization to skip multiple .numpy() calls when performing inplace ops on slices:
+        self._skip_next_setitem = False
 
     def extend(self, samples: Union[np.ndarray, Sequence[SampleValue]]):
         """Extends the end of the tensor by appending multiple elements from a sequence. Accepts a sequence, a single batched numpy array,
@@ -277,7 +292,7 @@ class Tensor:
             raise TypeError(f"Cannot infer numpy dtype for {val}")
 
     def __setitem__(self, item: Union[int, slice], value: Any):
-        """Update samples with new values. Sub-slice updates are not supported yet.
+        """Update samples with new values.
 
         Example:
             >>> tensor.append(np.zeros((10, 10)))
@@ -287,7 +302,11 @@ class Tensor:
             >>> tensor.shape
             (1, 3, 3)
         """
-
+        if isinstance(value, Tensor):
+            if value._skip_next_setitem:
+                value._skip_next_setitem = False
+                return
+            value = value.numpy(aslist=True)
         item_index = Index(item)
         self.chunk_engine.update(self.index[item_index], value)
 
@@ -323,39 +342,50 @@ class Tensor:
     def __array__(self) -> np.ndarray:
         return self.numpy()
 
-    # TODO: Inplace operatitions
+    @_inplace_op
     def __iadd__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __isub__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __imul__(self, other):
-        raise NotImplementedError
+        pass
 
-    def __idiv__(self, other):
-        raise NotImplementedError
+    @_inplace_op
+    def __itruediv__(self, other):
+        pass
 
+    @_inplace_op
     def __ifloordiv__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __imod__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __ipow__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __ilshift__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __irshift__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __iand__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __ixor__(self, other):
-        raise NotImplementedError
+        pass
 
+    @_inplace_op
     def __ior__(self, other):
-        raise NotImplementedError
+        pass
