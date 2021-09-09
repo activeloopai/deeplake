cac0e26205ad28ff9ee853a41eff17ed10b016db
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-09-09 13:51:43 +0530

    better tobytes

--- hub/core/version_control/version_chunk_list.py
+++ hub/core/version_control/version_chunk_list.py
@@ -1,15 +1,25 @@
+from typing import List
 from hub.core.storage.cachable import Cachable
 
 
 class VersionChunkList(Cachable):
     def __init__(self) -> None:
-        self.chunks = []
+        self.chunks: List[str] = []
+
+    def tobytes(self) -> bytes:
+        """Dump self.chunks in csv format"""
+        return bytes(",".join(self.chunks), "utf-8")
+
+    @classmethod
+    def frombuffer(cls, buffer: bytes):
+        """Load a VersionChunkList from a buffer"""
+        instance = cls()
+        instance.chunks = buffer.decode("utf-8").split(",")
+        return instance
 
     @property
     def nbytes(self):
-        if not self.chunks:
-            return 14
-        return 12 + (44 * len(self.chunks))
+        return 8 + ((len(self.chunks) - 1) * 9)
 
     def append(self, item):
         self.chunks.append(item)
