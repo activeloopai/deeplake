8c7c4961e3a8561a15590eeebcc2016a58a8a654
Fariz Rahman <farizrahman4u@gmail.com>
2021-09-15 23:31:53 +0400

    Update gcs.py

--- hub/core/storage/gcs.py
+++ hub/core/storage/gcs.py
@@ -5,17 +5,14 @@ import os
 import tempfile
 from typing import Dict, Union
 
-try:
-    from google.cloud import storage  # type: ignore
-    from google.api_core import retry  # type: ignore
-    from google.oauth2 import service_account  # type: ignore
-    import google.auth as gauth  # type: ignore
-    import google.auth.compute_engine  # type: ignore
-    import google.auth.credentials  # type: ignore
-    import google.auth.exceptions  # type: ignore
-    from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore
-except:
-    pass
+from google.cloud import storage  # type: ignore
+from google.api_core import retry  # type: ignore
+from google.oauth2 import service_account  # type: ignore
+import google.auth as gauth  # type: ignore
+import google.auth.compute_engine  # type: ignore
+import google.auth.credentials  # type: ignore
+import google.auth.exceptions  # type: ignore
+from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore
 from hub.core.storage.provider import StorageProvider
 from hub.util.exceptions import GCSDefaultCredsNotFoundError
 
