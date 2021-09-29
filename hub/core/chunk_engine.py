de216ec9269e1c22830f8da81348941ac0237b37
Abhinav Tuli <42538472+AbhinavTuli@users.noreply.github.com>
2021-09-29 12:34:59 +0530

    Revert "Adding shared memory support for Pytorch 3.6/3.7, speeding up pytorch integration"

--- hub/core/chunk_engine.py
+++ hub/core/chunk_engine.py
@@ -174,11 +174,11 @@ class ChunkEngine:
         return enc
 
     @property
-    def commit_chunk_set(self) -> Optional[CommitChunkSet]:
+    def commit_chunk_set(self) -> CommitChunkSet:
         """Gets the commit chunk set from cache, if one is not found it creates a blank one.
 
         Returns:
-            Optional[CommitChunkSet]: The commit chunk set keeps track of all the chunks present in the current commit, returns None for the first commit.
+            CommitChunkSet: The commit chunk set keeps track of all the chunks present in the current commit.
         """
         commit_id = self.version_state["commit_id"]
         if commit_id == FIRST_COMMIT_ID:
@@ -252,7 +252,7 @@ class ChunkEngine:
                 chunk_set = set()
             if chunk_name in chunk_set:
                 return commit_id
-            cur_node = cur_node.parent  # type: ignore
+            cur_node = cur_node.parent
         # the first commit doesn't have a commit chunk set, so any chunk that wasn't found belongs to the first commit
         return FIRST_COMMIT_ID
 
@@ -424,7 +424,7 @@ class ChunkEngine:
         if commit_id != FIRST_COMMIT_ID:
             # synchronize current chunk set, all older ones are immutable
             commit_chunk_set_key = get_tensor_commit_chunk_set_key(self.key, commit_id)
-            self.meta_cache[commit_chunk_set_key] = self.commit_chunk_set  # type: ignore
+            self.meta_cache[commit_chunk_set_key] = self.commit_chunk_set
 
     def _try_appending_to_last_chunk(
         self, buffer: memoryview, shape: Tuple[int]
