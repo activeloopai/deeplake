f3c5cebb7689d3a03230bd7e06f701976d506722
Andrii Liubimov <andrii.liubimov@gmail.com>
2021-10-13 01:25:30 -0400

    Refactor pytorch integration

--- hub/core/dataset.py
+++ hub/core/dataset.py
@@ -521,9 +521,9 @@ class Dataset:
         Returns:
             A torch.utils.data.DataLoader object.
         """
-        from hub.integrations import dataset_to_pytorch
+        from hub.integrations import dataset_to_pytorch as to_pytorch
 
-        dataloader = dataset_to_pytorch(
+        dataloader = to_pytorch(
             self,
             transform,
             tensors,
@@ -562,6 +562,7 @@ class Dataset:
             self._load_version_info()
 
         self._populate_meta()  # TODO: use the same scheme as `load_info`
+        self.read_only = self._read_only  # TODO: weird fix for dataset unpickling
         self.info = load_info(get_dataset_info_key(self.version_state["commit_id"]), self.storage, self.version_state)  # type: ignore
         self.index.validate(self.num_samples)
 
---
+++ hub/integrations/pytorch/dataset.py
@@ -0,0 +1,110 @@
+from hub.util.iterable_ordered_dict import IterableOrderedDict
+from typing import Callable, Optional, Sequence, List
+from hub.core.io import (
+    BufferedStreaming,
+    SampleStreaming,
+    Schedule,
+    ShufflingSchedulerWrapper,
+    SingleThreadScheduler,
+    MultiThreadedNativeScheduler,
+)
+import torch
+import numpy as np
+
+
+def use_scheduler(num_workers: int):
+    if num_workers <= 1:
+        return SingleThreadScheduler()
+    else:
+        return MultiThreadedNativeScheduler(num_workers)
+
+
+class TorchDataset(torch.utils.data.IterableDataset):
+    def __init__(
+        self,
+        dataset,
+        use_local_cache: bool = False,
+        tensors: Optional[Sequence[str]] = None,
+        transform: Optional[Callable] = None,
+        num_workers: int = 1,
+        shuffle: bool = False,
+        buffer_size: Optional[int] = None,
+    ) -> None:
+        super().__init__()
+
+        self.dataset = dataset
+        self.transform = transform
+        self.tensors = tensors
+        self.use_local_cache = use_local_cache
+        self.scheduler = use_scheduler(num_workers)
+
+        if shuffle:
+            self.scheduler = ShufflingSchedulerWrapper(self.scheduler)
+
+        streaming = SampleStreaming(
+            dataset,
+            scheduler=self.scheduler,
+            tensors=self.tensors,
+            use_local_cache=use_local_cache,
+        )
+
+        self.schedules: List[Schedule] = self.scheduler.schedule(
+            streaming.list_blocks()
+        )
+
+        self.shuffle: bool = shuffle
+        self.buffer_suze: Optional[int] = buffer_size
+
+    def __iter__(self):
+        worker_info = torch.utils.data.get_worker_info()
+        schedule: Schedule = self.schedules[0]
+
+        if worker_info is not None:
+            schedule = self.schedules[worker_info.id]
+
+        streaming = SampleStreaming(
+            self.dataset,
+            scheduler=self.scheduler,
+            tensors=self.tensors,
+            use_local_cache=self.use_local_cache,
+        )
+
+        if self.shuffle and self.buffer_suze is not None:
+            streaming = BufferedStreaming(streaming, self.buffer_suze)
+
+        stream = streaming.read(schedule)
+
+        yield from TorchDataset.apply_transform(
+            stream=TorchDataset.map_to_tensor(self.to_pytensor, stream),
+            transform=self.transform,
+        )
+
+    def __len__(self):
+        return sum(map(len, self.schedules))
+
+    @staticmethod
+    def apply_transform(stream, transform):
+        if transform is not None:
+            return map(transform, stream)
+        else:
+            return stream
+
+    @staticmethod
+    def map_to_tensor(map_func: Callable, stream):
+        return map(
+            lambda d: IterableOrderedDict((k, map_func(v)) for k, v in d.items()),
+            stream,
+        )
+
+    def _remap_dtype(self, tensor):
+        if tensor.dtype == np.uint16:
+            return tensor.astype(np.int32)
+        if tensor.dtype == np.uint32:
+            return tensor.astype(np.int64)
+        if tensor.dtype == np.uint64:
+            return tensor.astype(np.int64)
+
+        return tensor.copy()
+
+    def to_pytensor(self, tensor):
+        return torch.from_numpy(self._remap_dtype(tensor))
