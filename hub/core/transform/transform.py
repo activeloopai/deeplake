7510af5e94a4379418ecd9f90d579fef292b4e6b
Fariz Rahman <farizrahman4u@gmail.com>
2021-10-19 01:02:20 +0400

    mypy

--- hub/core/transform/transform.py
+++ hub/core/transform/transform.py
@@ -192,11 +192,11 @@ class Pipeline:
         if progressbar:
             thread = threading.Thread(target=_run)
             thread.start()
-            for i in tqdm.tqdm(range(len(data_in))):  # type: ignore
+            for i in tqdm.tqdm(range(len(data_in))):
                 while i + 1 > progress["value"]:
                     time.sleep(1)
                     if progress["error"]:
-                        raise progress["error"]
+                        raise progress["error"]  # type: ignore
             thread.join()
             progress_server.stop()
         else:
