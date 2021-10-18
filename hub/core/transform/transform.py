1a316cd6ff14d654626d2ec95c48314acd8e62f9
Fariz Rahman <farizrahman4u@gmail.com>
2021-10-19 02:19:14 +0400

    fix daemon thread

--- hub/core/transform/transform.py
+++ hub/core/transform/transform.py
@@ -192,13 +192,15 @@ class Pipeline:
         if progressbar:
             thread = threading.Thread(target=_run)
             thread.start()
-            for i in tqdm.tqdm(range(len(data_in))):
-                while i + 1 > progress["value"]:
-                    time.sleep(1)
-                    if progress["error"]:
-                        raise progress["error"]  # type: ignore
-            thread.join()
-            progress_server.stop()
+            try:
+                for i in tqdm.tqdm(range(len(data_in))):
+                    while i + 1 > progress["value"]:
+                        time.sleep(1)
+                        if progress["error"]:
+                            raise progress["error"]  # type: ignore
+            finally:
+                thread.join()
+                progress_server.stop()
         else:
             _run()
 
