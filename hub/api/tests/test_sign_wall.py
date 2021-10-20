8a280b48a75925205830d42456b1d5653ebea47c
McCrearyD <mccreary@dyllan.ai>
2021-10-20 17:38:29 +0400

    start signwall test

---
+++ hub/api/tests/test_sign_wall.py
@@ -0,0 +1,13 @@
+
+
+def test_sign_wall(hub_cloud_ds_generator):
+    ds = hub_cloud_ds_generator()
+
+    ds.create_tensor("images")
+    ds.images.append([1, 2, 3])
+
+    ds.info.terms_of_access = "access is only grantable to nerds"
+
+    
+
+    pass
