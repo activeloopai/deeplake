421f831d91efba25a62e6801e19fb574d0ef2e1e
McCrearyD <mccreary@dyllan.ai>
2021-10-22 15:46:15 +0400

    use environment variables for testing username, not just string literals

--- hub/constants.py
+++ hub/constants.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 
@@ -58,19 +59,23 @@ ENCODING_DTYPE = np.uint32
 # caclulate the number of bits to shift right when converting a 128-bit uuid into `ENCODING_DTYPE`
 UUID_SHIFT_AMOUNT = 128 - (8 * ENCODING_DTYPE(1).itemsize)
 
-HUB_CLOUD_DEV_USERNAME = "testingacc"
-
-PYTEST_MEMORY_PROVIDER_BASE_ROOT = "mem://hub_pytest"
-PYTEST_LOCAL_PROVIDER_BASE_ROOT = "/tmp/hub_pytest/"  # TODO: may fail for windows
-PYTEST_S3_PROVIDER_BASE_ROOT = "s3://hub-2.0-tests/"
-PYTEST_GCS_PROVIDER_BASE_ROOT = "gcs://snark-test/"
-PYTEST_HUB_CLOUD_PROVIDER_BASE_ROOT = f"hub://{HUB_CLOUD_DEV_USERNAME}/"
 
 # environment variables
+ENV_HUB_DEV_USERNAME = "ACTIVELOOP_HUB_USERNAME"
 ENV_HUB_DEV_PASSWORD = "ACTIVELOOP_HUB_PASSWORD"
 ENV_KAGGLE_USERNAME = "KAGGLE_USERNAME"
 ENV_KAGGLE_KEY = "KAGGLE_KEY"
 ENV_GOOGLE_APPLICATION_CREDENTIALS = "GOOGLE_APPLICATION_CREDENTIALS"
+
+HUB_CLOUD_DEV_USERNAME = os.getenv(ENV_HUB_DEV_USERNAME)
+HUB_CLOUD_DEV_PASSWORD = os.getenv(ENV_HUB_DEV_PASSWORD)
+
+# dataset base roots for pytests
+PYTEST_MEMORY_PROVIDER_BASE_ROOT = "mem://hub_pytest"
+PYTEST_LOCAL_PROVIDER_BASE_ROOT = "/tmp/hub_pytest/"  # TODO: may fail for windows
+PYTEST_S3_PROVIDER_BASE_ROOT = "s3://hub-2.0-tests/"
+PYTEST_GCS_PROVIDER_BASE_ROOT = "gcs://snark-test/"
+PYTEST_HUB_CLOUD_PROVIDER_BASE_ROOT = None if HUB_CLOUD_DEV_USERNAME is None else f"hub://{HUB_CLOUD_DEV_USERNAME}/"
 
 # pytest options
 MEMORY_OPT = "--memory-skip"
