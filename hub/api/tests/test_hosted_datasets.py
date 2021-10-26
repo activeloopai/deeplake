0620e822849de7fe014a15a789988fd2b9bcf0ac
McCrearyD <mccreary@dyllan.ai>
2021-10-26 11:56:14 +0400

    black

--- hub/api/tests/test_hosted_datasets.py
+++ hub/api/tests/test_hosted_datasets.py
@@ -1,9 +1,5 @@
-
-
-
 def test_mnist():
     ds = hub.load("hub://activeloop/mnist-test")
     sample = ds[0]
     assert sample.images.numpy().shape == (28, 28)
     assert sample.labels.numpy().shape == (1,)
-
