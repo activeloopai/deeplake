3a470d6b31bf5bcbbee161901e8cc3067dc732d6
FayazRahman <fayazrahman4u@gmail.com>
2021-11-02 00:03:25 +0530

    fix test

--- hub/api/tests/test_api.py
+++ hub/api/tests/test_api.py
@@ -456,7 +456,7 @@ def test_htype(memory_ds: Dataset):
     image = memory_ds.create_tensor("image", htype="image", sample_compression="png")
     bbox = memory_ds.create_tensor("bbox", htype="bbox")
     label = memory_ds.create_tensor("label", htype="class_label")
-    video = memory_ds.create_tensor("video", htype="video")
+    video = memory_ds.create_tensor("video", htype="video", sample_compression="mkv")
     bin_mask = memory_ds.create_tensor("bin_mask", htype="binary_mask")
     segment_mask = memory_ds.create_tensor("segment_mask", htype="segment_mask")
     keypoints_coco = memory_ds.create_tensor("keypoints_coco", htype="keypoints_coco")
@@ -671,6 +671,7 @@ def test_invalid_tensor_name(memory_ds):
 def test_compressions_list():
     assert hub.compressions == [
         "apng",
+        "avi",
         "bmp",
         "dib",
         "flac",
@@ -679,7 +680,9 @@ def test_compressions_list():
         "jpeg",
         "jpeg2000",
         "lz4",
+        "mkv",
         "mp3",
+        "mp4",
         "pcx",
         "png",
         "ppm",
--- hub/compression.py
+++ hub/compression.py
@@ -2,6 +2,7 @@
 Supported compressions (formats):
     Image : bmp, dib, gif, ico, jpeg, jp2, pcx, png, ppm, sgi, tga, tiff, webp, wmf, xbm
     Audio : flac, mp3, wav
+    Video : mp4, mkv, avi
     Bytes : lz4
 
 """
