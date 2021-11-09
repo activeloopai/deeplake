4d3a1dcd44932b71b0aed466cc6a322d4ab7eea7
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-11-09 15:41:11 +0530

    cleanup uncompressed chunk

--- hub/core/chunk/uncompressed_chunk.py
+++ hub/core/chunk/uncompressed_chunk.py
@@ -3,7 +3,6 @@ from hub.core.sample import Sample
 from hub.core.serialize import (
     check_sample_shape,
     bytes_to_text,
-    check_sample_size,
 )
 from hub.core.tiling.tile import SampleTiles
 from hub.util.casting import intelligent_cast
@@ -15,9 +14,6 @@ SerializedOutput = tuple[bytes, Optional[tuple]]
 
 
 class UncompressedChunk(BaseChunk):
-    def needs_to_be_tiled(self, nbytes) -> bool:
-        return nbytes > self.min_chunk_size
-
     def extend_if_has_space(
         self, incoming_samples: Union[List[Union[bytes, Sample, np.array]], np.array]
     ) -> int:
@@ -29,13 +25,12 @@ class UncompressedChunk(BaseChunk):
     def _extend_if_has_space_numpy(self, incoming_samples: np.array):
         num_samples = 0
         buffer_size = 0
-        for incoming_sample in incoming_samples:
-            shape = incoming_sample.shape
-            shape = self.normalize_shape(shape)
+
+        for sample in incoming_samples:
+            shape = self.normalize_shape(sample.shape)
             self.num_dims = self.num_dims or len(shape)
-            sample_nbytes = incoming_sample.nbytes
+            sample_nbytes = sample.nbytes
             check_sample_shape(shape, self.num_dims)
-            check_sample_size(sample_nbytes, self.min_chunk_size, self.compression)
             if not self.can_fit_sample(sample_nbytes, buffer_size):
                 break
             buffer_size += sample_nbytes
@@ -45,7 +40,7 @@ class UncompressedChunk(BaseChunk):
         self.data_bytes += samples.tobytes()
 
         if num_samples > 0:
-            shape = samples[0].shape if len(samples[0].shape) > 0 else (1,)
+            shape = self.normalize_shape(samples[0].shape)
             sample_nbytes = samples[0].nbytes
             for _ in range(num_samples):
                 self.register_in_meta_and_headers(sample_nbytes, shape)
@@ -62,19 +57,10 @@ class UncompressedChunk(BaseChunk):
             )
             self.num_dims = self.num_dims or len(shape)
             check_sample_shape(shape, self.num_dims)
-            # check_sample_size(sample_nbytes, self.min_chunk_size, self.compression)
             if isinstance(serialized_sample, SampleTiles):
                 incoming_samples[i] = serialized_sample
                 if not self.data_bytes:
-                    data = serialized_sample.yield_tile()
-                    sample_nbytes = len(data)
-                    self.data_bytes = data
-                    tile_shape = serialized_sample.tile_shape
-                    update_meta = serialized_sample.is_first_write()
-                    self.register_sample_to_headers(sample_nbytes, tile_shape)
-                    if update_meta:
-                        self.tensor_meta.length += 1
-                        self.tensor_meta.update_shape_interval(shape)
+                    self.write_tile(serialized_sample)
                     num_samples += 0.5
                 break
             else:
@@ -85,6 +71,17 @@ class UncompressedChunk(BaseChunk):
                 self.register_in_meta_and_headers(sample_nbytes, shape)
                 num_samples += 1
         return num_samples
+
+    def write_tile(self, sample: SampleTiles):
+        data = sample.yield_tile()
+        sample_nbytes = len(data)
+        self.data_bytes = data
+        tile_shape = sample.tile_shape
+        update_meta = sample.is_first_write
+        self.register_sample_to_headers(sample_nbytes, tile_shape)
+        if update_meta:
+            self.tensor_meta.length += 1
+            self.tensor_meta.update_shape_interval(sample.sample_shape)
 
     def read_sample(
         self, local_sample_index: int, cast: bool = True, copy: bool = False
