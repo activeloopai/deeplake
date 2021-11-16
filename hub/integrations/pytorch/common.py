33a74249a13b2c3bba0c9dac194ee8ef4109248a
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-11-16 20:13:01 +0530

    test fix

--- hub/integrations/pytorch/common.py
+++ hub/integrations/pytorch/common.py
@@ -6,20 +6,23 @@ def collate_fn(batch):
     import torch
 
     elem = batch[0]
-    if elem and isinstance(elem, np.ndarray) and isinstance(elem[0], str):
-        batch = [it[0] for it in batch]
+
     if isinstance(elem, IterableOrderedDict):
         return IterableOrderedDict(
             (key, collate_fn([d[key] for d in batch])) for key in elem.keys()
         )
+
+    if isinstance(elem, np.ndarray) and elem.size > 0 and isinstance(elem[0], str):
+        batch = [it[0] for it in batch]
     return torch.utils.data._utils.collate.default_collate(batch)
 
 
 def convert_fn(data):
     import torch
 
-    if data and isinstance(data, np.ndarray) and isinstance(data[0], str):
-        data = data[0]
     if isinstance(data, IterableOrderedDict):
         return IterableOrderedDict((k, convert_fn(v)) for k, v in data.items())
+    if isinstance(data, np.ndarray) and data.size > 0 and isinstance(data[0], str):
+        data = data[0]
+
     return torch.utils.data._utils.collate.default_convert(data)
