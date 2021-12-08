8ad1bd30f50b76c5259ae5da857f6f25658e608f
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-12-08 19:17:19 +0530

    remove unused variable

--- hub/util/encoder.py
+++ hub/util/encoder.py
@@ -190,6 +190,5 @@ def reset_cachables(target_ds: hub.Dataset) -> None:
         chunk_engine._tensor_meta = None
         chunk_engine._chunk_id_encoder = None
         chunk_engine._tile_encoder = None
-        chunk_engine._max_chunk_size = None
         chunk_engine._commit_chunk_set = None
         chunk_engine._commit_diff = None
