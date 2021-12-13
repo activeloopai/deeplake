4c8646d2c49aeadf79488b9ca80dcef8759d1cbc
Fariz Rahman <farizrahman4u@gmail.com>
2021-12-13 23:10:51 +0530

    format

--- hub/core/tensor.py
+++ hub/core/tensor.py
@@ -461,7 +461,6 @@ class Tensor:
             return self.numpy()
 
     def tobytes(self) -> bytes:
-        if self.index.values[0].subscriptable() or len(self.index.values) > 1:
         """Returns the bytes of the tensor. Only works for a single sample of tensor.
         If the tensor is uncompressed, this returns the bytes of the numpy array.
         If the tensor is sample compressed, this returns the compressed bytes of the sample.
@@ -473,7 +472,6 @@ class Tensor:
         Raises:
             ValueError: If the tensor has multiple samples.
         """
-
-        if self.index.values[0].subscriptable():
+        if self.index.values[0].subscriptable() or len(self.index.values) > 1:
             raise ValueError("tobytes() can be used only on exatcly 1 sample.")
         return self.chunk_engine.read_bytes_for_sample(self.index.values[0].value)  # type: ignore
