9483291e2113bb8c1a65949c8cddc6d42fc9f9b6
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-12-16 21:04:09 +0530

    add pseduo parent to commit_node

--- hub/core/version_control/commit_node.py
+++ hub/core/version_control/commit_node.py
@@ -11,6 +11,7 @@ class CommitNode:
         self.branch = branch
         self.children: List["CommitNode"] = []
         self.parent: Optional["CommitNode"] = None
+        self.pseudo_parents = []
         self.commit_message: Optional[str] = None
         self.commit_time: Optional[datetime] = None
         self.commit_user_name: Optional[str] = None
@@ -30,5 +31,13 @@ class CommitNode:
 
     def __repr__(self) -> str:
         return f"Commit : {self.commit_id} ({self.branch}) \nAuthor : {self.commit_user_name}\nTime   : {str(self.commit_time)[:-7]}\nMessage: {self.commit_message}"
+
+    def __getstate__(self) -> dict:
+        return self.__dict__
+
+    def __setstate__(self, state: dict):
+        self.__dict__.update(state)
+        if "pseudo_parents" not in state:
+            self.pseudo_parents = []
 
     __str__ = __repr__
