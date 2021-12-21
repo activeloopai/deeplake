3d1032a0a0f17b9046cb2ce84235cc93a0ff8167
Fariz Rahman <farizrahman4u@gmail.com>
2021-12-21 22:34:11 +0530

    format

--- hub/core/storage/__init__.py
+++ hub/core/storage/__init__.py
@@ -3,5 +3,4 @@ from hub.core.storage.s3 import S3Provider
 from hub.core.storage.memory import MemoryProvider
 from hub.core.storage.local import LocalProvider
 from hub.core.storage.lru_cache import LRUCache
-# from hub.core.storage.gcs import GCSProvider
-GCSProvider = S3Provider
+from hub.core.storage.gcs import GCSProvider
