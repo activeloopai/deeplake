6ae2bb02a0ab932977e8d9c74430d523a332689a
Fariz Rahman <farizrahman4u@gmail.com>
2021-12-28 21:01:40 +0530

    fix test

--- hub/api/tests/test_video.py
+++ hub/api/tests/test_video.py
@@ -8,8 +8,10 @@ from hub.core.dataset import Dataset
 @enabled_datasets
 @pytest.mark.parametrize("compression", hub.compression.VIDEO_COMPRESSIONS)
 def test_video(ds: Dataset, compression, video_paths):
-    for path in video_paths[compression]:
-        ds.create_tensor("video", htype="video", sample_compression=compression)
+    for i, path in enumerate(video_paths[compression]):
+        tensor = ds.create_tensor(
+            f"video_{i}", htype="video", sample_compression=compression
+        )
         sample = hub.read(path)
         assert len(sample.shape) == 4
         if compression in ("mp4", "mkv"):
@@ -19,7 +21,7 @@ def test_video(ds: Dataset, compression, video_paths):
         assert sample.shape[-1] == 3
         with ds:
             for _ in range(5):
-                ds.video.append(hub.read(path))  # type: ignore
-            ds.video.extend([hub.read(path) for _ in range(5)])  # type: ignore
+                tensor.append(hub.read(path))  # type: ignore
+            tensor.extend([hub.read(path) for _ in range(5)])  # type: ignore
         for i in range(10):
-            assert ds.video[i].numpy().shape == sample.shape  # type: ignore
+            assert tensor[i].numpy().shape == sample.shape  # type: ignore
