d01b8314b64b195ea93c75091f35b137774181f6
Fariz Rahman <farizrahman4u@gmail.com>
2021-12-29 21:08:40 +0530

    del old pt

--- hub/integrations/tests/test_pytorch.py
+++ hub/integrations/tests/test_pytorch.py
@@ -11,7 +11,6 @@ from hub.core.dataset import Dataset
 from hub.core.storage.memory import MemoryProvider
 from hub.constants import KB
 
-from hub.integrations.pytorch.pytorch_old import dataset_to_pytorch
 from hub.tests.dataset_fixtures import enabled_datasets
 
 
@@ -243,37 +242,6 @@ def test_pytorch_with_compression(ds: Dataset):
 
 @requires_torch
 @enabled_datasets
-def test_pytorch_small_old(ds):
-    with ds:
-        ds.create_tensor("image", max_chunk_size=PYTORCH_TESTS_MAX_CHUNK_SIZE)
-        ds.image.extend(np.array([i * np.ones((10, 10)) for i in range(256)]))
-        ds.create_tensor("image2", max_chunk_size=PYTORCH_TESTS_MAX_CHUNK_SIZE)
-        ds.image2.extend(np.array([i * np.ones((12, 12)) for i in range(256)]))
-
-    if isinstance(get_base_storage(ds.storage), MemoryProvider):
-        with pytest.raises(DatasetUnsupportedPytorch):
-            dl = dataset_to_pytorch(
-                ds, num_workers=0, batch_size=1, python_version_warning=False
-            )
-        return
-
-    # .pytorch will automatically switch depending on version, this syntax is being used to ensure testing of old code on Python 3.8
-    dl = dataset_to_pytorch(
-        ds, num_workers=0, batch_size=1, python_version_warning=False
-    )
-
-    for _ in range(2):
-        for i, batch in enumerate(dl):
-            np.testing.assert_array_equal(
-                batch["image"].numpy(), i * np.ones((1, 10, 10))
-            )
-            np.testing.assert_array_equal(
-                batch["image2"].numpy(), i * np.ones((1, 12, 12))
-            )
-
-
-@requires_torch
-@enabled_datasets
 def test_custom_tensor_order(ds):
     with ds:
         tensors = ["a", "b", "c", "d"]
@@ -281,49 +249,34 @@ def test_custom_tensor_order(ds):
             ds.create_tensor(t, max_chunk_size=PYTORCH_TESTS_MAX_CHUNK_SIZE)
             ds[t].extend(np.random.random((3, 4, 5)))
 
-    if isinstance(get_base_storage(ds.storage), MemoryProvider):
-        with pytest.raises(DatasetUnsupportedPytorch):
-            dl = dataset_to_pytorch(
-                ds, num_workers=0, tensors=["c", "d", "a"], python_version_warning=False
-            )
-        return
-
     with pytest.raises(TensorDoesNotExistError):
         dl = ds.pytorch(num_workers=0, tensors=["c", "d", "e"])
-    with pytest.raises(TensorDoesNotExistError):
-        dl = dataset_to_pytorch(
-            ds, num_workers=0, tensors=["c", "e"], python_version_warning=False
-        )
 
-    dl_new = ds.pytorch(num_workers=0, tensors=["c", "d", "a"])
-    dl_old = dataset_to_pytorch(
-        ds, num_workers=0, tensors=["c", "d", "a"], python_version_warning=False
-    )
+    dl = ds.pytorch(num_workers=0, tensors=["c", "d", "a"])
 
-    for dl in [dl_new, dl_old]:
-        for i, batch in enumerate(dl):
-            c1, d1, a1 = batch
-            a2 = batch["a"]
-            c2 = batch["c"]
-            d2 = batch["d"]
-            assert "b" not in batch
-            np.testing.assert_array_equal(a1, a2)
-            np.testing.assert_array_equal(c1, c2)
-            np.testing.assert_array_equal(d1, d2)
-            np.testing.assert_array_equal(a1[0], ds.a.numpy()[i])
-            np.testing.assert_array_equal(c1[0], ds.c.numpy()[i])
-            np.testing.assert_array_equal(d1[0], ds.d.numpy()[i])
-            batch = pickle.loads(pickle.dumps(batch))
-            c1, d1, a1 = batch
-            a2 = batch["a"]
-            c2 = batch["c"]
-            d2 = batch["d"]
-            np.testing.assert_array_equal(a1, a2)
-            np.testing.assert_array_equal(c1, c2)
-            np.testing.assert_array_equal(d1, d2)
-            np.testing.assert_array_equal(a1[0], ds.a.numpy()[i])
-            np.testing.assert_array_equal(c1[0], ds.c.numpy()[i])
-            np.testing.assert_array_equal(d1[0], ds.d.numpy()[i])
+    for i, batch in enumerate(dl):
+        c1, d1, a1 = batch
+        a2 = batch["a"]
+        c2 = batch["c"]
+        d2 = batch["d"]
+        assert "b" not in batch
+        np.testing.assert_array_equal(a1, a2)
+        np.testing.assert_array_equal(c1, c2)
+        np.testing.assert_array_equal(d1, d2)
+        np.testing.assert_array_equal(a1[0], ds.a.numpy()[i])
+        np.testing.assert_array_equal(c1[0], ds.c.numpy()[i])
+        np.testing.assert_array_equal(d1[0], ds.d.numpy()[i])
+        batch = pickle.loads(pickle.dumps(batch))
+        c1, d1, a1 = batch
+        a2 = batch["a"]
+        c2 = batch["c"]
+        d2 = batch["d"]
+        np.testing.assert_array_equal(a1, a2)
+        np.testing.assert_array_equal(c1, c2)
+        np.testing.assert_array_equal(d1, d2)
+        np.testing.assert_array_equal(a1[0], ds.a.numpy()[i])
+        np.testing.assert_array_equal(c1[0], ds.c.numpy()[i])
+        np.testing.assert_array_equal(d1[0], ds.d.numpy()[i])
 
     dls = ds.pytorch(num_workers=0, tensors=["c", "d", "a"])
     for i, batch in enumerate(dls):
@@ -359,11 +312,6 @@ def test_readonly_with_two_workers(local_ds):
 
     ptds = ds.pytorch(num_workers=2)
     # no need to check input, only care that readonly works
-    for _ in ptds:
-        pass
-
-    ptds = dataset_to_pytorch(ds)
-
     for _ in ptds:
         pass
 
