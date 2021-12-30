2a36b555713bd426cc6c950a396f6e926ef352a5
Fariz Rahman <farizrahman4u@gmail.com>
2021-12-30 22:06:06 +0530

    darg fix

--- hub/core/dataset/dataset.py
+++ hub/core/dataset/dataset.py
@@ -1475,7 +1475,7 @@ class Dataset:
                 the VDS is stored under `.queries` subdirectory of the source dataset's storage. If the user doesn't have
                 write access to the source dataset and the source dataset is a hub cloud dataset, then the VDS is stored
                 is stored under the user's hub account and can be accessed using hub.load(f"hub://{username}/queries/{query_hash}").
-            _ret_ds (Optional, str): If True, the VDS is retured as such without converting it to a view. If False, the VDS path is returned.
+            _ret_ds bool: If True, the VDS is retured as such without converting it to a view. If False, the VDS path is returned.
                 Default False.
             ds_args (dict): Additional args for creating VDS when path is specified. (See documentation for `hub.dataset()`)
 
@@ -1513,6 +1513,9 @@ class Dataset:
 
     def _get_view(self):
         """Returns a view for this VDS. Only works if this Dataset is a virtual dataset.
+
+        Returns:
+            A view of the source dataset based on the indices from VDS.
 
         Raises:
             Exception: If this is not a VDS.
