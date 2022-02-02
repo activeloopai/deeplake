26dc6d600ab1c3bb91da9491072a9a61f2f6d407
Andrii Liubimov <andrii.liubimov@gmail.com>
2022-02-01 20:14:54 -0500

    Fix of race condition

--- hub/core/compute/process.py
+++ hub/core/compute/process.py
@@ -2,7 +2,6 @@ import warnings
 from hub.core.compute.provider import ComputeProvider, SharedValue
 from pathos.pools import ProcessPool  # type: ignore
 from pathos.helpers import mp as pathos_multiprocess  # type: ignore
-import ctypes
 
 
 class ProcessProvider(ComputeProvider):
@@ -22,6 +21,7 @@ class ProcessProvider(ComputeProvider):
         self.pool.close()
         self.pool.join()
         self.pool.clear()
+        self._manager.shutdown()
         self._closed = True
 
     def __del__(self):
@@ -35,10 +35,19 @@ class ProcessProvider(ComputeProvider):
 class ManagedValue(SharedValue):
     def __init__(self, manager) -> None:
         super().__init__()
-        self._val = manager.Value(ctypes.c_uint64, 0)
+        self._val = manager.Value("l", 0)
+        self.set(0)
 
     def set(self, val) -> None:
         self._val.value = val
 
     def get(self):
-        return self._val.value
+        # python have a race condition here
+        # https://bugs.python.org/issue40402
+        # suggested fix: try/catch
+        while True:
+            try:
+                val = self._val.value
+                return val
+            except:
+                continue
--- hub/core/compute/thread.py
+++ hub/core/compute/thread.py
@@ -1,7 +1,6 @@
 from multiprocessing import Manager
 from hub.core.compute.provider import ComputeProvider, SharedValue
 from pathos.pools import ThreadPool  # type: ignore
-import ctypes
 
 
 class ThreadProvider(ComputeProvider):
@@ -20,15 +19,25 @@ class ThreadProvider(ComputeProvider):
         self.pool.close()
         self.pool.join()
         self.pool.clear()
+        self._manager.shutdown()
 
 
 class ManagedValue(SharedValue):
     def __init__(self, manager) -> None:
         super().__init__()
-        self._val = manager.Value(ctypes.c_uint64, 0)
+        self._val = manager.Value("l", 0)
+        self.set(0)
 
     def set(self, val) -> None:
         self._val.value = val
 
     def get(self):
-        return self._val.value
+        # python have a race condition here
+        # https://bugs.python.org/issue40402
+        # suggested fix: try/catch
+        while True:
+            try:
+                val = self._val.value
+                return val
+            except:
+                continue
