9287c970c259a80019c2afd8b8fcc02b8b256421
Fariz Rahman <farizrahman4u@gmail.com>
2022-02-21 20:23:56 +0530

    more headers

--- hub/core/video_streaming.py
+++ hub/core/video_streaming.py
@@ -218,7 +218,7 @@ def stream_video(chunk_id, sample_id):
         resp.headers.add("Accept-Ranges", "bytes")
         resp.headers.add(
             "Content-Range",
-            "bytes {0}-{1}/{2}".format(start, start + length - 1, file_size),
+            "bytes {0}-{1}/{2}".format(start, start + length - 1, "*"),
         )
         return resp
     except Exception as e:
