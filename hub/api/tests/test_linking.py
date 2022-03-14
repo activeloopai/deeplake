5f8efa34109e459e7f843ea8bd393f2c29747e13
Fariz Rahman <farizrahman4u@gmail.com>
2022-03-14 15:15:17 +0530

    update test

--- hub/api/tests/test_linking.py
+++ hub/api/tests/test_linking.py
@@ -28,3 +28,22 @@ def test_linking_sequence(memory_ds):
             assert len(ds.x) == len(ds.x_id) == 10
             np.testing.assert_array_equal(ds.x_id.numpy(), np.zeros((10, 1)))
 
+
+def test_linking_sequence_update(memory_ds):
+    ds = memory_ds
+    with ds:
+        ds.create_tensor("x", htype="sequence")
+        ds.create_tensor("x_id")
+        id_f = lambda _: 0
+        id_f2 = lambda *_: 1  # updated samples will have x_id=1
+        with LinkTransformTestContext(id_f, "id"):
+            with LinkTransformTestContext(id_f2, "id2"):
+                ds._link_tensors("x", "x_id", append_f="id", update_f="id2", flatten_sequence=False)
+                ds.x.extend(np.random.random((10, 5, 3, 2)))
+                ds.x[0] += 1
+                ds.x[3] += 1
+                expected = np.zeros((10, 1))
+                expected[0] = 1
+                expected[3] = 1
+                np.testing.assert_array_equal(ds.x_id.numpy(), expected)
+
