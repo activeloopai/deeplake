fdd72f9fd32b47fd49091b5bf1012a47633e2738
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-03-19 12:42:46 +0530

    mypy fix

--- hub/util/generate_id.py
+++ hub/util/generate_id.py
@@ -2,6 +2,6 @@ from uuid import uuid4
 import numpy as np
 
 
-def generate_id(dtype: np.dtype):
+def generate_id(dtype: type):
     shift = 128 - (8 * dtype(1).itemsize)
     return dtype(uuid4().int >> shift)
