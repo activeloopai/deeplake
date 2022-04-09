cc2dd53eebb954da4624a6af9d380c9e3f56a96c
david.buniatyan@gmail.com <david.buniatyan@gmail.com>
2022-04-09 12:12:22 -0700

    simple request passing bytes to python

--- hub/pheonix/libpheonix.cpp
+++ hub/pheonix/libpheonix.cpp
@@ -17,12 +17,13 @@ auto simple_request(int a) {
   std::string readBuffer;
 
   curl = curl_easy_init();
-
-  curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-  res = curl_easy_perform(curl);
-  curl_easy_cleanup(curl);
+  if (curl){
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+  }
 
   return py::bytes(readBuffer);
 }
