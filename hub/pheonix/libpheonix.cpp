82224f9996ea8531e904e7fc4ece97a5780b057c
david.buniatyan@gmail.com <david.buniatyan@gmail.com>
2022-04-09 11:44:51 -0700

    simple request working

---
+++ hub/pheonix/libpheonix.cpp
@@ -0,0 +1,35 @@
+#include <torch/extension.h>
+
+#include <iostream>
+#include <vector>
+
+#include <curl/curl.h>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+auto simple_request(int a) {
+  CURL *curl;
+  CURLcode res;
+  std::string readBuffer;
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    std::cout << readBuffer << std::endl;
+  }
+  return 0;
+}
+
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("simple_request", &simple_request, "simple request");
+}
---
+++ hub/pheonix/main.py
@@ -0,0 +1,11 @@
+from torch.utils.cpp_extension import load
+import os
+
+module_path = os.path.dirname(__file__)
+
+pheonix_cpp = load(name="pheonix_cpp", 
+                   extra_ldflags=["-lcurl"], 
+                   sources=[os.path.join(module_path, "libpheonix.cpp")], verbose=True)
+
+import pheonix_cpp
+print(pheonix_cpp.simple_request(1))
