3e059acd7f86fbceb32a2d3a013a16db78c36be0
david.buniatyan@gmail.com <david.buniatyan@gmail.com>
2022-04-11 16:55:28 -0700

    oroutines compiled and readme

--- hub/pheonix/libpheonix.cpp
+++ hub/pheonix/libpheonix.cpp
@@ -2,8 +2,42 @@
 
 #include <iostream>
 #include <vector>
+
 #include <coroutine>
 #include <curl/curl.h>
+
+
+#include <concepts>
+#include <coroutine>
+#include <exception>
+#include <iostream>
+
+
+struct ReturnObject {
+  struct promise_type {
+    ReturnObject get_return_object() { return {}; }
+    std::suspend_never initial_suspend() { return {}; }
+    std::suspend_never final_suspend() noexcept { return {}; }
+    void unhandled_exception() {}
+  };
+};
+
+struct Awaiter {
+  std::coroutine_handle<> *hp_;
+  constexpr bool await_ready() const noexcept { return false; }
+  void await_suspend(std::coroutine_handle<> h) { *hp_ = h; }
+  constexpr void await_resume() const noexcept {}
+};
+
+ReturnObject
+counter(std::coroutine_handle<> *continuation_out)
+{
+  Awaiter a{continuation_out};
+  for (unsigned i = 0;; ++i) {
+    co_await a;
+    std::cout << "counter: " << i << std::endl;
+  }
+}
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -15,6 +49,15 @@ auto simple_request(int a) {
   CURL *curl;
   CURLcode res;
   std::string readBuffer;
+
+  std::coroutine_handle<> h;
+  counter(&h);
+  for (int i = 0; i < 3; ++i) {
+    std::cout << "In main1 function\n";
+    h();
+  }
+  h.destroy();
+
 
   curl = curl_easy_init();
   if (curl){
@@ -29,6 +72,9 @@ auto simple_request(int a) {
 }
 
 
+
+
+// try hunter gate or use cmake list
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("simple_request", &simple_request, "simple request");
 }
---
+++ hub/pheonix/readme.md
@@ -0,0 +1,15 @@
+
+
+
+### Setup Compiler
+
+Need gcc to be 10 to support coroutines and c++20. Why not clang? because pytorch is compiled using gcc so for now we will use gcc-10
+```
+sudo apt install gcc-10 g++-10
+```
+then point c++ to gcc-10
+
+```
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 1000 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+```
+
