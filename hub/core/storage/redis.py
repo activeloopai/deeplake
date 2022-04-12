23b22feaf89fabe129ca48262b93bcc41f753a8e
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-04-12 10:39:31 +0530

    redis-client

--- hub/core/storage/redis.py
+++ hub/core/storage/redis.py
@@ -1,18 +1,17 @@
-import os
-import pathlib
-import pickle
-import posixpath
-import shutil
-from typing import Optional, Set
-from redis import StrictRedis
+from ctypes import Union
+from sqlite3 import DataError
+from typing import Any, Awaitable, Iterable
+from redis.typing import (
+    BitfieldOffsetT,
+    KeyT
+)
+ResponseT = Union[Awaitable, Any]
+from .helpers import list_or_args
+from yaml import KeyToken
 from config import redis_url
 import redis
-from common.redis_client import get_redis_client
-from hub.core.storage.provider import StorageProvider
-from hub.util.exceptions import DirectoryAtPathException, FileAtPathException, S3SetError
 
-
-class LocalProvider(StorageProvider):
+class ReadisProvider(redis_url):
     """Provider class for using the local filesystem."""
 
     def __init__(self, redis_url):
@@ -24,132 +23,266 @@ class LocalProvider(StorageProvider):
         
         self.r = redis.StrictRedis(url = redis_url, charset = "utf-8", decode_responses = True)
 
-    def __getitem__(self, path: str):
-        """Gets the object present at the path within the given byte range.
-
-        Args:
-            path (str): The path relative to the root of the provider.
-
-        Returns:
-            bytes: The bytes of the object present at the path.
-
-        Raises:
-            KeyError: If an object is not found at the path.
-            DirectoryAtPathException: If a directory is found at the path.
-            Exception: Any other exception encountered while trying to fetch the object.
+    def set(self, fmt: str, offset: BitfieldOffsetT, value: int):
         """
-        try:
-            pickled_value = super(LocalProvider, self).get(path)
-            if pickled_value is None:
-                return None
-            return pickle.loads(pickled_value)
-        except DirectoryAtPathException:
-            raise
-        except FileNotFoundError:
-            raise KeyError(path)
-
-    def __setitem__(self, path: str, value: bytes):
-        """Sets the object present at the path with the value
-
-        Args:
-            path (str): the path relative to the root of the provider.
-            value (bytes): the value to be assigned at the path.
-
-        Raises:
-            Exception: If unable to set item due to directory at path or permission or space issues.
-            FileAtPathException: If the directory to the path is a file instead of a directory.
-            ReadOnlyError: If the provider is in read-only mode.
+        Set the value of a given bitfield.
+        :param fmt: format-string for the bitfield being read, e.g. 'u8' for
+            an unsigned 8-bit integer.
+        :param offset: offset (in number of bits). If prefixed with a
+            '#', this is an offset multiplier, e.g. given the arguments
+            fmt='u8', offset='#2', the offset will be 16.
+        :param int value: value to set at the given position.
+        :returns: a :py:class:`BitFieldOperation` instance.
         """
-        self.check_readonly()
-        full_path = self._check_is_file(path)
-        try:
-            cache = get_redis_client()
-            for k,v in path, value:
-                kw = {path: value}
-                cache.mset(kw)
-        except Exception as err:
-            raise S3SetError(err)
-
-    def __delitem__(self, path: str):
-        """Delete the object present at the path.
-
-        Args:
-            path (str): the path to the object relative to the root of the provider.
-
-        Raises:
-            KeyError: If an object is not found at the path.
-            DirectoryAtPathException: If a directory is found at the path.
-            Exception: Any other exception encountered while trying to fetch the object.
-            ReadOnlyError: If the provider is in read-only mode.
+        self.operations.append(("SET", fmt, offset, value))
+        return self
+    
+    def get(self, fmt: str, offset: BitfieldOffsetT):
         """
-        self.check_readonly()
-        try:
-            self.rr.delete(path)
-        except DirectoryAtPathException:
-            raise
-        except FileNotFoundError:
-            raise KeyError
-
-    def __iter__(self):
-        """Generator function that iterates over the keys of the provider.
-
-        Yields:
-            str: the path of the object that it is iterating over, relative to the root of the provider.
+        Get the value of a given bitfield.
+        :param fmt: format-string for the bitfield being read, e.g. 'u8' for
+            an unsigned 8-bit integer.
+        :param offset: offset (in number of bits). If prefixed with a
+            '#', this is an offset multiplier, e.g. given the arguments
+            fmt='u8', offset='#2', the offset will be 16.
+        :returns: a :py:class:`BitFieldOperation` instance.
         """
-        yield from self._all_keys()
+        self.operations.append(("GET", fmt, offset))
+        return self
 
-    def __len__(self):
-        """Returns the number of files present inside the root of the provider.
-
-        Returns:
-            int: the number of files present inside the root.
+    def deluser(self, *username: str, **kwargs) -> ResponseT:
         """
-        keys = redis.allkeys('*')
-        for key in keys:
-            type = redis.type(key)
-            if type == "string":
-                val = redis.get(key)
-            if type == "hash":
-                vals = redis.hgetall(key)
-            if type == "zset":
-                vals = redis.zrange(key, 0, -1)
-            if type == "list":
-                vals = redis.lrange(key, 0, -1)
-            if type == "set":
-                vals = redis.smembers(key)
-        return vals
+        Delete the ACL for the specified ``username``s
 
-    def _all_keys(self, refresh: bool = False) -> Set[str]:
-        """Lists all the objects present at the root of the Provider.
-
-        Args:
-            refresh (bool): refresh keys
-
-        Returns:
-            set: set of all the objects found at the root of the Provider.
+        For more information check https://redis.io/commands/acl-deluser
         """
-        if self.files is None or refresh:
-            p = self.r.pipeline()
-            for key in self.r.keys():
-                p.hgetall(key)
-            key_set = set()
-            for h in p.execute():
-                key_set.add(h)
-            return key_set
+        return self.execute_command("ACL DELUSER", *username, **kwargs)
 
-    def clear(self, ns: str):
-        """Deletes ALL data on the local machine (under self.root). Exercise caution!"""
-        """:param ns: str, namespace i.e your:prefix"""
-        cache = StrictRedis()
-        CHUNK_SIZE = 5000
-        self.check_readonly()
-        cursor = '0'
-        ns_keys = ns + '*'
-        while cursor != 0:
-            cursor, keys = cache.scan(cursor=cursor, match=ns_keys, count=CHUNK_SIZE)
-            if keys:
-                cache.delete(*keys)
-        return True
+    def getuser(self, username: str, **kwargs) -> ResponseT:
+        """
+        Get the ACL details for the specified ``username``.
 
-    def __contains__(self, key) -> bool:
-        return self.r.exists(key)
+        If ``username`` does not exist, return None
+        """
+        return self.execute_command("ACL GETUSER", username, **kwargs)
+
+    def setuser(
+        self,
+        username: str,
+        enabled: bool = False,
+        nopass: bool = False,
+        passwords: Union[str, Iterable[str], None] = None,
+        hashed_passwords: Union[str, Iterable[str], None] = None,
+        categories: Union[Iterable[str], None] = None,
+        commands: Union[Iterable[str], None] = None,
+        keys: Union[Iterable[KeyT], None] = None,
+        reset: bool = False,
+        reset_keys: bool = False,
+        reset_passwords: bool = False,
+        **kwargs,
+    ) -> ResponseT:
+        """
+        Create or update an ACL user.
+
+        Create or update the ACL for ``username``. If the user already exists,
+        the existing ACL is completely overwritten and replaced with the
+        specified values.
+
+        ``enabled`` is a boolean indicating whether the user should be allowed
+        to authenticate or not. Defaults to ``False``.
+
+        ``nopass`` is a boolean indicating whether the can authenticate without
+        a password. This cannot be True if ``passwords`` are also specified.
+
+        ``passwords`` if specified is a list of plain text passwords
+        to add to or remove from the user. Each password must be prefixed with
+        a '+' to add or a '-' to remove. For convenience, the value of
+        ``passwords`` can be a simple prefixed string when adding or
+        removing a single password.
+
+        ``hashed_passwords`` if specified is a list of SHA-256 hashed passwords
+        to add to or remove from the user. Each hashed password must be
+        prefixed with a '+' to add or a '-' to remove. For convenience,
+        the value of ``hashed_passwords`` can be a simple prefixed string when
+        adding or removing a single password.
+
+        ``categories`` if specified is a list of strings representing category
+        permissions. Each string must be prefixed with either a '+' to add the
+        category permission or a '-' to remove the category permission.
+
+        ``commands`` if specified is a list of strings representing command
+        permissions. Each string must be prefixed with either a '+' to add the
+        command permission or a '-' to remove the command permission.
+
+        ``keys`` if specified is a list of key patterns to grant the user
+        access to. Keys patterns allow '*' to support wildcard matching. For
+        example, '*' grants access to all keys while 'cache:*' grants access
+        to all keys that are prefixed with 'cache:'. ``keys`` should not be
+        prefixed with a '~'.
+
+        ``reset`` is a boolean indicating whether the user should be fully
+        reset prior to applying the new ACL. Setting this to True will
+        remove all existing passwords, flags and privileges from the user and
+        then apply the specified rules. If this is False, the user's existing
+        passwords, flags and privileges will be kept and any new specified
+        rules will be applied on top.
+
+        ``reset_keys`` is a boolean indicating whether the user's key
+        permissions should be reset prior to applying any new key permissions
+        specified in ``keys``. If this is False, the user's existing
+        key permissions will be kept and any new specified key permissions
+        will be applied on top.
+
+        ``reset_passwords`` is a boolean indicating whether to remove all
+        existing passwords and the 'nopass' flag from the user prior to
+        applying any new passwords specified in 'passwords' or
+        'hashed_passwords'. If this is False, the user's existing passwords
+        and 'nopass' status will be kept and any new specified passwords
+        or hashed_passwords will be applied on top.
+
+        For more information check https://redis.io/commands/acl-setuser
+        """
+        encoder = self.get_encoder()
+        pieces: list[str or bytes] = [username]
+
+        if reset:
+            pieces.append(b"reset")
+
+        if reset_keys:
+            pieces.append(b"resetkeys")
+
+        if reset_passwords:
+            pieces.append(b"resetpass")
+
+        if enabled:
+            pieces.append(b"on")
+        else:
+            pieces.append(b"off")
+
+        if (passwords or hashed_passwords) and nopass:
+            raise DataError(
+                "Cannot set 'nopass' and supply " "'passwords' or 'hashed_passwords'"
+            )
+
+        if passwords:
+            # as most users will have only one password, allow remove_passwords
+            # to be specified as a simple string or a list
+            passwords = list_or_args(passwords, [])
+            for i, password in enumerate(passwords):
+                password = encoder.encode(password)
+                if password.startswith(b"+"):
+                    pieces.append(b">%s" % password[1:])
+                elif password.startswith(b"-"):
+                    pieces.append(b"<%s" % password[1:])
+                else:
+                    raise DataError(
+                        f"Password {i} must be prefixed with a "
+                        f'"+" to add or a "-" to remove'
+                    )
+
+        if hashed_passwords:
+            # as most users will have only one password, allow remove_passwords
+            # to be specified as a simple string or a list
+            hashed_passwords = list_or_args(hashed_passwords, [])
+            for i, hashed_password in enumerate(hashed_passwords):
+                hashed_password = encoder.encode(hashed_password)
+                if hashed_password.startswith(b"+"):
+                    pieces.append(b"#%s" % hashed_password[1:])
+                elif hashed_password.startswith(b"-"):
+                    pieces.append(b"!%s" % hashed_password[1:])
+                else:
+                    raise DataError(
+                        f"Hashed password {i} must be prefixed with a "
+                        f'"+" to add or a "-" to remove'
+                    )
+
+        if nopass:
+            pieces.append(b"nopass")
+
+        if categories:
+            for category in categories:
+                category = encoder.encode(category)
+                # categories can be prefixed with one of (+@, +, -@, -)
+                if category.startswith(b"+@"):
+                    pieces.append(category)
+                elif category.startswith(b"+"):
+                    pieces.append(b"+@%s" % category[1:])
+                elif category.startswith(b"-@"):
+                    pieces.append(category)
+                elif category.startswith(b"-"):
+                    pieces.append(b"-@%s" % category[1:])
+                else:
+                    raise DataError(
+                        f'Category "{encoder.decode(category, force=True)}" '
+                        'must be prefixed with "+" or "-"'
+                    )
+        if commands:
+            for cmd in commands:
+                cmd = encoder.encode(cmd)
+                if not cmd.startswith(b"+") and not cmd.startswith(b"-"):
+                    raise DataError(
+                        f'Command "{encoder.decode(cmd, force=True)}" '
+                        'must be prefixed with "+" or "-"'
+                    )
+                pieces.append(cmd)
+
+        if keys:
+            for key in keys:
+                key = encoder.encode(key)
+                pieces.append(b"~%s" % key)
+
+        return self.execute_command("ACL SETUSER", *pieces, **kwargs)
+
+    def acl_genpass(self, bits: Union[int, None] = None, **kwargs) -> ResponseT:
+        """Generate a random password value.
+        If ``bits`` is supplied then use this number of bits, rounded to
+        the next multiple of 4.
+        """
+        pieces = []
+        if bits is not None:
+            try:
+                b = int(bits)
+                if b < 0 or b > 4096:
+                    raise ValueError
+            except ValueError:
+                raise DataError(
+                    "genpass optionally accepts a bits argument, " "between 0 and 4096."
+                )
+        return self.execute_command("ACL GENPASS", *pieces, **kwargs)
+
+    def copy(
+        self,
+        source: str,
+        destination: str,
+        destination_db: Union[str, None] = None,
+        replace: bool = False,
+    ) -> ResponseT:
+        """
+        Copy the value stored in the ``source`` key to the ``destination`` key.
+
+        ``destination_db`` an alternative destination database. By default,
+        the ``destination`` key is created in the source Redis database.
+
+        ``replace`` whether the ``destination`` key should be removed before
+        copying the value to it. By default, the value is not copied if
+        the ``destination`` key already exists.
+        """
+        params = [source, destination]
+        if destination_db is not None:
+            params.extend(["DB", destination_db])
+        if replace:
+            params.append("REPLACE")
+        return self.execute_command("COPY", *params)
+    
+    def flushall(self, asynchronous: bool = False, **kwargs) -> ResponseT:
+        """
+        Delete all keys in all databases on the current host.
+
+        ``asynchronous`` indicates whether the operation is
+        executed asynchronously by the server.
+
+        """
+        args = []
+        if asynchronous:
+            args.append(b"ASYNC")
+        return self.execute_command("FLUSHALL", *args, **kwargs)
