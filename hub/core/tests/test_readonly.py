c6f888ab191db5428cc5e9b5f074c56fe2b51a82
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-04-19 06:59:29 +0530

    few changes

--- hub/core/tests/test_readonly.py
+++ hub/core/tests/test_readonly.py
@@ -14,9 +14,9 @@ def test_MPO():
         assert arr.dtype == 'uint8'
             
 def test_FLI():
-    for path in test_files:
+    for path in static_test_file:
         sample = hub.read(path)
         arr = np.array(sample)
-        assert arr.shape[-1] == 3
-        assert arr.shape[0] == 480
+        assert arr.shape[-1] == 128
+        assert arr.shape[0] == 128
         assert arr.dtype == 'uint8'
