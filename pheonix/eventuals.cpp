cb572cb7628e5f818aa1a18ff0de11cfca24b147
david.buniatyan@gmail.com <david.buniatyan@gmail.com>
2022-04-24 11:56:09 -0700

    eventuals failinng

--- pheonix/eventuals.cpp
+++ pheonix/eventuals.cpp
@@ -8,17 +8,60 @@
 #include "eventuals/terminal.h"
 #include "eventuals/then.h"
 #include "eventuals/http.h"
+#include "eventuals/reduce.h"
 
 #include "eventuals/interrupt.h"
 #include "eventuals/let.h"
 #include "eventuals/scheduler.h"
 #include "eventuals/type-traits.h"
+#include "eventuals/conditional.h"
+#include "eventuals/map.h"   
+#include "eventuals/stream.h"
 
+#include "eventuals/if.h"
 
 int main() {
   eventuals::EventLoop::ConstructDefault();
 
+  auto request = [](auto i){ 
+    std::cout << i << std::endl;
+    return eventuals::http::Get("http://localhost:8000/") | eventuals::Then([&](auto response){
+      return response.code;
+    }); 
+  };
 
+  auto e = [&]() {
+    return eventuals::Stream<int>()
+               .context(2)
+               .next([](auto& count, auto& k) {
+                 if (count > 0) {
+                   k.Emit(count--);
+                 } else {
+                   k.Ended();
+                 }
+               })
+               .done([](auto&, auto& k) {
+                 k.Ended();
+               })
+        | eventuals::Map([&](int i) {
+            std::cout << i << std::endl;
+            std::cout << "request" << std::endl;
+            return request(i); // fails on second request
+          })
+        | eventuals::Reduce(
+               /* sum = */ 0,
+               [](auto& sum) {
+                 return eventuals::Then([&](auto&& value) {
+                   //auto resp = value();
+                   std::cout << "get first value" << std::endl;
+                   sum += value;
+                   std::cout << sum << std::endl;
+                   return true;
+                 });
+               });
+  };
+
+  /*
   std::cout << __LINE__ << std::endl;
   auto e = []() {
     return eventuals::Closure([v = std::vector<int>()]() mutable {
@@ -39,6 +82,8 @@ int main() {
              });
     });
   };
+  */ 
+
   std::cout << __LINE__ << std::endl;
   auto [future, k] = eventuals::Terminate(std::move(e()));
   k.Start();
