55d80852388ee9af242aeeffc83b77ea0248e314
Levon Ghukasyan <levon_ghukasyan@live.com>
2022-05-06 12:10:59 +0400

    minor fix

--- hub/core/chunk/chunk_compressed_chunk.py
+++ hub/core/chunk/chunk_compressed_chunk.py
@@ -309,36 +309,32 @@ class ChunkCompressedChunk(BaseChunk):
         return sample, shape
 
     def _pop_sample(self):
+        if not self.shapes_encoder.is_empty():
+            self.shapes_encoder._pop()
         if self.is_byte_compression:
             self.decompressed_bytes = self.decompressed_bytes[
                 : self.byte_positions_encoder[-1][0]
             ]
-            if not self.shapes_encoder.is_empty():
-                self.shapes_encoder._pop()
             self._data_bytes = compress_bytes(self.decompressed_bytes, self.compression)
         else:
             self.decompressed_samples.pop()
-            if not self.shapes_encoder.is_empty():
-                self.shapes_encoder._pop()
             self._data_bytes = compress_multiple(
                 self.decompressed_samples, self.compression
             )
         self._changed = False
 
     def pop_multiple(self, num_samples):
+        for _ in range(num_samples):
+            if not self.shapes_encoder.is_empty():
+                self.shapes_encoder._pop()
         if self.is_byte_compression:
             total_samples = self.num_samples
             self.decompressed_bytes = self.decompressed_bytes[
                 : self.byte_positions_encoder[total_samples - num_samples][0]
             ]
-            for _ in range(num_samples):
-                if not self.shapes_encoder.is_empty():
-                    self.shapes_encoder._pop()
             self._data_bytes = compress_bytes(self.decompressed_bytes, self.compression)
         else:
             for _ in range(num_samples):
-                if not self.shapes_encoder.is_empty():
-                    self.shapes_encoder._pop()
                 self.decompressed_samples.pop()
             self._data_bytes = compress_multiple(
                 self.decompressed_samples, self.compression
