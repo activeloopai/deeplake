175ea381a6abc5933322ddd3114b16592ccfdd7f
Fariz Rahman <farizrahman4u@gmail.com>
2022-05-09 02:44:05 -0500

    docstr for hub.tiled

--- hub/api/tiled.py
+++ hub/api/tiled.py
@@ -8,4 +8,21 @@ def tiled(
     tile_shape: Optional[Tuple[int, ...]] = None,
     dtype: Union[str, np.dtype] = np.dtype("uint8"),
 ):
+    """Allocates an empty sample of shape `sample_shape`, broken into tiles of shape `tile_shape` (except for edge tiles).
+
+    Example:
+        ```
+        with ds:
+            ds.create_tensor("image", htype="image", sample_compression="png")
+            ds.image.append(hub.tiled(sample_shape=(1003, 1103, 3), tile_shape=(10, 10, 3)))
+            ds.image[0][-217:, :212, 1:] = np.random.randint(0, 256, (217, 212, 2), dtype=np.uint8)
+
+    Args:
+        sample_shape (Tuple[int]): full shape of the sample
+        tile_shape (Optional, Tuple[int]): The sample will be will stored as tiles where each tile will have this shape (except edge tiles).
+        If not specified, it will be computed such that each tile is close to half of the tensor's `max_chunk_size` (after compression).
+
+    Returns:
+        PartialSample: A PartialSample instance which can be appended to a Tensor.
+    """
     return PartialSample(sample_shape=sample_shape, tile_shape=tile_shape, dtype=dtype)
