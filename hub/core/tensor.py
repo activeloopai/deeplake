fd3e501ce38be33011ada4638943a536b73e6b35
Fariz Rahman <farizrahman4u@gmail.com>
2022-05-09 03:12:47 -0500

    invalidate view on update

--- hub/core/tensor.py
+++ hub/core/tensor.py
@@ -602,6 +602,7 @@ class Tensor:
         """
         self.check_link_ready()
         self._write_initialization()
+        [f() for f in self.dataset._update_hooks]
         update_link_callback = self._update_links if self.meta.links else None
         if isinstance(value, Tensor):
             if value._skip_next_setitem:
