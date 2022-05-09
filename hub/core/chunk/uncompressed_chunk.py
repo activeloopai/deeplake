f3bdbc74903715740181e55a96371e1d23f38e3c
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-05-09 13:36:26 +0530

    add assert

--- hub/core/chunk/uncompressed_chunk.py
+++ hub/core/chunk/uncompressed_chunk.py
@@ -32,7 +32,9 @@ class UncompressedChunk(BaseChunk):
 
         samples = incoming_samples[:num_samples]
         samples = intelligent_cast(samples, self.dtype, self.htype)
-        self.data_bytes += samples.tobytes()  # type: ignore
+
+        assert isinstance(self.data_bytes, bytearray)
+        self.data_bytes += samples.tobytes()
 
         if num_samples > 0:
             shape = self.normalize_shape(samples[0].shape)
