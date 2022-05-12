1ffb55fe63d1ad9ae3730c24d07a06fa0f03628b
Fayaz Rahman <fayazrahman4u@gmail.com>
2022-05-12 15:19:40 +0530

    [BUG] Image htypes fix for recompression (#1670)
    * fix for recompression
    
    * add test

--- hub/util/image.py
+++ hub/util/image.py
@@ -4,7 +4,7 @@ from hub.core.sample import Sample
 import numpy as np
 
 
-def convert_sample(image_sample: Sample, mode: str, compression: str) -> Sample:
+def convert_sample(image_sample: Sample, mode: str) -> Sample:
     if image_sample.path:
         image = Image.open(image_sample.path)
     elif image_sample._buffer:
@@ -15,7 +15,7 @@ def convert_sample(image_sample: Sample, mode: str, compression: str) -> Sample:
 
     image = image.convert(mode)
     image_bytes = BytesIO()
-    image.save(image_bytes, format=compression)
+    image.save(image_bytes, format=image_sample.compression)
     converted = Sample(
         buffer=image_bytes.getvalue(), compression=image_sample.compression
     )
