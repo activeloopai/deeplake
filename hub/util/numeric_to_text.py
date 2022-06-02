8cf1fecd1098a01a923ddb5ccedc846155bebb8e
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-06-02 23:08:44 +0530

    fix

--- hub/util/numeric_to_text.py
+++ hub/util/numeric_to_text.py
@@ -1,7 +1,7 @@
 import numpy as np
+from typing import List
 
-
-def numeric_to_text(inp, class_names: list[str]):
+def numeric_to_text(inp, class_names: List[str]):
     if isinstance(inp, np.integer):
         idx = int(inp)
         if idx < len(class_names):
