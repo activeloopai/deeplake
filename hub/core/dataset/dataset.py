d898ca8f4052c3a32f9552c3d53a50f363eff13c
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-06-15 18:25:19 +0530

    rename get_creds to get_creds_keys

--- hub/core/dataset/dataset.py
+++ hub/core/dataset/dataset.py
@@ -2655,7 +2655,7 @@ class Dataset:
             "Managed creds are not supported for datasets that are not connected to activeloop platform."
         )
 
-    def get_creds(self) -> List[str]:
+    def get_creds_keys(self) -> List[str]:
         """Returns the list of creds keys added to the dataset. These are used to fetch external data in linked tensors"""
         return self.link_creds.creds_keys
 
