ba2837584aa7433162d086e14ee1cae52f2c7e93
FayazRahman <fayazrahman4u@gmail.com>
2022-06-17 11:57:01 +0530

    fix

--- hub/client/log.py
+++ hub/client/log.py
@@ -12,7 +12,8 @@ logger = logging.getLogger("hub")
 
 def configure_logger(debug=0):
     log_level = logging.DEBUG if debug == 1 else logging.INFO
-    logging.basicConfig(format="%(message)s", level=log_level, stream=sys.stdout)
+    logger.setLevel(log_level)
+    logging.basicConfig(format="%(message)s", stream=sys.stdout)
 
 
 configure_logger(0)
