279b782a9d096d9a2caf07a9760fb3c9185c9d44
Neel <suhaasneel22@gmail.com>
2022-06-18 17:21:22 +0530

    add batch_size

--- hub/core/augment/augment.py
+++ hub/core/augment/augment.py
@@ -2,10 +2,11 @@ from hub.core.dataset import Dataset
 from typing import List
 from hub.core.transform.transform import ComputeFunction 
 from torch.utils.data.dataloader import DataLoader
-# import torch
+import torch
 # import cv2 as cv
 # from PIL import Image
-# import numpy as np
+import numpy as np
+import time
 
 
 def pipeline(image, pipe):
@@ -23,23 +24,33 @@ def pipeline(image, pipe):
   return updated_image
 
 
-def hub_iterator(dataloader, pipe):
-  for x, y in dataloader:
-    image = x.cpu().detach().numpy().squeeze()
-    if pipe == None:
-      yield (image, y)
-    else:
-      yield(pipeline(image, pipe), y)
+def hub_iterator(dataloader,tensor_name, pipe):
+  for _, sample  in enumerate(dataloader):
+    image_batch = sample[tensor_name].cpu().detach().numpy()
+    num_images = image_batch.shape[0]
+    image_arr = []
+    for i in range(num_images):
+      image = image_batch[i]
+      image = pipeline(image, pipe)
+      image_arr.append(image)
+    
+    sample[tensor_name] = torch.from_numpy(np.array(image_arr))
+    yield sample
 
 class Hubloader():
-  def __init__(self, loader: Dataset, pipeline=None):
-    if loader is not DataLoader:
-      loader = loader.pytorch()
+  def __init__(self, loader: Dataset, tensor_name: str, pipeline, batch_size):
+    
+    if isinstance(loader, Dataset):
+      loader = loader.pytorch(batch_size = batch_size)
+      
+    
     self.dataloader = loader
     self.pipeline = pipeline
+    self.tensor_name = tensor_name
+
 
   def __iter__(self):
-    return hub_iterator(self.dataloader, self.pipeline)
+    return hub_iterator(self.dataloader, self.tensor_name, self.pipeline)
 
 
 
@@ -47,5 +58,5 @@ class Augment():
   def __init__(self, pipeline: List[ComputeFunction]):
     self.pipeline = pipeline
     # self.hub_loader = Hubloader(data_in, pipeline)
-  def __call__(self, loader):
-    return Hubloader(loader, self.pipeline)
+  def __call__(self, loader, tensor_name="images", batch_size = 1):
+    return Hubloader(loader, tensor_name, self.pipeline, batch_size)
