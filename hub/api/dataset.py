0c7f0ecbaf9356b4f062ff58781b5fa0d4219c17
Fariz Rahman <farizrahman4u@gmail.com>
2022-06-27 18:58:41 +0400

    deepcopy + s3 fixes (#1749)

--- hub/api/dataset.py
+++ hub/api/dataset.py
@@ -716,7 +716,6 @@ class dataset:
             copy_func(keys, pg_callback)
 
         keys = src_storage._all_keys()
-
         if tensors is not None:
             required_tensors = src_ds._resolve_tensor_list(tensors)
             for t in required_tensors[:]:
