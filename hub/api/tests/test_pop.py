113cb7ace066017ec52bad64e56a7eb2a5583cba
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-06-29 16:21:17 +0530

    add test

---
+++ hub/api/tests/test_pop.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def test_multiple(memory_ds):
+    with memory_ds as ds:
+        ds.create_tensor("xyz")
+        for i in range(1, 11):
+            ds.xyz.append(i * np.ones((i, i)))
+        for pop_count in range(1, 6):
+            ds.xyz.pop(5)
+            assert len(ds.xyz) == 10 - pop_count
+            for i in range(len(ds.xyz)):
+                ofs = 1 if i < 5 else 1 + pop_count
+                target = i + ofs
+                assert ds.xyz[i].shape == ds.xyz[i].numpy().shape == (target, target)
+                np.testing.assert_array_equal(
+                    ds.xyz[i].numpy(), target * np.ones((target, target))
+                )
