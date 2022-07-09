5f12f280e8afeaced2412db1adb85312f3c017c2
FayazRahman <fayazrahman4u@gmail.com>
2022-07-09 22:34:05 +0530

    ref for viewentry

--- hub/core/dataset/view_entry.py
+++ hub/core/dataset/view_entry.py
@@ -17,6 +17,7 @@ class ViewEntry:
 
     @property
     def id(self) -> str:
+        """Returns id of the view."""
         return self.info["id"]
 
     @property
@@ -25,6 +26,7 @@ class ViewEntry:
 
     @property
     def message(self) -> str:
+        """Returns the message with which the view was saved."""
         return self.info.get("message", "")
 
     def __str__(self):
@@ -43,9 +45,11 @@ class ViewEntry:
         return ds
 
     def optimize(self, unlink=True):
+        """Optimizes the view by copying the required data."""
         self.info = self._ds._optimize_saved_view(
             self.info["id"], external=self._external, unlink=unlink
         )
 
     def delete(self):
+        """Deletes the view."""
         self._ds.delete_view(id=self.info["id"])
