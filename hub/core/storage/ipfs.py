b43856d66d0648151ee9149fb6f0197a8ae4cf9c
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-13 08:54:55 +0200

    WIP: more robust link structure fetching

--- hub/core/storage/ipfs.py
+++ hub/core/storage/ipfs.py
@@ -4,8 +4,11 @@ import hub
 import time
 import json
 import logging
+import dag_cbor
 import requests
+import pandas as pd
 
+from io import StringIO,BytesIO
 from requests.exceptions import HTTPError
 from typing import Optional, Set, Sequence
 from hub.core.storage.provider import StorageProvider
@@ -25,7 +28,7 @@ class IPFSProvider(StorageProvider):
         super().__init__()
         self.coreurl = coreurl
         self.cid = cid
-        self.links = self.get_links()
+        self.links = self._get_links(cid)
         self.gw = IPFSGateway(url=self.coreurl)
         self.storage_type = storage_type
         self.api_key = api_key
@@ -35,13 +38,34 @@ class IPFSProvider(StorageProvider):
         try:
             query = next(i for i in self.links if i['Name'] == path)
             cid = query['Hash']['/']
-            res, content = self.gw.get(cid)
-            if res.status_code == 200:
-                return content
+            # res, content = self.gw.get(cid)
+            res, content = self.gw.cat(cid)
+            # content = self.read_json(cid)
+            # assert 1==0, f'Content is {content}'
+            # cont = pd.read_json(StringIO(content))
+            b = bytes(content,'utf-8')
+            if res.status_code == 200:                
+                return b
             else:
-                raise HTTPError (parse_error_message(res))
-        except:
+                assert 1==0
+                # raise HTTPError (parse_error_message(res))
+        except StopIteration:
             print(f"Path requested: {path}")
+        except FileNotFoundError:
+            print(f"Path requested: {path}, filenotfound")
+            raise KeyError(path)
+        except AssertionError:
+            print(f'Status code: {res.status_code}, Assertion error, content is {content}, path is {path}, cid query is {cid}, query is {query}, and in bytes we have {b}')
+        except TypeError:
+            print('Got type error.')
+        # try:
+        #     full_path = self._check_is_file(path)
+        #     with open(full_path, "rb") as file:
+        #         return file.read()
+        # except DirectoryAtPathException:
+        #     raise
+        # except FileNotFoundError:
+        #     raise KeyError(path)
 
     def __setitem__(self, path, value):
         """Sets the object present at the path with the value"""
@@ -91,28 +115,70 @@ class IPFSProvider(StorageProvider):
         else:
             super().clear()
 
-    def dag_get(self):
+    def dag_get(self, cid=None, url=None):
         """Get a DAG node from IPFS."""
-        
-        params = {}
-        params['arg'] = self.cid
-        params['output-codec'] = 'dag-json'
-        
-        response = requests.post(f'{self.coreurl}/dag/get', params=params)
-        
-        if response.status_code == 200:
-            return response, parse_response(response)
+        if (cid is not None) and (url is not None):
+            params = {}
+            params['arg'] = cid
+            params['output-codec'] = 'dag-json'
+            
+            response = requests.post(f'{url}/dag/get', params=params)
+            
+            if response.status_code == 200:
+                return response, parse_response(response)
 
+            else:
+                raise HTTPError (parse_error_message(response))
         else:
-            raise HTTPError (parse_error_message(response))
+            params = {}
+            params['arg'] = self.cid
+            params['output-codec'] = 'dag-json'
+            
+            response = requests.post(f'{self.coreurl}/dag/get', params=params)
+            
+            if response.status_code == 200:
+                return response, parse_response(response)
 
-    def get_links(self):
-        if (self.coreurl is not None) and (self.cid is not None):
-            res = self.dag_get()
-            links = res[1]['Links']
-            return links
-        else:
-            raise Error("Coreurl or CID not defined.")
+            else:
+                raise HTTPError (parse_error_message(response))
+
+    def read_json(self, 
+        cid:str, 
+    ):
+        r, data = self.gw.cat(cid)      
+        
+        return pd.read_json(StringIO(data))
+
+    def _file_or_dir(self,
+        name
+        ):
+        return 'file' if len(name.split('.')) > 1 else 'dir'
+
+    def _get_links(self,
+        cid,
+        fol=''
+    ):
+        root_struct = {}
+        struct = {}
+        _, content = self.dag_get(url=self.coreurl, cid=cid)
+        links = content['Links']
+
+        for link in links:
+            name = f'{fol}/{link["Name"]}'
+            hash_ = str(link['Hash']['/'])
+            type_ = self._file_or_dir(name)
+
+            if type_ == 'dir':
+                details = self._get_links(hash_, name)
+
+            else:
+                details = {'Hash': hash_, 'type': type_}
+
+            struct[name] = details
+
+        root_struct[fol] = struct
+
+        return root_struct
 
 def parse_error_message(
     response, # Response object from requests
@@ -201,6 +267,28 @@ class IPFSGateway():
         res.raise_for_status()
         return res.headers
 
+    def cat(self, 
+        cid:str, # Path to the IPFS object
+        **kwargs
+    ):
+        'Read a file from IPFS'
+        
+        params = {}
+        params['arg'] = cid
+        params.update(kwargs)
+        
+        res = self.session.post(f'{self.url}/cat', params=params)
+        
+        if res.status_code == 200:
+            return res, res.text
+        
+        else:
+            if res.status_code == 500:
+                raise TypeError (f"dag node {cid} is a directory; Provide a file CID")
+            else:
+                raise HTTPError (parse_error_message(res))
+
+
     def apipost(self, call, **kwargs):
         logger.debug("post %s via %s", call, self.url)
         if 'data' in kwargs.keys():
@@ -265,3 +353,37 @@ class IPFSGateway():
             return (self.state, None)
 
 
+class DownloadDir:
+    'Download a IPFS directory to your local disk'
+    def __init__(self,
+        coreurl:str,
+        root_cid:str, # Root CID of the directory
+        output_fol:str, # Path to save in your local disk
+    ):
+
+        self.url = coreurl
+        self.root = root_cid
+        self.output = output_fol
+        self.full_structure = None
+
+    
+
+
+    def _save_links(self,
+        links
+    ):
+        for k, v in links.items():
+            if len(k.split('.')) < 2:
+                if not os.path.exists(k): os.mkdir(k)
+                self._save_links(v)
+
+            else:
+                data = cat_items(self.url, links[k]['Hash']).content
+
+                with open(k, 'wb') as f:
+                    f.write(data)
+
+    def download(self
+    ):
+        self.full_structure = self._get_links(self.root, self.output)
+        self._save_links(self.full_structure)
