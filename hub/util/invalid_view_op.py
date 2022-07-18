5d7817a421208c5e1f91116332134f6079a5a186
Fariz Rahman <farizrahman4u@gmail.com>
2022-07-18 23:54:46 +0400

    Update invalid_view_op.py

--- hub/util/invalid_view_op.py
+++ hub/util/invalid_view_op.py
@@ -9,9 +9,9 @@ def invalid_view_op(callable: Callable):
     @wraps(callable)
     def inner(x, *args, **kwargs):
         ds = x if isinstance(x, hub.Dataset) else x.dataset
-        if not getattr(ds, "_allow_view_updates", False):
+        if not ds.__dict__.get("_allow_view_updates"):
             is_del = callable.__name__ == "delete"
-            managed_view = hasattr(ds, "_view_entry")
+            managed_view = "_view_entry" in ds.__dict__
             has_vds = getattr(x, "_vds", False)
             is_view = not x.index.is_trivial() or has_vds or managed_view
             if is_view and not (is_del and (has_vds or managed_view)):
