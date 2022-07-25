0bc8e72c08a3476735d855df9d70f3eb413a5898
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-25 15:58:30 +0200

    WIP: initialized integration tests

--- hub/integrations/tests/test_ipfs.py
+++ hub/integrations/tests/test_ipfs.py
@@ -1,10 +1,23 @@
 import pytest
+import hub
+import os
 
 def test_load():
-    return
+    cid = 'ipfs://QmTPWJ3JWHqCHKHADV3BLmc6baiL7GyBSDg7m3Ga5dtWwP'
+    gw = 'https://ipfs.infura.io:5001/api/v0'
+    ds = hub.load(cid, creds={"gw": gw}, read_only=True)
+    assert ds.tensors['data'].key == 'data'
 
 def test_copy():
+    ds = hub.dataset('./test/')
+    p = os.path.abspath("./test/")
+    ds_copy = hub.copy('./test', 'ipfs://', dest_creds={"fpath": p})
+    assert ds.path == 'ipfs://'
     return
 
 def test_delete():
+    ds = hub.dataset('./test/')
+    p = os.path.abspath("./test/")
+    ds_copy = hub.copy('./test', 'ipfs://', dest_creds={"fpath": p})
+    ds_copy.delete()
     return
