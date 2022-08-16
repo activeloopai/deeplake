235557e62b4c3fc725fd3ac7d836dce6e03daec4
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-08-16 16:27:40 +0400

    mypy fixes

--- hub/integrations/hub3/convert_to_hub3.py
+++ hub/integrations/hub3/convert_to_hub3.py
@@ -1,5 +1,5 @@
 import hub
-from hub.integrations.hub3.util import raise_indra_installation_error
+from hub.integrations.hub3.util import raise_indra_installation_error  # type: ignore
 
 try:
     from indra import api  # type: ignore
