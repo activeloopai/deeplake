b59e2b57f730a71c5427a74619fe7d557e35f62e
Daniel <DanielGareev@gmail.com>
2022-08-17 20:32:50 +0200

    Add an API entry point clean_labels() in Dataset class

--- hub/core/dataset/dataset.py
+++ hub/core/dataset/dataset.py
@@ -1343,6 +1343,70 @@ class Dataset:
     def read_only(self, value: bool):
         self._set_read_only(value, True)
 
+    def clean_labels(
+        self,
+        module = None,
+        criterion = None,
+        optimizer = None,
+        optimizer_lr: int = 0.01,
+        device: str = "cpu",
+        epochs: int = 10,
+        folds: int = 5,
+        verbose: bool = True,
+        tensors: Optional[list] = None,
+        dataloader_train_params: [dict] = None,
+        dataloader_valid_params: Optional[dict] = None,
+        overwrite: bool = False
+        # skorch_kwargs: Optional[dict] = None,
+    ):
+        """
+        Cleans the labels of the dataset. Computes out-of-sample predictions and uses Confident Learning (CL) algorithm to clean the labels.
+        Creates a set of tensors under label_issues group for the entire dataset.
+
+        Note:
+            Currently, only image classification task us supported. Therefore, the method accepts two tensors for the images and labels (e.g. ['images', 'labels']).
+            The tensors can be specified in dataloader_train_params or tensors. Any PyTorch module can be used as a classifier.
+
+        Args:
+            module (class): A PyTorch torch.nn.Module module (class or instance). In general, the uninstantiated class should be passed, although instantiated modules will also work. Default is torchvision.models.resnet18(), which is a PyTorch ResNet-18 model.
+            criterion (class): A PyTorch criterion. The uninitialized criterion (loss) used to optimize the module. Default is torch.nn.CrossEntropyLoss.
+            optimizer (class): A PyTorch optimizer. The uninitialized optimizer (update rule) used to optimize the module. Default is torch.optim.SGD.
+            optimizer_lr (int): The learning rate passed to the optimizer. Default is 0.01.
+            device (str): A PyTorch device. The device on which the module and criterion are located. Default is "cpu".
+            epochs (int): The number of epochs to train for each fit() call. Default is 10.
+            tensors (list): A list of tensor names that would be considered for cleaning (e.g. ['images', 'labels']).
+            dataloader_train_params (dict): Keyword arguments to pass into torch.utils.data.DataLoader. Options that may especially impact accuracy include: shuffle, batch_size.
+            dataloader_valid_params (dict): Keyword arguments to pass into torch.utils.data.DataLoader. Options that may especially impact accuracy include: shuffle, batch_size. If not provided, dataloader_train_params will be used with shuffle=False.
+            overwrite (bool): If True, will overwrite label_issues tensors if they already exists. Default is False.
+            fold (int): Sets the number of cross-validation folds used to compute out-of-sample probabilities for each example in the dataset. The default is 5.
+            skorch_kwargs (dict): Keyword arguments to pass into skorch.NeuralNet. Options that may especially impact accuracy include: ...
+
+        Returns:
+            label_issues: A boolean mask for the entire dataset where True represents a label issue and False represents an example that is confidently/accurately labeled.
+            label_quality_scores: Returns label quality scores for each datapoint, where lower scores indicate labels less likely to be correct.
+        """
+
+        from hub.core.experimental.labels import clean_labels
+
+        label_issues, label_quality_scores = clean_labels(
+            self,
+            module=module,
+            criterion=criterion,
+            device=device,
+            epochs=epochs,
+            folds=folds,
+            verbose=verbose,
+            tensors=tensors,
+            dataloader_train_params=dataloader_train_params,
+            dataloader_valid_params=dataloader_valid_params,
+            optimizer=optimizer,
+            optimizer_lr=optimizer_lr,
+            overwrite=overwrite
+            # skorch_kwargs=skorch_kwargs,
+        )
+
+        return label_issues, label_quality_scores
+
     @hub_reporter.record_call
     def pytorch(
         self,
