b63351430009f41186377eb08b83db086a4fd710
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-08-23 20:12:21 +0530

    changes

--- hub/core/tests/test_compression.py
+++ hub/core/tests/test_compression.py
@@ -58,6 +58,73 @@ def test_array(compression, compressed_image_paths):
         np.testing.assert_array_equal(array, decompressed_array)
     else:
         assert_images_close(array, decompressed_array)
+        
+@pytest.mark.parametrize("compression", image_compressions + BYTE_COMPRESSIONS)
+def test_multi_array(compression, compressed_image_paths):
+    compression_type = get_compression_type(compression)
+    if compression_type == IMAGE_COMPRESSION:
+        img = Image.open(compressed_image_paths[compression][0])
+        img2 = img.resize((img.size[0] // 2, img.size[1] // 2))
+        img3 = img.resize((img.size[0] // 3, img.size[1] // 3))
+        arrays = list(map(np.array, [img, img2, img3]))
+        compressed_buffer = compress_multiple(arrays, compression)
+        decompressed_arrays = decompress_multiple(
+            compressed_buffer, [arr.shape for arr in arrays]
+        )
+    elif compression_type == BYTE_COMPRESSION:
+        arrays = [np.random.randint(0, 10, (32, 32)) for _ in range(3)]
+        compressed_buffer = compress_multiple(arrays, compression)
+        decompressed_arrays = decompress_multiple(
+            compressed_buffer, [(32, 32)] * 3, arrays[0].dtype, compression
+        )
+
+    for arr1, arr2 in zip(arrays, decompressed_arrays):
+        if compression == "png" or compression_type == BYTE_COMPRESSION:
+            np.testing.assert_array_equal(arr1, arr2)
+        else:
+            assert_images_close(arr1, arr2)
+
+
+@pytest.mark.parametrize("compression", image_compressions)
+def test_verify(compression, compressed_image_paths, corrupt_image_paths):
+    for path in compressed_image_paths[compression]:
+        sample = hub.read(path)
+        sample_loaded = hub.read(path)
+        sample_loaded.compressed_bytes(compression)
+        sample_verified_and_loaded = hub.read(path, verify=True)
+        sample_verified_and_loaded.compressed_bytes(compression)
+        pil_image_shape = np.array(Image.open(path)).shape
+        assert (
+            sample.shape
+            == sample_loaded.shape
+            == sample_verified_and_loaded.shape
+            == pil_image_shape
+        ), (
+            sample.shape,
+            sample_loaded.shape,
+            sample_verified_and_loaded.shape,
+            pil_image_shape,
+        )
+        verify_compressed_file(path, compression)
+        with open(path, "rb") as f:
+            verify_compressed_file(f, compression)
+    if compression in corrupt_image_paths:
+        path = corrupt_image_paths[compression]
+        sample = hub.read(path)
+        sample.compressed_bytes(compression)
+        Image.open(path)
+        with pytest.raises(CorruptedSampleError):
+            sample = hub.read(path, verify=True)
+            sample.compressed_bytes(compression)
+        with pytest.raises(CorruptedSampleError):
+            verify_compressed_file(path, compression)
+        with pytest.raises(CorruptedSampleError):
+            with open(path, "rb") as f:
+                verify_compressed_file(f, compression)
+        with pytest.raises(CorruptedSampleError):
+            with open(path, "rb") as f:
+                verify_compressed_file(f.read(), compression)
+
 
 
 def test_lz4_bc():
