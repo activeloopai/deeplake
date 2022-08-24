ade4fd5396b4736ce03d4f332a91ca4ea6a32bc5
Neel <suhaasneel22@gmail.com>
2022-08-24 16:56:16 +0530

    documentation

--- hub/core/augment/augment.py
+++ hub/core/augment/augment.py
@@ -9,12 +9,29 @@ from hub.core.augment.utils import *
 import numpy as np
 import time
 
-class Augmenter():    #used to be Pipeline
-  def __init__(self):
-    self.pipe_dict = {}
-    pass
+class Augmenter():    
+  def __init__(self, transformation_dict=None):
+    """
+    Creates an Augmenter object. 
+    
+    Args:
+      pipe_dict: (Optional) Give the transformation dictionary as input.
+        Expected structure of transformation_dict -> keys: Tensors
+                                                     values: List of transformation sequences
+    """
+    if transformation_dict!=None:
+      self.pipe_dict = transformation_dict
+    else:
+      self.pipe_dict = {}
 
   def add_step(self, input_tensors , step_transform):
+    """
+    Adds a transformation_pipeline to each of the tensors in input_tensors.
+
+    Args:
+      input_tensors: List of tensors
+      step_transform: The transformation sequence to be used to transform tensors.
+    """
     for tensor in input_tensors:
       if tensor not in self.pipe_dict.keys():
         self.pipe_dict[tensor] = [step_transform]
@@ -22,11 +39,38 @@ class Augmenter():    #used to be Pipeline
         self.pipe_dict[tensor].append(step_transform)
 
   def augment(self, ds, num_workers=1):
+    """
+    Returns a Dataloader. Each sample in the dataloader contains a transformed tensor according to the defined steps.
+
+    Args: 
+      ds: Takes in a Hub dataset. 
+      num_workers: The number of workers to use. 
+    """
+    #Todo - Add other arguments from dataset.pytorch
     pipe_dict = self.pipe_dict.copy()
     return ds.pytorch(transform=pipe_dict, multiple_transforms=True, num_workers=num_workers)
 
 class Policy():
-  def __init__(self, policy_input="image net"):#policy_name="image net"
+  def __init__(self, policy_input="random_policy_1"):
+    """
+    Initializes an Auto-Augment like policy.
+
+    Args:
+      policy_input: Can be a string or a list of tuples. Each tuple might have multiple tuples inside which represents
+                    transformations to be applied sequentially according to a probability and magnitude. 
+   
+    Example:
+        ``` 
+        policy_input = [
+          (("transform1", probability1, magnitude1), ("transform2", probability2, magnitude2)),
+          (("transform3", probability3, magnitude3), ("transform4", probability4, magnitude4)),
+        ]
+        policy_obj = Policy(policy_input)
+        ```
+        For each sample(image) we choose one of the elements of the list and iterate through the tuple to perform all
+        the transformations according to their probability and magnitude.
+                    
+    """
     if isinstance(policy_input,str):
       self.policy_set = self.get_policies(policy_input)
     else:
@@ -35,8 +79,14 @@ class Policy():
 
 
   def get_policies(self, policy_name):
+    """
+    Stores pretrained policies. Triggered if the policy_input is a string.
 
-    if policy_name == "image net":
+    Args:
+      policy_name: Name of a pretrained policy.
+    """
+
+    if policy_name == "random_policy_1":
       return [
         (("Posterize", 0.4, 4), ("Rotate", 0.6, 30)),
         (("Equalize", 0.8, None), ("Equalize", 0.6, None)),
@@ -64,6 +114,12 @@ class Policy():
 
 
   def run_policy(self, image):
+    """
+    Runs a policy on a given image.shape
+
+    Args:
+      image: A numpy array
+    """
     num_policies = len(self.policy_set)
     policy_id = np.random.randint(num_policies)
     policy = self.policy_set[policy_id]
@@ -75,6 +131,9 @@ class Policy():
   
 
   def run_policy_sample(self, sample):
+    """
+    Runs a policy on a sample. Used in return_dataloader. 
+    """
     image = sample[self.tensor]
     num_policies = len(self.policy_set)
     policy_id = np.random.randint(num_policies)
@@ -86,7 +145,17 @@ class Policy():
     sample[self.tensor] = image
     return sample
 
-  def return_dataloader(self, ds, tensor="images", batch_size = 1, num_workers = None):
+  def augment(self, ds, tensor="images", batch_size = 1, num_workers = None):#used to be return_dataloader
+    """
+    Returns a dataloader. 
+
+    Args:
+      ds: A hub dataset.
+      tensor: tensor_name that has to be transformed.
+      batch_size: The batch size of the dataloader. 
+      num_workers: The number of workers to be used.
+    """
+    #TODO add pytorch arguments
     self.tensor = tensor
     if num_workers is not None:
       return ds.pytorch(batch_size=batch_size, transform=self.run_policy_sample, num_workers = num_workers)
--- hub/core/augment/utils.py
+++ hub/core/augment/utils.py
@@ -1,49 +1,24 @@
 from hub.core.augment.transforms import *
 
 def transform_image(comp_func):
+    """
+    Returns a trasformed tensor. Takes in a hub compute object.
+
+    Args:
+        comp_func: Hub compute function.
+    """
     return comp_func.func(*comp_func.args)
 
 
-def transform_compute(transform_name, magnitude):
-    if transform_name == "TranslateX":      #kept between -1 and 1.
-        return translate(magnitude, 0)
-    elif transform_name == "TranslateY":     #kept between -1 and 1.
-        return translate(0, magnitude)
-    
-    elif transform_name == "Rotate":    # expects degrees positive is anticlockwise
-        return scale_rotate(magnitude, 1)
-    
-    elif transform_name == "Brightness":        #0 black 1 orig, greater than 0 expected
-        return adjust_brightness_contrast(magnitude, 0)
-
-    elif transform_name == "Color":         #between 0 and 1 grayscale if 0 and orig if 1
-        return adjust_saturation(magnitude)
-
-    elif transform_name == "Contrast":      #mag = 1 implies pixel=pixel + 1*mean_piexel_val; expected greater than 0 
-        return adjust_brightness_contrast(1, magnitude)
-
-    elif transform_name == "Posterize":
-        if not isinstance(int):
-            raise ValueError(f"Expected integer type for Posterize got {type(magnitude)}")
-        return posterize(magnitude)
-
-    elif transform_name == "Solarize":     #choose pixel value above which to invert pixel
-        return solarize(magnitude)
-
-    elif transform_name == "Equalize":      #no parameter needed equalizes pizel magnitudes
-        return equalize()
-
-    elif transform_name == "ShearX":        #shear angle range depends on the dimension of image
-        return shearX(magnitude)
-
-    elif transform_name == "ShearY":        #shear angle range depends on the dimension of image
-        return shearY(magnitude)
-    elif transform_name == "Invert":    
-        return invert()
-
-
-
 def run_transform(image, transform_name, magnitude):
+    """
+    Runs a transform on an image and returns the image.
+
+    Args:
+        image: A numpy array. 
+        transform_name: Name of the transform to apply.
+        magnitude: Magnitude of the transform to apply.
+    """
     if transform_name == "TranslateX":      #kept between -1 and 1.
         return transform_image(translate(image, magnitude, 0))
     elif transform_name == "TranslateY":     #kept between -1 and 1.
@@ -81,24 +56,3 @@ def run_transform(image, transform_name, magnitude):
         return transform_image(invert(image))
 
 
-
-#make a pipeline using the probability and then use that to drive the transformations.
-#   Policy
-#       init - define the policy_set or input a created one
-#       run_policy_set
-#       get_policy_set(from a set of predefined policies)
-#           
-#
-#
-#
-#      Policy_Set example - List of lists which again has a list where each List of list is a policy with a defined set of transformations.
-#      [
-#           [[trans, prob, magnitude], [trans, prob, magnitude]....]
-#                                     .   
-#                                     .        
-#                                     .        
-#                                     .        
-#                                     .        
-#                                     .        
-#                                     .        
-#      ]
