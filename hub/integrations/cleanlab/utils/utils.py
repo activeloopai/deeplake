1d6ff0d5780e704e72a14d5a882f5311c1e6ebe5
lowlypalace <lwlypalace@gmail.com>
2022-08-26 17:34:35 +0200

    Fix error

--- hub/integrations/cleanlab/utils/utils.py
+++ hub/integrations/cleanlab/utils/utils.py
@@ -10,5 +10,5 @@ def is_image_tensor(image_tensor_htype):
     return image_tensor_htype in unsupported_image_htypes or image_tensor_htype.startswith("sequence")
 
 def is_label_tensor(label_tensor_htype):
-    unsupported_image_htypes = set("bbox", "point", "video", "audio", "segment_mask", "binary_mask", "keypoints_coco", "image")
-    return label_tensor_htype in unsupported_image_htypes or label_tensor_htype.startswith("sequence")
+    unsupported_label_htypes = set("bbox", "point", "video", "audio", "segment_mask", "binary_mask", "keypoints_coco", "image")
+    return label_tensor_htype in unsupported_label_htypes or label_tensor_htype.startswith("sequence")
