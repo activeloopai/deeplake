1acaf404850d6a2912a65b2919f9e3a09ea43e33
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-08-30 05:30:56 +0000

    import fix

--- hub/experimental/dataloader.py
+++ hub/experimental/dataloader.py
@@ -2,7 +2,7 @@ from typing import Callable, List, Optional
 from hub.experimental.convert_to_hub3 import dataset_to_hub3  # type: ignore
 from hub.experimental.util import raise_indra_installation_error  # type: ignore
 from hub.experimental.util import collate_fn as default_collate  # type: ignore
-from hub.experimental import query
+from hub.experimental.hub3_query import query
 from hub.util.bugout_reporter import hub_reporter
 
 try:
