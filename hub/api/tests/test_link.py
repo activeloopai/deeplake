d8bbe1bc9229e43b0d26912cd138ad17e33e2566
Fariz Rahman <farizrahman4u@gmail.com>
2022-09-14 10:30:43 +0400

    parser test fix

--- hub/api/tests/test_link.py
+++ hub/api/tests/test_link.py
@@ -22,35 +22,35 @@ from hub.util.htype import parse_complex_htype  # type: ignore
 
 def test_complex_htype_parsing():
     with pytest.raises(ValueError):
-        is_sequence, is_link, htype = parse_complex_htype("link")
+        is_sequence, is_link, is_tag, htype = parse_complex_htype("link")
 
-    is_sequence, is_link, htype = parse_complex_htype("sequence")
+    is_sequence, is_link, is_tag, htype = parse_complex_htype("sequence")
     assert is_sequence
     assert not is_link
     assert htype == "generic"
 
     with pytest.raises(ValueError):
-        is_sequence, is_link, htype = parse_complex_htype("sequence[link]")
+        is_sequence, is_link, is_tag, htype = parse_complex_htype("sequence[link]")
 
     with pytest.raises(ValueError):
-        is_sequence, is_link, htype = parse_complex_htype("link[sequence]")
+        is_sequence, is_link, is_tag, htype = parse_complex_htype("link[sequence]")
 
-    is_sequence, is_link, htype = parse_complex_htype("sequence[image]")
+    is_sequence, is_link, is_tag, htype = parse_complex_htype("sequence[image]")
     assert is_sequence
     assert not is_link
     assert htype == "image"
 
-    is_sequence, is_link, htype = parse_complex_htype("link[image]")
+    is_sequence, is_link, is_tag, htype = parse_complex_htype("link[image]")
     assert not is_sequence
     assert is_link
     assert htype == "image"
 
-    is_sequence, is_link, htype = parse_complex_htype("link[sequence[image]]")
+    is_sequence, is_link, is_tag, htype = parse_complex_htype("link[sequence[image]]")
     assert is_sequence
     assert is_link
     assert htype == "image"
 
-    is_sequence, is_link, htype = parse_complex_htype("sequence[link[video]]")
+    is_sequence, is_link, is_tag, htype = parse_complex_htype("sequence[link[video]]")
     assert is_sequence
     assert is_link
     assert htype == "video"
