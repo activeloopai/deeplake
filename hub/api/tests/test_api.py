c97847749e5fd155dca7d63d80dd42bc9d8f63ef
FayazRahman <fayazrahman4u@gmail.com>
2022-09-16 12:19:07 +0200

    test

--- hub/api/tests/test_api.py
+++ hub/api/tests/test_api.py
@@ -2093,9 +2093,11 @@ def test_hub_related_permission_exceptions(
     dataset_handler_error_check(runner, username, password)
 
 
-def test_incompat_dtype_msg(local_ds, capsys):
-    local_ds.create_tensor("abc", dtype="uint32")
-    with pytest.raises(TensorDtypeMismatchError):
-        local_ds.abc.append([0.0])
-    captured = capsys.readouterr()
-    assert "True" not in captured
+def test_copy_label_sync_disabled(local_ds, capsys):
+    abc = local_ds.create_tensor("abc", htype="class_label")
+    abc.extend([1, 2, 3, 4, 5])
+    ds = local_ds.copy(
+        f"{local_ds.path}_copy", overwrite=True, progressbar=False, num_workers=2
+    )
+    captured = capsys.readouterr().out
+    assert captured == ""
