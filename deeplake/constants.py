3f768ec0623537379e23db5361633bf96a6bde8c
FayazRahman <fayazrahman4u@gmail.com>
2022-09-27 20:16:54 +0200

    black

--- deeplake/constants.py
+++ deeplake/constants.py
@@ -90,9 +90,7 @@ PYTEST_MEMORY_PROVIDER_BASE_ROOT = "mem://hub_pytest"
 PYTEST_LOCAL_PROVIDER_BASE_ROOT = "/tmp/hub_pytest/"  # TODO: may fail for windows
 PYTEST_S3_PROVIDER_BASE_ROOT = "s3://hub-2.0-tests/"
 PYTEST_GCS_PROVIDER_BASE_ROOT = "gcs://snark-test/"
-PYTEST_GDRIVE_PROVIDER_BASE_ROOT = (
-    "gdrive://hubtest"
-)
+PYTEST_GDRIVE_PROVIDER_BASE_ROOT = "gdrive://hubtest"
 PYTEST_HUB_CLOUD_PROVIDER_BASE_ROOT = (
     None if HUB_CLOUD_DEV_USERNAME is None else f"hub://{HUB_CLOUD_DEV_USERNAME}/"
 )
