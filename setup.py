2a127fe11314ddca8b95e3f7242c9cd41e3377bf
Fariz Rahman <farizrahman4u@gmail.com>
2022-09-29 03:08:51 +0530

    3.0.3 (#1906)

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ def libdeeplake_availabe():
 
 if libdeeplake_availabe():
     install_requires.insert(0, "libdeeplake==0.0.14")
-install_requires.append("hub-redirect==3.0.3")
+install_requires.append("hub>=2.8.7")
 
 
 config = {
