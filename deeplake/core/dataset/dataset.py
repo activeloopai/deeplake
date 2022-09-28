c151dc73120492111cf1993712f6b9676add2ef0
adolkhan <adilkha.sarsen@gmail.com>
2022-09-28 13:19:10 +0600

    Changed according to comments

--- deeplake/core/dataset/dataset.py
+++ deeplake/core/dataset/dataset.py
@@ -508,11 +508,6 @@ class Dataset:
         kwargs["is_sequence"] = kwargs.get("is_sequence") or is_sequence
         kwargs["is_link"] = kwargs.get("is_link") or is_link
         kwargs["verify"] = verify
-        if is_link and (
-            sample_compression != UNSPECIFIED or chunk_compression != UNSPECIFIED
-        ):
-            sample_compression = UNSPECIFIED
-            chunk_compression = UNSPECIFIED
 
         if not self._is_root():
             return self.root.create_tensor(
