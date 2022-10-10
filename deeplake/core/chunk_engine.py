428d3039aa79c87630f5d12f3c2a8e1badb1ddc9
adolkhan <adilkha.sarsen@gmail.com>
2022-10-10 16:20:23 +0600

    Removed some changes

--- deeplake/core/chunk_engine.py
+++ deeplake/core/chunk_engine.py
@@ -706,10 +706,6 @@ class ChunkEngine:
             Tuple[List[BaseChunk], Dict[Any, Any]]
         """
         current_chunk = start_chunk
-        last_index = self.num_samples - 1
-
-        if self.num_chunks == 0 or last_index in self.tile_encoder:
-            current_chunk = None
 
         updated_chunks = []
         if current_chunk is None:
@@ -768,7 +764,7 @@ class ChunkEngine:
                 pbar.update(num_samples_added)
         if progressbar:
             pbar.close()
-        self.start_chunk = current_chunk  # type: ignore
+
         if register:
             return updated_chunks
         return updated_chunks, tiles
@@ -794,7 +790,7 @@ class ChunkEngine:
         samples, verified_samples = self._sanitize_samples(samples)
         self._samples_to_chunks(
             samples,
-            start_chunk=self.start_chunk if self.start_chunk is not None else self.last_appended_chunk(),
+            start_chunk=self.last_appended_chunk(),
             register=True,
             progressbar=progressbar,
             update_commit_diff=update_commit_diff,
