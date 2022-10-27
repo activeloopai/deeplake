d50cd7d5b77c7211bf11b4ad0e111a62ba58f87d
Abhinav Tuli <42538472+AbhinavTuli@users.noreply.github.com>
2022-10-27 18:51:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ def libdeeplake_availabe():
 
 
 if libdeeplake_availabe():
-    install_requires.insert(0, "libdeeplake==0.0.22")
+    install_requires.insert(0, "libdeeplake==0.0.23")
 install_requires.append("hub>=2.8.7")
 
 
