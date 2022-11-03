fb35b1f7bdb80ddd77b9a4f21a8c92c44e7575b8
progerdav <developer.gyulnazaryan@gmail.com>
2022-11-03 20:16:19 +0400

    Cleanup [part 2]

--- deeplake/auto/unstructured/coco/constants.py
+++ deeplake/auto/unstructured/coco/constants.py
@@ -9,5 +9,6 @@ DEFAULT_COCO_TENSOR_PARAMS = {
     },
     "category_id": {"htype": "class_label", "sample_compression": None},
     "bbox": {"htype": "bbox", "sample_compression": None},
+    "keypoints": {"htype": "keypoints_coco" },
     "_other": DEFAULT_GENERIC_TENSOR_PARAMS,
 }
