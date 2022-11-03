fa8fb5e029f571114876f4c769ba6cfed8947039
FayazRahman <fayazrahman4u@gmail.com>
2022-11-03 16:13:00 +0530

    fixes

--- deeplake/api/tests/test_access_method.py
+++ deeplake/api/tests/test_access_method.py
@@ -20,10 +20,15 @@ def test_access_method_parsing():
 
 def test_access_method(s3_ds_generator):
     with pytest.raises(DatasetHandlerError):
-        deeplake.dataset("./some_non_existent_path", access_method="download")
+        deeplake.dataset(
+            "./some_non_existent_path", access_method="local", download=True
+        )
 
     with pytest.raises(DatasetHandlerError):
         deeplake.dataset("./some_non_existent_path", access_method="local")
+
+    with pytest.raises(DatasetHandlerError):
+        deeplake.dataset("./some_non_existent_path", download=True)
 
     ds = s3_ds_generator()
     with ds:
@@ -31,10 +36,7 @@ def test_access_method(s3_ds_generator):
         for i in range(10):
             ds.x.append(i)
 
-    ds = s3_ds_generator(access_method="download")
-    with pytest.raises(DatasetHandlerError):
-        # second time download is not allowed
-        s3_ds_generator(access_method="download")
+    ds = s3_ds_generator(access_method="local")
     assert not ds.path.startswith("s3://")
     for i in range(10):
         assert ds.x[i].numpy() == i
@@ -43,14 +45,26 @@ def test_access_method(s3_ds_generator):
         s3_ds_generator(access_method="invalid")
 
     with pytest.raises(ValueError):
-        s3_ds_generator(access_method="download", overwrite=True)
+        s3_ds_generator(access_method="local", overwrite=True)
 
     with pytest.raises(ValueError):
-        s3_ds_generator(access_method="local", overwrite=True)
+        s3_ds_generator(access_method="local", download=True, overwrite=True)
+
+    with pytest.raises(ValueError):
+        s3_ds_generator(download=True, overwrite=True)
 
     ds = s3_ds_generator(access_method="local")
     assert not ds.path.startswith("s3://")
     for i in range(10):
         assert ds.x[i].numpy() == i
+
+    remote = s3_ds_generator()
+    for i in range(10):
+        remote.x.append(10 + i)
+
+    ds = s3_ds_generator(download=True)
+    assert not ds.path.startswith("s3://")
+    for i in range(20):
+        ds.x[i].numpy() == i
 
     ds.delete()
--- deeplake/api/tests/test_api.py
+++ deeplake/api/tests/test_api.py
@@ -1764,58 +1764,6 @@ def test_create_branch_when_locked_out(local_ds):
     local_ds.create_tensor("x")
 
 
-def test_access_method(s3_ds_generator):
-    with pytest.raises(DatasetHandlerError):
-        deeplake.dataset(
-            "./some_non_existent_path", access_method="local", download=True
-        )
-
-    with pytest.raises(DatasetHandlerError):
-        deeplake.dataset("./some_non_existent_path", access_method="local")
-
-    with pytest.raises(DatasetHandlerError):
-        deeplake.dataset("./some_non_existent_path", download=True)
-
-    ds = s3_ds_generator()
-    with ds:
-        ds.create_tensor("x")
-        for i in range(10):
-            ds.x.append(i)
-
-    ds = s3_ds_generator(access_method="local")
-    assert not ds.path.startswith("s3://")
-    for i in range(10):
-        assert ds.x[i].numpy() == i
-
-    with pytest.raises(ValueError):
-        s3_ds_generator(access_method="invalid")
-
-    with pytest.raises(ValueError):
-        s3_ds_generator(access_method="local", overwrite=True)
-
-    with pytest.raises(ValueError):
-        s3_ds_generator(access_method="local", download=True, overwrite=True)
-
-    with pytest.raises(ValueError):
-        s3_ds_generator(download=True, overwrite=True)
-
-    ds = s3_ds_generator(access_method="local")
-    assert not ds.path.startswith("s3://")
-    for i in range(10):
-        assert ds.x[i].numpy() == i
-
-    remote = s3_ds_generator()
-    for i in range(10):
-        remote.x.append(10 + i)
-
-    ds = s3_ds_generator(download=True)
-    assert not ds.path.startswith("s3://")
-    for i in range(20):
-        ds.x[i].numpy() == i
-
-    ds.delete()
-
-
 def test_local_storage_path(local_ds):
     path = "/tmp/deeplake/"
     dataset_name = "hub://activeloop/mnist-train"
