97382eb5c7717cc8c233dc82f451f6a287bda88d
Sasun Hambardzumyan <xustup@gmail.com>
2022-11-07 14:50:41 +0400

    Added sample function to dataset.

--- deeplake/experimental/__init__.py
+++ deeplake/experimental/__init__.py
@@ -1,3 +1,4 @@
 from deeplake.experimental.dataloader import dataloader, DeepLakeDataLoader
 from deeplake.experimental.libdeeplake_query import query
+from deeplake.experimental.libdeeplake_query import sample
 from deeplake.experimental.convert_to_libdeeplake import dataset_to_libdeeplake
--- deeplake/experimental/test_query.py
+++ deeplake/experimental/test_query.py
@@ -13,3 +13,35 @@ def test_query(local_ds):
     assert len(dsv) == 20
     for i in range(20):
         assert dsv.label[i].numpy() == 2
+
+
+@requires_libdeeplake
+def test_sample(local_ds):
+    with local_ds as ds:
+        ds.create_tensor("label")
+        for i in range(100):
+            ds.label.append(floor(i / 20))
+
+    dsv = local_ds.sample(
+        "max_weight(label == 2: 10, label == 1: 1)", replace=False, size=10
+    )
+    assert len(dsv) == 10
+    for i in range(10):
+        assert dsv.label[i].numpy() == 2 or dsv.label[i].numpy() == 1
+
+    dsv = local_ds.sample("max_weight(label == 2: 10, label == 1: 1)", replace=True)
+    assert len(dsv) == 100
+    for i in range(100):
+        assert dsv.label[i].numpy() == 2 or dsv.label[i].numpy() == 1
+
+    dsv = local_ds.sample("label")
+    assert len(dsv) == 100
+
+    weights = list()
+    for i in range(100):
+        weights.append(1 if floor(i / 20) == 0 else 0)
+
+    dsv = local_ds.sample(weights)
+    assert len(dsv) == 100
+    for i in range(100):
+        assert dsv.label[i].numpy() == 0
