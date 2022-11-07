437254cebb642de3dea489fccf3b8f5cc1dfd8ff
Fariz Rahman <farizrahman4u@gmail.com>
2022-11-07 14:31:01 +0530

    more speeeeed

--- deeplake/core/tensor.py
+++ deeplake/core/tensor.py
@@ -956,12 +956,13 @@ class Tensor:
                 tdt = tensor.dtype
                 vs = get_link_transform(v["extend"])(samples, self.link_creds)
                 if tdt:
-                    vs = [
-                        v.astype(tdt)
-                        if isinstance(v, np.ndarray) and v.dtype != tdt
-                        else v
-                        for v in vs
-                    ]
+                    if not isinstance(vs, np.ndarray):
+                        vs = [
+                            v.astype(tdt)
+                            if isinstance(v, np.ndarray) and v.dtype != tdt
+                            else v
+                            for v in vs
+                        ]
                 tensor.extend(vs)
 
     def _update_links(
