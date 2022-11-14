f5b4b0611c41c20ff4ec906f20cb7f30c10384a9
AbhinavTuli <abhinav.tuli99@gmail.com>
2022-11-14 16:56:52 +0530

    fix

--- deeplake/util/class_label.py
+++ deeplake/util/class_label.py
@@ -58,12 +58,12 @@ def convert_hash_to_idx(hashes, hash_idx_map):
             return hashes
 
 
-def convert_to_text(inp, class_names: List[str]):
+def convert_to_text(inp, class_names: List[str], return_original=False):
     if isinstance(inp, np.integer):
         idx = int(inp)
         if idx < len(class_names):
             return class_names[idx]
-        return None
+        return idx if return_original else None
     return [convert_to_text(item, class_names) for item in inp]
 
 
