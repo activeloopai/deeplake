d9c575f76aadc4de014e9a289e4b5a6d4ae9fe8d
Fariz Rahman <farizrahman4u@gmail.com>
2022-11-16 03:06:16 +0530

    fix

--- deeplake/integrations/mmdet/mmdet_.py
+++ deeplake/integrations/mmdet/mmdet_.py
@@ -783,13 +783,6 @@ def train_detector(
     """
     cfg = compat_cfg(cfg)
 
-    if isinstance(train_dataset, (list, tuple)):
-        if len(train_dataset) > 1:
-            raise NotImplementedError(
-                "Training on multiple Deep Lake datasets is not supported."
-            )
-        train_dataset = train_dataset[0]
-
     if distributed:
         cfg.gpu_ids = range(torch.cuda.device_count())
     elif not hasattr(cfg, "gpu_ids"):
