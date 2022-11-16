7d77906090ef587446ab4a3a0c7aa8573957c718
Fariz Rahman <farizrahman4u@gmail.com>
2022-11-17 00:32:13 +0530

    fix

--- deeplake/enterprise/dataloader.py
+++ deeplake/enterprise/dataloader.py
@@ -92,7 +92,8 @@ class DeepLakeDataLoader(DataLoader):
     def __len__(self):
         import torch.distributed as dist
         world_size = dist.get_world_size() if dist.is_available() else 1
-        return math.ceil(len(self.dataset) / ((self._batch_size or 1) * world_size))
+        round_fn = math.floor if self._drop_last else math.ceil
+        return round_fn(len(self.dataset) / ((self._batch_size or 1) * world_size))
 
     def batch(self, batch_size: int, drop_last: bool = False):
         """Returns a batched :class:`DeepLakeDataLoader` object.
