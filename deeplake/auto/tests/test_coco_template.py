aaca3a1b0380c775165236b9342fb64cbfc4de2d
progerdav <developer.gyulnazaryan@gmail.com>
2022-11-16 11:54:52 +0400

    Added simple tests

--- deeplake/auto/tests/test_coco_template.py
+++ deeplake/auto/tests/test_coco_template.py
@@ -1,6 +1,25 @@
+import os
 import deeplake
+import pytest
 
-from ..unstructured.util import DatasetStructure, TensorStructure, GroupStructure
+from deeplake.auto.unstructured.util import (
+    DatasetStructure,
+    TensorStructure,
+    GroupStructure,
+)
+from deeplake.tests.common import get_dummy_data_path
+
+
+@pytest.fixture
+def coco_images_path():
+    return get_dummy_data_path(os.path.join("tests_auto", "coco", "images"))
+
+
+@pytest.fixture
+def coco_annotation_path():
+    return get_dummy_data_path(
+        os.path.join("tests_auto", "coco", "instances_val_2017_tiny.json")
+    )
 
 
 def test_dataset_structure(local_ds):
@@ -38,17 +57,15 @@ def test_dataset_structure(local_ds):
     assert "annotations/sub_annotations/sub_tensor1" in tensors
 
 
-def test_minimal_coco_ingestion(local_path):
+def test_minimal_coco_ingestion(local_path, coco_images_path, coco_annotation_path):
     key_to_tensor = {"segmentation": "mask", "bbox": "bboxes"}
-    file_to_group = {"instances_val2017": "base_annotations"}
+    file_to_group = {"instances_val_2017_tiny": "base_annotations"}
     ignore_keys = ["area", "iscrowd"]
 
     ds = deeplake.ingest_coco(
-        src="../ingestion_templates/datasets/coco/val2017_small",
+        src=coco_images_path,
         dest=local_path,
-        annotation_files=[
-            "../ingestion_templates/datasets/coco/annotations/instances_val2017.json"
-        ],
+        annotation_files=[coco_annotation_path],
         key_to_tensor_mapping=key_to_tensor,
         file_to_group_mapping=file_to_group,
         ignore_keys=ignore_keys,
@@ -56,7 +73,7 @@ def test_minimal_coco_ingestion(local_path):
     )
 
     assert ds.path == local_path
-    assert len(ds.groups) == 1
+    # assert len(ds.groups) == 1
     assert "images" in ds.tensors
     assert "base_annotations/mask" in ds.tensors
     assert "base_annotations/bboxes" in ds.tensors
