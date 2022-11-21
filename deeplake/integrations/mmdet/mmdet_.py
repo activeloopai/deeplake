8f28bc482dab47f14f23fcdaee90fc340aff4e94
Fariz Rahman <farizrahman4u@gmail.com>
2022-11-21 16:22:29 +0530

    test rand

--- deeplake/integrations/mmdet/mmdet_.py
+++ deeplake/integrations/mmdet/mmdet_.py
@@ -580,6 +580,10 @@ def transform(
     else:
         gt_masks = None
 
+    img = np.random.randint(0, 100, (640, 390, 3), dtype=np.uint8)
+    bboxes = np.random.uniform(0, 50, (10, 4)).astype(np.float32)
+    labels = np.random.randint(0, 80, (10, ), dtype=labels.dtype)
+
     return pipeline(
         {
             "img": img,
