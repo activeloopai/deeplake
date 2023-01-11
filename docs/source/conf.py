019c4eaaf029af0c3b72996c6e6e123c67a0f199
FayazRahman <fayazrahman4u@gmail.com>
2023-01-11 19:55:30 +0530

    trial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {"logo_only": True}
+html_theme_options = {"logo_only": False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
