775f75a5d40ea1888f842146b56a3f9e50ff13f7
AbhinavTuli <abhinav.tuli99@gmail.com>
2023-01-26 22:58:02 +0530

    not needed in tensorflow, default collate will handle

--- deeplake/integrations/tf/common.py
+++ deeplake/integrations/tf/common.py
@@ -16,11 +16,6 @@ def collate_fn(batch):
         batch = [it[0] for it in batch]
     elif isinstance(elem, Polygons):
         batch = [it.numpy() for it in batch]
-    elif isinstance(elem, (tuple, list)):
-        elem_type = type(elem)
-        return [
-            elem_type([convert_to_tensor(item) for item in sample]) for sample in batch
-        ]
     return default_collate(batch)
 
 
