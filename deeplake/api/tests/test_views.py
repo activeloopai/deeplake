8660c617e06884ad4ffea8f97e1ac639fd8e66aa
FayazRahman <fayazrahman4u@gmail.com>
2023-02-04 00:46:40 +0530

    tests

--- deeplake/api/tests/test_views.py
+++ deeplake/api/tests/test_views.py
@@ -1,6 +1,7 @@
+from deeplake.util.exceptions import DatasetViewSavingError
 from deeplake.client.utils import get_user_name
+from deeplake.cli.auth import logout, login
 from click.testing import CliRunner
-from deeplake.cli.auth import logout
 
 import numpy as np
 
@@ -16,9 +17,10 @@ def populate(ds):
     ds.extend(
         {
             "images": np.random.randint(0, 256, (100, 20, 20, 3), dtype=np.uint8),
-            "label": np.random.randint(0, 3, (100,)),
+            "labels": np.random.randint(0, 3, (100,)),
         }
     )
+    ds.commit()
 
 
 def test_view_token_only(
@@ -26,9 +28,9 @@ def test_view_token_only(
 ):
     username = get_user_name()
     if username != "public":
-        login = True
+        state = "logged in"
     else:
-        login = False
+        state = "logged out"
     runner = CliRunner()
     result = runner.invoke(logout)
     assert result.exit_code == 0
@@ -52,9 +54,68 @@ def test_view_token_only(
     loaded = ds.load_view("50to100")
     np.testing.assert_array_equal(loaded.images.numpy(), ds[50:100].images.numpy())
     np.testing.assert_array_equal(loaded.labels.numpy(), ds[50:100].labels.numpy())
-    assert loaded.path == posixpath.join(hub_cloud_path, ".queries/50to1000")
+    assert loaded._vds.path == posixpath.join(hub_cloud_path, ".queries/50to100")
 
-    ds = ds.load_view("25to100")
+    loaded = ds.load_view("25to100")
     np.testing.assert_array_equal(loaded.images.numpy(), ds[25:100].images.numpy())
     np.testing.assert_array_equal(loaded.labels.numpy(), ds[25:100].labels.numpy())
-    assert loaded.path == f"hub://{username}/queries/.queries/25to100"
+    ds_name = posixpath.split(loaded.path)[-1]
+    assert (
+        loaded._vds.path
+        == f"hub://{username}/queries/.queries/[{username}][{ds_name}]25to100"
+    )
+
+    ds.delete_view("25to100")
+    deeplake.delete(hub_cloud_path, token=hub_cloud_dev_token)
+
+    if state == "logged in":
+        runner.invoke(login, f"-u {username} -p {password}")
+
+
+def test_view_public(hub_cloud_path, hub_cloud_dev_credentials):
+    username = get_user_name()
+    if username != "public":
+        state = "logged in"
+    else:
+        state = "logged out"
+    runner = CliRunner()
+    result = runner.invoke(logout)
+    assert result.exit_code == 0
+
+    username, password = hub_cloud_dev_credentials
+
+    ds = deeplake.load("hub://activeloop/mnist-train")
+    view = ds[100:200]
+
+    # not logged in
+    with pytest.raises(DatasetViewSavingError):
+        view.save_view(id="100to200")
+
+    runner.invoke(login, f"-u {username} -p {password}")
+
+    ds = deeplake.load("hub://activeloop/mnist-train")
+    view = ds[100:200]
+    view.save_view(id="100to200")
+
+    runner.invoke(logout)
+    ds = deeplake.load("hub://activeloop/mnist-train")
+    with pytest.raises(KeyError):
+        ds.load_view("100to200")
+
+    runner.invoke(login, f"-u {username} -p {password}")
+
+    ds = deeplake.load("hub://activeloop/mnist-train")
+    loaded = ds.load_view("100to200")
+    np.testing.assert_array_equal(loaded.images.numpy(), ds[100:200].images.numpy())
+    np.testing.assert_array_equal(loaded.labels.numpy(), ds[100:200].labels.numpy())
+    ds_name = posixpath.split(loaded.path)[-1]
+    assert (
+        loaded._vds.path
+        == f"hub://{username}/queries/.queries/[{username}][{ds_name}]100to200"
+    )
+
+    ds.delete_view("100to200")
+    deeplake.delete(hub_cloud_path)
+
+    if state == "logged out":
+        runner.invoke(logout)
