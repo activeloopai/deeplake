02c76b2bddcb9eef772a40d1b265844e325f4bcf
Ivo Stranic <istranic@gmail.com>
2023-02-24 11:58:44 -0500

    Added a sanitize function

--- deeplake/auto/structured/dataframe.py
+++ deeplake/auto/structured/dataframe.py
@@ -22,7 +22,40 @@ class DataFrame(StructuredDataset):
         if not isinstance(self.source, pd.DataFrame):
             raise Exception("Source is not a pandas dataframe object.")
 
-        self.column_params = column_params if column_params is not None else {}
+        self._initialize_params(column_params)
+
+        print("self.column_params: {}".format(self.column_params))
+
+    def _sanitize_tensor(self, input: str):
+        """Sanitize a string to be a valid tensor name
+
+        Args:
+            input: A string that will be sanitized
+        """
+
+        invalid_chars = ["?", "!", "/", "\\", "#", "'", '"']
+        new_char = "_"
+        for char in invalid_chars:
+            input = input.replace(char, new_char)
+
+        return input
+
+    def _initialize_params(self, column_params):
+        column_params = column_params if column_params is not None else {}
+
+        keys = list(self.source.columns)
+
+        for key in keys:
+            if key in column_params.keys():
+                column_params[key]["name"] = column_params[key].get(
+                    "name", self._sanitize_tensor(key)
+                )  # Get the specified name, and if it doesn't exist, use the sanitized column name
+            else:
+                column_params[key] = {
+                    "name": self._sanitize(key)
+                }  # If column parameters are not specified for a column, use the sanitized column name
+
+        self.column_params = column_params
 
     def fill_dataset(self, ds: Dataset, progressbar: bool = True) -> Dataset:
         """Fill dataset with data from the dataframe - one tensor per column
@@ -47,38 +80,57 @@ class DataFrame(StructuredDataset):
                 if progressbar:
                     print(f"\column={key}, dtype={self.source[key].dtype}")
                 try:
-                    tensor_params = self.column_params.get(
-                        key, {"name": key}
-                    )  # Pull columns settings if specified. If not, default the tensor name to the column name
-                    if "name" not in tensor_params.keys():
-                        tensor_params.update(
-                            name=key
-                        )  # If column settings were specified but the name was omitted, the key should be used as the tensor name
+                    # tensor_params = self.column_params.get(
+                    #     key, {"name": key}
+                    # )  # Pull columns settings if specified. If not, default the tensor name to the column name
+                    # if "name" not in tensor_params.keys():
+                    #     tensor_params.update(
+                    #         name=key
+                    #     )  # If column settings were specified but the name was omitted, the key should be used as the tensor name
+
+                    tensor_params = self.column_params[key]
 
                     dtype = self.source[key].dtype
                     if (
                         dtype == np.dtype("object")
                         and "htype" not in tensor_params.keys()
                     ):
-                        tensor_params.update(htype="text") # Use "text" htype for text data, and if the htype is not specified
+                        tensor_params.update(
+                            htype="text"
+                        )  # Use "text" htype for text data, and if the htype is not specified
                     else:
-                        tensor_params.update(dtype=dtype, create_shape_tensor=tensor_params.get('create_shape_tensor', False)) # htype will be auto-inferred for numeric data unless the htype is specified in tensor_params
+                        tensor_params.update(
+                            dtype=dtype,
+                            create_shape_tensor=tensor_params.get(
+                                "create_shape_tensor", False
+                            ),
+                        )  # htype will be auto-inferred for numeric data unless the htype is specified in tensor_params
 
                     if key not in ds.tensors:
                         ds.create_tensor(**tensor_params)
 
-                    if "htype" in tensor_params.keys() and "link" in tensor_params["htype"]:
+                    if (
+                        "htype" in tensor_params.keys()
+                        and "link" in tensor_params["htype"]
+                    ):
                         creds_key = tensor_params.get("creds_key", None)
                         extend_values = [
                             link(value, creds_key=creds_key)
                             for value in self.source[key].values
                         ]
-                    elif "htype" in tensor_params.keys() and "image" in tensor_params["htype"]:
-                        extend_values = [read(value) for value in self.source[key].values]
+                    elif (
+                        "htype" in tensor_params.keys()
+                        and "image" in tensor_params["htype"]
+                    ):
+                        extend_values = [
+                            read(value) for value in self.source[key].values
+                        ]
                     else:
                         extend_values = self.source[key].values
 
-                    ds[tensor_params["name"]].extend(extend_values, progressbar=progressbar)
+                    ds[tensor_params["name"]].extend(
+                        extend_values, progressbar=progressbar
+                    )
                 except Exception as e:
                     print(str(e))
                     skipped_keys.append(key)
