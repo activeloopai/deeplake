3ab80f227e4fba0b594d398b079ebacf008b27af
AbhinavTuli <abhinav.tuli99@gmail.com>
2023-03-08 10:33:38 +0530

    fix

--- deeplake/core/dataset/deeplake_cloud_dataset.py
+++ deeplake/core/dataset/deeplake_cloud_dataset.py
@@ -319,9 +319,12 @@ class DeepLakeCloudDataset(Dataset):
         key_index = self.link_creds.creds_mapping[creds_key] - 1
         managed_info, replaced_index = None, None
         original_creds_key = creds_key
+        original_key_is_managed = original_creds_key in self.link_creds.managed_creds
+
         if new_creds_key is not None and new_creds_key != creds_key:
             replaced_index = self.link_creds.replace_creds(creds_key, new_creds_key)
             creds_key = new_creds_key
+
         try:
             if managed is not None:
                 management_changed = self.link_creds.change_creds_management(
@@ -329,6 +332,8 @@ class DeepLakeCloudDataset(Dataset):
                 )
                 if management_changed:
                     managed_info = (managed, key_index)
+            if original_key_is_managed and managed is not False:
+                self.link_creds.populate_single_manged_creds(creds_key)
         except Exception:
             if replaced_index is not None:
                 # revert the change
