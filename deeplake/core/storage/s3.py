86f8d2b9e8d8958c82c8e80540c74d0508f9ed7e
AbhinavTuli <abhinav.tuli99@gmail.com>
2023-04-06 17:06:36 +0530

    added support for creds override

--- deeplake/core/storage/s3.py
+++ deeplake/core/storage/s3.py
@@ -123,6 +123,7 @@ class S3Provider(StorageProvider):
         self.profile_name = profile_name
         self._initialize_s3_parameters()
         self._presigned_urls: Dict[str, Tuple[str, float]] = {}
+        self.creds_used: Optional[str] = None
 
     def subdir(self, path: str, read_only: bool = False):
         sd = self.__class__(
@@ -437,6 +438,7 @@ class S3Provider(StorageProvider):
             "loaded_creds_from_environment",
             "read_only",
             "profile_name",
+            "creds_used",
         }
 
     def __getstate__(self):
