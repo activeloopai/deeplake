243ddea1803b46f0a4719e9009cabe737034c024
Levon Ghukasyan <levon_ghukasyan@live.com>
2023-05-02 11:52:59 +0400

    fix python version

--- setup.py
+++ setup.py
@@ -49,14 +49,14 @@ extras = {
 def libdeeplake_availabe():
     py_ver = sys.version_info
     if sys.platform == "linux":
-        if py_ver >= (3, 6) and py_ver <= (3, 10):
+        if py_ver >= (3, 6) and py_ver <= (3, 11):
             return True
     if sys.platform == "darwin":
         mac_ver = list(map(int, platform.mac_ver()[0].split(".")))
         if (
             (mac_ver[0] > 10 or mac_ver[0] == 10 and mac_ver[1] >= 12)
             and py_ver >= (3, 7)
-            and py_ver <= (3, 10)
+            and py_ver <= (3, 11)
         ):
             return True
     return False
