8f080816a62fcfa41dc1df2e0f611fc49199fbfb
adilkhan <adilkhan.sarsen@nu.edu.kz>
2023-05-09 23:39:44 +0600

    Fix

--- deeplake/core/vectorstore/vector_search/ingestion/test_data_ingestion.py
+++ deeplake/core/vectorstore/vector_search/ingestion/test_data_ingestion.py
@@ -6,33 +6,6 @@ from deeplake.core.vectorstore.vector_search.ingestion import data_ingestion
 
 
 def test_data_ingestion():
-    corrupted_data = [
-        {
-            "text": np.array([0.1, 0.2, 0.3, 0.4]),
-            "id": np.int64(1),
-            "metadata": {"a": 1},
-            "embedding": np.array([0.1, 0.2, 0.3, 0.4]),
-        },
-        {
-            "text": "b",
-            "id": np.int64(2),
-            "metadata": {"b": 2},
-            "embedding": np.array([0.1, 0.2, 0.3, 0.4]),
-        },
-        {
-            #     "text": np.array([0.1, 0.2, 0.3, 0.4]),
-            #     "id": np.int64(3),
-            #     "metadata": {"c": 3},
-            #     "embedding": np.array([0.1, 0.2, 0.3, 0.4]),
-            # },
-            # {
-            #     "text": "d",
-            #     "id": np.int64(4),
-            #     "metadata": {"d": 4},
-            #     "embedding": np.array([0.1, 0.2, 0.3, 0.4]),
-        },
-    ]
-
     data = [
         {
             "text": "a",
@@ -95,18 +68,12 @@ def test_data_ingestion():
         chunk_compression="lz4",
     )
 
-    # data_ingestion.run_data_ingestion(
-    #     dataset=dataset,
-    #     elements=data,
-    #     embedding_function=None,
-    #     ingestion_batch_size=1024,
-    #     num_workers=2,
-    # )
-
     data_ingestion.run_data_ingestion(
         dataset=dataset,
-        elements=corrupted_data,
+        elements=data,
         embedding_function=None,
-        ingestion_batch_size=1,
-        num_workers=0,
+        ingestion_batch_size=1024,
+        num_workers=2,
     )
+
+    assert len(dataset) == 4
