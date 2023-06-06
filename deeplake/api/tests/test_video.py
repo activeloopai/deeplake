d57473afc0a5766714bd41d96798fbaa57d8365d
FayazRahman <fayazrahman4u@gmail.com>
2023-06-06 22:33:07 +0530

    typo

--- deeplake/api/tests/test_video.py
+++ deeplake/api/tests/test_video.py
@@ -66,7 +66,7 @@ def test_video_slicing(local_ds: Dataset, video_paths):
     ("vstream_path", "hub_token"),
     [
         ("gcs_vstream_path", "hub_cloud_dev_token"),
-        ("azure_vstream_path", "hub_cloud_token"),
+        ("azure_vstream_path", "hub_cloud_dev_token"),
         ("s3_vstream_path", "hub_cloud_dev_token"),
         ("hub_cloud_vstream_path", "hub_cloud_dev_token"),
     ],
