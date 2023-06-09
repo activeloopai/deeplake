9e1e060c4afe7b40a06034478715027b3a443e5d
FayazRahman <fayazrahman4u@gmail.com>
2023-06-09 16:43:43 +0530

    test

--- deeplake/api/tests/test_connect_datasets.py
+++ deeplake/api/tests/test_connect_datasets.py
@@ -61,17 +61,16 @@ def test_connect_dataset_cases(local_ds, memory_ds, hub_cloud_ds):
     with pytest.raises(InvalidSourcePathError):
         hub_cloud_ds.connect(creds_key="some_creds", dest_path="hub://someorg/somename")
 
-def test_connect_user_not_in_org(
-    azure_ds_generator   
-):
-    with azure_ds_generator() as ds:
+
+def test_connect_user_not_in_org(s3_ds_generator):
+    with s3_ds_generator() as ds:
         ds.create_tensor("x")
         ds.x.append(10)
-    
+
     with pytest.raises(TokenPermissionError) as e:
         ds.connect(creds_key="some_creds", dest_path="hub://bad-org/some-name")
         assert "dataset path" in str(e)
-    
+
     with pytest.raises(TokenPermissionError) as e:
         ds.connect(creds_key="some_creds", org_id="bad-org", ds_name="some-name")
         assert "organization id" in str(e)
