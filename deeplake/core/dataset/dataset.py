a446b9f4c7b7db6f5769d1fd5b3e695e4519002e
FayazRahman <fayazrahman4u@gmail.com>
2023-06-13 21:03:40 +0530

    update docstring

--- deeplake/core/dataset/dataset.py
+++ deeplake/core/dataset/dataset.py
@@ -2406,8 +2406,8 @@ class Dataset:
         """Prints a summary of the dataset.
 
         Args:
-            force (bool): Some dataset views might take a long time to summarize. If `force=True`, the summary will be printed regardless.
-                An error will be raised otherwise
+            force (bool): Dataset views with more than 10000 samples might take a long time to summarize. If `force=True`,
+                the summary will be printed regardless. An error will be raised otherwise.
 
         Raises:
             ValueError: If the dataset view might take a long time to summarize and `force=False`
