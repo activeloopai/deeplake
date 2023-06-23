3ad2df4960ed4a85692334142be01dcbdb61d004
Adilkhan Sarsen <54854336+adolkhan@users.noreply.github.com>
2023-06-23 15:25:48 +0600

    VectorStore bug fixes (#2441)
    * Fixes
    
    * Undo extend fix
    
    * tests
    
    * black fix

--- deeplake/core/vectorstore/test_deeplake_vectorstore.py
+++ deeplake/core/vectorstore/test_deeplake_vectorstore.py
@@ -32,7 +32,7 @@ def embedding_fn(text, embedding_dim=EMBEDDING_DIM):
 
 
 def embedding_fn2(text, embedding_dim=EMBEDDING_DIM):
-    pass  # pragma: no cover
+    return []  # pragma: no cover
 
 
 def embedding_fn3(text, embedding_dim=EMBEDDING_DIM):
@@ -119,12 +119,18 @@ def test_search_basic(local_path, hub_cloud_dev_token):
     )
     vector_store.add(embedding=embeddings, text=texts, metadata=metadatas)
 
+    with pytest.raises(ValueError):
+        vector_store.add(
+            embedding_function=embedding_fn2,
+            embedding_data=texts,
+            text=texts,
+            metadata=metadatas,
+        )
     # Check that default option works
     data_default = vector_store.search(
         embedding=query_embedding,
     )
     assert (len(data_default.keys())) > 0
-
     # Use python implementation to search the data
     data_p = vector_store.search(
         embedding=query_embedding,
@@ -159,6 +165,15 @@ def test_search_basic(local_path, hub_cloud_dev_token):
         == 2
     )  # One for each return_tensors
     assert len(data_ce.keys()) == 3  # One for each return_tensors + score
+
+    with pytest.raises(ValueError):
+        data_ce = vector_store_cloud.search(
+            query=f"SELECT * WHERE id=='{vector_store_cloud.dataset.ids[0].numpy()[0]}'",
+            embedding=query_embedding,
+            exec_option="compute_engine",
+            k=2,
+            return_tensors=["ids", "text"],
+        )
 
     # Run a full custom query
     test_text = vector_store_cloud.dataset.text[0].data()["value"]
--- deeplake/core/vectorstore/vector_search/dataset/dataset.py
+++ deeplake/core/vectorstore/vector_search/dataset/dataset.py
@@ -378,6 +378,9 @@ def extend_or_ingest_dataset(
             ):
                 embedded_data = func(data)
                 embedded_data = np.array(embedded_data, dtype=np.float32)
+                if len(embedded_data) == 0:
+                    raise ValueError("embedding function returned empty list")
+
                 processed_tensors[tensor] = embedded_data
 
         dataset.extend(processed_tensors)
