ab7b5d5f4ee3098738fa98518ea3ffffe3684269
FayazRahman <fayazrahman4u@gmail.com>
2023-07-07 16:18:42 +0530

    init

--- deeplake/util/access_method.py
+++ deeplake/util/access_method.py
@@ -1,9 +1,13 @@
 import os
 import time
 import deeplake
-from deeplake.constants import TIMESTAMP_FILENAME
-from deeplake.util.exceptions import DatasetHandlerError
-from deeplake.util.storage import get_local_storage_path
+from deeplake.constants import TIMESTAMP_FILENAME, DOWNLOAD_MANAGED_PATH_SUFFIX
+from deeplake.util.exceptions import DatasetHandlerError, UnprocessableEntityException
+from deeplake.util.storage import get_local_storage_path, storage_provider_from_path
+from deeplake.util.connect_dataset import connect_dataset_entry
+from deeplake.util.path import get_path_type
+from deeplake.util.keys import get_dataset_linked_creds_key
+from deeplake.core.link_creds import LinkCreds
 
 
 def check_access_method(access_method: str, overwrite: bool):
@@ -48,6 +52,56 @@ def parse_access_method(access_method: str):
     return access_method, num_workers, scheduler
 
 
+def managed_creds_used_in_dataset(path, creds, token):
+    managed_creds_used = False
+    if get_path_type(path) == "hub":
+        # need to connect dataset to backend if managed creds are used in it
+        storage = storage_provider_from_path(
+            path, creds=creds, read_only=True, token=token
+        )
+        linked_creds_key = get_dataset_linked_creds_key()
+        try:
+            data_bytes = storage[linked_creds_key]
+        except KeyError:
+            data_bytes = None
+
+        if data_bytes:
+            link_creds = LinkCreds.frombuffer(data_bytes)
+        else:
+            link_creds = LinkCreds()
+
+        managed_creds_used = (
+            len(link_creds.managed_creds_keys.intersection(link_creds.used_creds_keys))
+            > 0
+        )
+    return managed_creds_used
+
+
+def connect_dataset_entry_if_needed(
+    path, local_path, download, managed_creds_used, token
+):
+    if managed_creds_used:
+        print(
+            "Managed credentials are used in the dataset. Connecting local dataset to backend..."
+        )
+        connect_path = path + DOWNLOAD_MANAGED_PATH_SUFFIX
+        if download:
+            try:
+                connect_dataset_entry(
+                    local_path,
+                    "S3_KEY",
+                    connect_path,
+                    token=token,
+                    verbose=False,
+                    allow_local=True,
+                )
+            except UnprocessableEntityException:
+                # dataset already connected to backend
+                pass
+        local_path = connect_path
+    return local_path
+
+
 def get_local_dataset(
     access_method,
     path,
@@ -67,6 +121,9 @@ def get_local_dataset(
     download = access_method == "download" or (
         access_method == "local" and not deeplake.exists(local_path)
     )
+
+    managed_creds_used = managed_creds_used_in_dataset(path, creds, token)
+
     if download:
         if not ds_exists:
             raise DatasetHandlerError(
@@ -83,6 +140,10 @@ def get_local_dataset(
             verbose=False,
             overwrite=True,
         )
+
+    local_path = connect_dataset_entry_if_needed(
+        path, local_path, managed_creds_used, download, token
+    )
 
     ds = deeplake.load(
         local_path,
