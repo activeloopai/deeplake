99edcbec35e80133ef145ea2e76c10b441e5da41
Ivo Stranic <istranic@gmail.com>
2023-07-13 15:58:26 -0400

    Added reporting for locks

--- deeplake/api/dataset.py
+++ deeplake/api/dataset.py
@@ -419,7 +419,17 @@ class dataset:
                 local_cache_size=local_cache_size,
             )
 
-            feature_report_path(path, "empty", {"Overwrite": overwrite}, token=token)
+            feature_report_path(
+                path,
+                "empty",
+                {
+                    "runtime": runtime,
+                    "overwrite": overwrite,
+                    "lock_enabled": lock_enabled,
+                    "lock_timeout": lock_timeout,
+                },
+                token=token,
+            )
         except Exception as e:
             if isinstance(e, UserNotLoggedInException):
                 raise UserNotLoggedInException from None
@@ -575,7 +585,12 @@ class dataset:
                 memory_cache_size=memory_cache_size,
                 local_cache_size=local_cache_size,
             )
-            feature_report_path(path, "load", {}, token=token)
+            feature_report_path(
+                path,
+                "load",
+                {"lock_enabled": lock_enabled, "lock_timeout": lock_timeout},
+                token=token,
+            )
         except Exception as e:
             if isinstance(e, UserNotLoggedInException):
                 raise UserNotLoggedInException from None
