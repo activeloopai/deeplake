8898315825cb4ca4a73fbb92b62233df19457525
Sounak Chakraborty <sounakr@gmail.com>
2023-07-18 09:25:42 +0530

    Test case addition for Index Maintenance : Append | Update | Delete on base dataset.

--- deeplake/core/tests/test_vdb_indexes.py
+++ deeplake/core/tests/test_vdb_indexes.py
@@ -66,3 +66,106 @@ def test_query_recall(local_ds_generator):
         warnings.warn(f"Recall is too high - {recall}. Make sure that the query uses indexing instead of bruteforcing.")
     else:
         print(f"Recall is in the expected range - {recall}")
+
+
+@requires_libdeeplake
+def test_index_maintenance_append(local_ds_generator):
+    deeplake_ds = local_ds_generator()
+    with deeplake_ds:
+        deeplake_ds.create_tensor("embedding", htype="embedding", dtype=np.float32)
+        for _ in range(200):
+            deeplake_ds.embedding.append(
+                np.random.random_sample(
+                    384
+                ).astype(np.float32)
+            )
+    deeplake_ds.embedding.create_vdb_index("hnsw_1")
+    # Append rows and check query recall.
+    deeplake_ds.embedding.append(
+            np.random.random_sample(
+                384
+            ).astype(np.float32))
+
+    # Check if the index is recreated properly.
+    es = deeplake_ds.embedding.get_vdb_indexes()
+    assert len(es) == 1
+    assert es[0]['id'] == 'hnsw_1'
+    assert es[0]['distance'] == 'l2_norm'
+    assert es[0]['type'] == 'hnsw'
+
+    # check the query recall.
+    correct = 0
+    for i in range(len(deeplake_ds.embedding)):
+        v = deeplake_ds.embedding[i].numpy()
+        s = ','.join(str(c) for c in v)
+        view = deeplake_ds.query(f"select * order by l2_norm(embedding - array[{s}]) limit 1")
+        if view.index.values[0].value[0] == i:
+            correct += 1
+
+    recall = float(correct) / len(deeplake_ds)
+    if recall < 0.7:
+        warnings.warn(f"Recall is too low - {recall}. Make sure that indexing works properly")
+    elif recall >= 1:
+        warnings.warn(f"Recall is too high - {recall}. Make sure that the query uses indexing instead of bruteforcing.")
+    else:
+        print(f"Recall is in the expected range - {recall}")
+
+    @requires_libdeeplake
+    def test_index_maintenance_update(local_ds_generator):
+        deeplake_ds = local_ds_generator()
+        with deeplake_ds:
+            deeplake_ds.create_tensor("embedding", htype="embedding", dtype=np.float32)
+            deeplake_ds.embedding.append(
+                np.random.random_sample(
+                    200
+                ).astype(np.float32))
+        deeplake_ds.embedding.create_vdb_index("hnsw_1")
+        # Append rows and check query recall.
+        deeplake_ds.embedding.update({"embedding": [1] * 200})
+
+        # Check if the index is recreated properly.
+        es = deeplake_ds.embedding.get_vdb_indexes()
+        assert len(es) == 1
+        assert es[0]['id'] == 'hnsw_1'
+        assert es[0]['distance'] == 'l2_norm'
+        assert es[0]['type'] == 'hnsw'
+
+        # check the query recall.
+        correct = 0
+        for i in range(len(deeplake_ds.embedding)):
+            v = deeplake_ds.embedding[i].numpy()
+            s = ','.join(str(c) for c in v)
+            view = deeplake_ds.query(f"select * order by l2_norm(embedding - array[{s}]) limit 1")
+            if view.index.values[0].value[0] == i:
+                correct += 1
+
+        recall = float(correct) / len(deeplake_ds)
+        if recall < 0.7:
+            warnings.warn(f"Recall is too low - {recall}. Make sure that indexing works properly")
+        elif recall >= 1:
+            warnings.warn(
+                f"Recall is too high - {recall}. Make sure that the query uses indexing instead of bruteforcing.")
+        else:
+            print(f"Recall is in the expected range - {recall}")
+
+
+   def test_index_maintenance_delete(local_ds_generator):
+        deeplake_ds = local_ds_generator()
+        with deeplake_ds:
+            deeplake_ds.create_tensor("embedding", htype="embedding", dtype=np.float32)
+            for _ in range(200):
+                deeplake_ds.embedding.append(
+                    np.random.random_sample(
+                        384
+                    ).astype(np.float32)
+                )
+        deeplake_ds.embedding.create_vdb_index("hnsw_1")
+        # delete dataset and check if the index exists.
+        deeplake_ds.delete()
+
+        with pytest.raises(ValueError):
+            deeplake_ds.embedding.delete_vdb_index("hnsw_1")
+
+
+
+
