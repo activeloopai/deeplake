c9838d2844c523c56cb21eb8490ce4508972618f
Levon Ghukasyan <levon_ghukasyan@live.com>
2023-07-25 21:07:40 +0400

    True to false

--- deeplake/enterprise/dataloader.py
+++ deeplake/enterprise/dataloader.py
@@ -671,7 +671,7 @@ class DeepLakeDataLoader(DataLoader):
         return iter(self._dataloader)
 
 
-def dataloader(dataset, ignore_errors: bool = True) -> DeepLakeDataLoader:
+def dataloader(dataset, ignore_errors: bool = False) -> DeepLakeDataLoader:
     """Returns a :class:`~deeplake.enterprise.dataloader.DeepLakeDataLoader` object which can be transformed to either pytorch dataloader or numpy.
 
 
