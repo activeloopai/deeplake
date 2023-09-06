813f21025db21efd57f9adf4626732f05f2903bf
Nathan Voxland <nathan@voxland.net>
2023-09-06 17:06:37 -0500

    Re-enabled test_data_ingestion.py

--- deeplake/core/vectorstore/vector_search/ingestion/test_data_ingestion.py
+++ deeplake/core/vectorstore/vector_search/ingestion/test_data_ingestion.py
@@ -22,9 +22,6 @@ def corrupted_embedding_function(emb, threshold):
 
 
 @pytest.mark.slow
-@pytest.mark.flaky(retry_count=3)
-@pytest.mark.timeout(60)
-@pytest.mark.skip(reason="causing lockups")
 def test_ingest_data(local_path):
     data = [
         {
