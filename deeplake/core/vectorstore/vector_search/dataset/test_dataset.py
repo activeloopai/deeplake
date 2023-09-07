2095402e948dfa00f34a05f3715fc5a432c8db47
adilkhan <adilkhan.sarsen@nu.edu.kz>
2023-09-08 00:00:03 +0600

    test fix

--- deeplake/core/vectorstore/vector_search/dataset/test_dataset.py
+++ deeplake/core/vectorstore/vector_search/dataset/test_dataset.py
@@ -42,6 +42,8 @@ def test_create(caplog, hub_cloud_path, hub_cloud_dev_token):
         embedding_function=Embedding,
         runtime=None,
         exec_option="python",
+        org_id=None,
+        username="public",
     )
     assert len(dataset) == 0
     assert set(dataset.tensors.keys()) == {
@@ -69,6 +71,8 @@ def test_create(caplog, hub_cloud_path, hub_cloud_dev_token):
         runtime={"tensor_db": True},
         overwrite=True,
         embedding_function=Embedding,
+        org_id=None,
+        username="testingacc2",
     )
     assert len(dataset) == 0
     assert set(dataset.tensors.keys()) == {
@@ -102,6 +106,8 @@ def test_create(caplog, hub_cloud_path, hub_cloud_dev_token):
             runtime=None,
             overwrite=True,
             embedding_function=Embedding,
+            org_id=None,
+            username="testingacc2",
         )
 
 
@@ -119,6 +125,8 @@ def test_load(caplog, hub_cloud_dev_token):
         token=hub_cloud_dev_token,
         embedding_function=None,
         runtime=None,
+        org_id=None,
+        username="testingacc2",
     )
     assert dataset.max_len == 10
 
@@ -140,6 +148,8 @@ def test_load(caplog, hub_cloud_dev_token):
             embedding_function=None,
             overwrite=False,
             runtime=None,
+            org_id=None,
+            username="public",
         )
         assert (
             f"The default deeplake path location is used: {DEFAULT_VECTORSTORE_DEEPLAKE_PATH}"
@@ -159,6 +169,8 @@ def test_load(caplog, hub_cloud_dev_token):
             embedding_function=None,
             overwrite=False,
             runtime=None,
+            org_id=None,
+            username="public",
         )
 
     with pytest.raises(ValueError):
