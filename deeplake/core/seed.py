839ffb033cf6cc49746ffb4578dfb6274cb11c91
Levon Ghukasyan <levon_ghukasyan@live.com>
2023-09-12 10:14:37 +0400

    move docs

--- deeplake/core/seed.py
+++ deeplake/core/seed.py
@@ -15,10 +15,38 @@ class DeeplakeRandom(object):
         """Set random seed to the deeplake engines
 
         Args:
-            seed (int, optional): integer seed to initialise the engines, used to control random behaviour and bring reproducability. Set number to initialise the engines to reset the seed set None. Defaults to None.
+            seed (int, optional): integer seed to initialize the engines, used to control random behavior and bring reproducibility. Set number to initialize the engines, to reset the seed set None. Defaults to None.
 
         Raises:
-            TypeError: If the provided value is not expected one.
+            TypeError: If the provided value type is not an expected one.
+
+        Background
+        ----------
+
+        To train and operate with the deeplake features and keep reproducibility between all the engines
+        See the examples below on how to operate with the random engines.
+
+        Deeplake does not provide in-house random number generator but to control the reproducibility
+        and keep track on flow stages can be used :meth:`deeplake.random.seed` to seed the RNG for all the engines (both
+        enterprise and non enterprise)::
+
+            import deeplake
+            deeplake.random.seed(0)
+
+        Random number generators in other libraries
+        -------------------------------------------
+        If you or any of the libraries you are using rely on NumPy, you can seed the global
+        NumPy RNG with::
+
+            import numpy as np
+            np.random.seed(0)
+
+            import random
+            random.seed(0)
+
+        Those will impact to all the places where deeplake uses those libraries but with use of use :meth:`deeplake.random.seed` we are nor changing any library
+        global state instead just initializing singleton instances in the random engines
+
 
         """
         if seed is None or isinstance(seed, int):
--- docs/source/deeplake.random.rst
+++ docs/source/deeplake.random.rst
@@ -1,45 +1,6 @@
 deeplake.random.seed
 ====================
 
-Train and operate with the deeplake features and keep reproducibility between all the engines
-See the ``deeplake.random.seed`` method on hot to operate with the random engines:
-
-.. currentmodule:: deeplake.core.seed
-
-.. autosummary::
-    :toctree:
-    :nosignatures:
-
-    deeplake.random
-
-Deeplake random number generator
---------------------------------
-Deeplake does not provide random in-house random number generator but to control the reproducibility
-and keep track on the stages of 
-You can use :meth:`deeplake.random.seed` to seed the RNG for all the engines (both
-enterprise and non enterprise)::
-
-    import deeplake
-    deeplake.random.seed(0)
-
-
-Random number generators in other libraries
--------------------------------------------
-If you or any of the libraries you are using rely on NumPy, you can seed the global
-NumPy RNG with::
-
-    import numpy as np
-    np.random.seed(0)
-
-    import random
-    random.seed(0)
-
-those will impact to all the places where deeplake uses those libraries but with use of use :meth:`deeplake.random.seed` we are nor changing any library
-global state instead just initialising singlton instances in the random engines
-
-DeeplakeRandom
-~~~~~~~~~~~~~~~~~~
-
 .. currentmodule:: deeplake.core.seed
 
 .. autoclass:: DeeplakeRandom
