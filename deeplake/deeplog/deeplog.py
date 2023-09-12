0e3eb9b795f1691e323e5d8a1e9db4765b9cad6f
Nathan Voxland <nathan@voxland.net>
2023-09-12 18:29:20 -0500

    Added initial stub deeplog objects

--- deeplake/deeplog/deeplog.py
+++ deeplake/deeplog/deeplog.py
@@ -1,12 +1,24 @@
-from typing import List
+from typing import List, Union
 
 from deeplake.deeplog.actions import (
+    DeepLogAction,
     AddFileAction,
     CreateBranchAction,
     ProtocolAction,
     MetadataAction,
 )
 
+
+class DeeplogState:
+
+    def __init__(self, action: Union[DeepLogAction, List[DeepLogAction]]):
+        self.action = action
+
+    def version(self) -> str:
+        return 5
+
+    def data(self) -> DeepLogAction:
+        return self.action
 
 class DeepLog:
     def version(self) -> int:
@@ -15,20 +27,23 @@ class DeepLog:
     def path(self) -> str:
         return "path/to/deeplog"
 
-    def protocol(self) -> ProtocolAction:
-        return ProtocolAction()
+    def protocol(self, version: int = -1) -> DeeplogState:
+        """
+        Return the protocol at the given version. If version == -1, use the latest version
+        """
+        return DeeplogState(ProtocolAction())
 
-    def metadata(self) -> MetadataAction:
-        return MetadataAction()
+    def metadata(self, version: int = -1) -> DeeplogState:
+        return DeeplogState(MetadataAction())
 
-    def branches(self) -> List[CreateBranchAction]:
-        return [
+    def branches(self, version: int = -1) -> DeeplogState[List[CreateBranchAction]]:
+        return DeeplogState([
             CreateBranchAction("", "main"),
             CreateBranchAction("8331-351-3313", "other_branch"),
-        ]
+        ])
 
-    def data_files(self) -> List[AddFileAction]:
-        return [
+    def data_files(self, version: int = -1) -> DeeplogState[List[AddFileAction]]:
+        return DeeplogState([
             AddFileAction("my/path.txt"),
             AddFileAction("other/path.txt"),
-        ]
+        ])
