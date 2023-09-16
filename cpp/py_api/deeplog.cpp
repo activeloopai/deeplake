dad16c6464b383ac80e6d51a1248dd589c5e7259
Nathan Voxland <nathan@voxland.net>
2023-09-16 00:30:10 -0500

    Binded real deeplog class as DeepLogCpp for now

--- cpp/py_api/deeplog.cpp
+++ cpp/py_api/deeplog.cpp
@@ -1,28 +1,12 @@
 #include <pybind11/pybind11.h>
-
-//int add(int i, int j) {
-//    return i + j;
-//}
-//
-//PYBIND11_MODULE(deeplake, m) {
-//    m.doc() = "pybind11 example plugin"; // optional module docstring
-//
-//    m.def("add", &add, "A function that adds two numbers");
-//}
-
-struct Pet {
-    Pet(const std::string &name) : name(name) { }
-    void setName(const std::string &name_) { name = name_; }
-    const std::string &getName() const { return name; }
-
-    std::string name;
-};
+#include "../deeplog/deeplog.hpp"
 
 PYBIND11_MODULE(_deeplake, m) {
     auto m_deeplog = m.def_submodule("_deeplog");
 
-    pybind11::class_<Pet>(m_deeplog, "Pet", "A Pet")
-            .def(pybind11::init<const std::string &>())
-            .def("setName", &Pet::setName, "Set the pet's name")
-            .def("getName", &Pet::getName, "Get the pet's name");
+    pybind11::class_<deeplake::deeplog>(m_deeplog, "DeepLogCpp")
+            .def_static("open", &deeplake::deeplog::open)
+            .def_static("create", &deeplake::deeplog::create)
+//            .def("version", &deeplake::deeplog::version)
+            .def("path", &deeplake::deeplog::path);
 }
