13b3a7fb28ca806d316f6b6fb346a31b9a65f79c
Nathan Voxland <nathan@voxland.net>
2023-09-22 13:10:33 -0500

    Updated version to 3.7.1

--- deeplake/__init__.py
+++ deeplake/__init__.py
@@ -85,7 +85,7 @@ __all__ = [
 ]
 
 
-__version__ = "3.6.27"
+__version__ = "3.7.1"
 warn_if_update_required(__version__)
 __encoded_version__ = np.array(__version__)
 config = {"s3": Config(max_pool_connections=50, connect_timeout=300, read_timeout=300)}
