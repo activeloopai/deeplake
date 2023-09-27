1fef81d4403864c616c5785a49c6f33b25e5f3fb
Sasun Hambardzumyan <xustup@gmail.com>
2023-09-28 01:41:23 +0400

    Fixed cursor disappear on fast spinners.

--- deeplake/util/spinner.py
+++ deeplake/util/spinner.py
@@ -76,7 +76,7 @@ class Spinner(threading.Thread):
         try:
             time.sleep(deeplake.constants.SPINNER_START_DELAY)
             frames = cycle("/-\\|")
-            if not self._hide_event.is_set():
+            if not self._hide_event.is_set() and not self._stop_event.is_set():
                 self._hide_cursor()
             while not self._stop_event.is_set():
                 if self._hide_event.is_set():
