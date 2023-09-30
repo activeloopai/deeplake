633a0d17099b3587455df29fef9585866f8219d2
adilkhan <adilkhan.sarsen@nu.edu.kz>
2023-09-30 18:47:33 +0600

    fix

--- deeplake/core/vectorstore/vectorstore_factory.py
+++ deeplake/core/vectorstore/vectorstore_factory.py
@@ -4,14 +4,14 @@ from deeplake.client.client import DeepMemoryBackendClient
 
 
 def vectorstore_factory(
+    path,
     *args,
     **kwargs,
 ):
     dm_client = DeepMemoryBackendClient(token=kwargs.get("token"))
-    path = kwargs.get("path")
     # TODO: add support for windows
     dataset_id = path[6:].split("/")[0]
     deepmemory_is_available = dm_client.deepmemory_is_available(dataset_id)
     if kwargs.get("runtime") == {"tensor_db": True} and deepmemory_is_available:
-        return DeepMemoryVectorStore(client=dm_client, *args, **kwargs)
-    return VectorStore(*args, **kwargs)
+        return DeepMemoryVectorStore(path=path, client=dm_client, *args, **kwargs)
+    return VectorStore(path=path, *args, **kwargs)
