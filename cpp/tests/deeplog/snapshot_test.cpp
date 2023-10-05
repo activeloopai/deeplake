97c426cc93a09a1f79e169758024f4fb50b46cea
FayazRahman <fayazrahman4u@gmail.com>
2023-10-05 19:51:33 +0530

    fix test

--- cpp/tests/deeplog/snapshot_test.cpp
+++ cpp/tests/deeplog/snapshot_test.cpp
@@ -25,7 +25,7 @@ TEST_F(DeeplogSnapshotTest, construct) {
     auto log = deeplog::deeplog::create(test_dir, 4);
     auto main_id = deeplog::metadata_snapshot(log).find_branch("main")->id;
 
-    auto action = deeplog::add_file_action("my/path", "chunk", 3, 45, true);
+    auto action = deeplog::add_file_action("my/path", "chunk", 3, 45, true, 20);
     log->commit(main_id, log->version(main_id), {std::make_shared<deeplog::add_file_action>(action)});
 
     auto snapshot0 = deeplog::snapshot(main_id, 0, log);
