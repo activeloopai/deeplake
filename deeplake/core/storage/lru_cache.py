0dc051a8eb5880a0ccee62130d1d52487a2815aa
FayazRahman <fayazrahman4u@gmail.com>
2023-11-14 20:36:43 +0530

    test nworkers

--- deeplake/core/storage/lru_cache.py
+++ deeplake/core/storage/lru_cache.py
@@ -6,6 +6,7 @@ from deeplake.core.chunk.base_chunk import BaseChunk
 from typing import Any, Dict, Optional, Union
 
 from deeplake.core.storage.provider import StorageProvider
+import time
 
 
 def _get_nbytes(obj: Union[bytes, memoryview, DeepLakeMemoryObject]):
@@ -218,9 +219,13 @@ class LRUCache(StorageProvider):
     def load_items_from_next_storage(self, paths):
         """Pre-load items from next storage into cache"""
         if self.next_storage is not None:
-            for key, result in self.next_storage.get_items(paths):
-                if _get_nbytes(result) <= self.cache_size:
-                    self._insert_in_cache(key, result)
+            for nworkers in (4, 8, 16, 32, 64):
+                t1 = time.time()
+                for key, result in self.next_storage.get_items(paths, n=nworkers):
+                    if _get_nbytes(result) <= self.cache_size:
+                        self._insert_in_cache(key, result)
+                t2 = time.time()
+                print(f"{nworkers} took {t2 - t1} seconds.")
 
     def get_bytes(
         self,
