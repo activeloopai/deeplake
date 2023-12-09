22fbc519635779918d8625dac8d3f742afa18e56
khustup2 <sasun@activeloop.ai>
2023-12-09 19:29:23 +0400

    More tests.

--- deeplake/core/tests/test_query.py
+++ deeplake/core/tests/test_query.py
@@ -2,10 +2,11 @@ import deeplake
 from deeplake.tests.common import requires_libdeeplake
 from deeplake.core.dataset.deeplake_query_dataset import DeepLakeQueryDataset
 import pytest
+import numpy as np
 
 
 @requires_libdeeplake
-def test_single_sourcequery():
+def test_single_source_query():
     ds = deeplake.query('SELECT * FROM "hub://activeloop/mnist-train"')
     assert len(ds) == 60000
     assert len(ds.tensors) == 2
@@ -16,3 +17,26 @@ def test_single_sourcequery():
     assert len(ds) == 60000
     assert len(ds.tensors) == 1
     assert ds.images.meta.htype == "image"
+
+
+@requires_libdeeplake
+def test_multi_source_query():
+    with pytest.raises(RuntimeError):
+        ds = deeplake.query(
+            'SELECT * FROM "hub://activeloop/mnist-train" UNION (SELECT * FROM "hub://activeloop/imagenet-train")'
+        )
+
+    ds = deeplake.query(
+        'SELECT * FROM "hub://activeloop/mnist-train" UNION (SELECT images, labels FROM "hub://activeloop/imagenet-train")'
+    )
+    assert len(ds) == 1341166
+    assert len(ds.tensors) == 2
+    assert ds.images.meta.htype == "image"
+    assert ds.labels.meta.htype == "class_label"
+
+    ds = deeplake.query(
+        'SELECT * FROM (SELECT * FROM "hub://activeloop/mnist-train" UNION (SELECT images, labels FROM "hub://activeloop/imagenet-train")) WHERE labels == 0 ORDER BY RANDOM()'
+    )
+    assert len(ds) == 7223
+    d = ds.labels.numpy()
+    assert np.all(d == 0)
