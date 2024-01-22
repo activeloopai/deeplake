6ca7f5c2062bdcaf65d68eab9517c8bcae879c06
khustup2 <sasun@activeloop.ai>
2024-01-22 19:20:07 +0000

    Added indra storage provider.

---
+++ deeplake/core/storage/indra.py
@@ -0,0 +1,53 @@
+from deeplake.core.storage.provider import StorageProvider
+from indra.api import storage
+from typing import Optional, Union
+
+class IndraProvider(StorageProvider):
+    """Provider class for using Indra storage provider."""
+
+    def __init__(
+        self,
+        root: Union[str, storage.provider],
+        read_only: Optional[bool] = False,
+        **kwargs,
+    ):
+        if isinstance(root, str):
+            self.core = storage.create(root, read_only, **kwargs)
+        else:
+            self.core = root
+
+    def subdir(self, path: str, read_only: bool = False):
+        return IndraProvider(self.core.subdir(path, read_only))
+
+    def __setitem__(self, path, content):
+        self.check_readonly()
+        self.core.set(path, content)
+
+    def __getitem__(self, path):
+        return bytes(self.core.get(path))
+
+    def get_bytes(
+        self, path, start_byte: Optional[int] = None, end_byte: Optional[int] = None
+    ):
+        s = start_byte or 0
+        e = end_byte or 0
+        return bytes(self.core.get(path, s, e))
+
+    def get_object_size(self, path: str) -> int:
+        return self.core.length(path)
+
+    def __delitem__(self, path):
+        return self.core.remove(path)
+
+    def _all_keys(self):
+        return self.core.list("")
+
+    def __len__(self):
+        return len(self.core.list(""))
+
+    def __iter__(self):
+        return iter(self.core.list(""))
+
+    def clear(self, prefix=""):
+        self.core.clear(prefix)
+
