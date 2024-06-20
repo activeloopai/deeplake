f0223fbfac5f1a47f650ad763dc891516bea0452
Levon Ghukasyan <levon@activeloop.ai>
2024-06-20 13:50:34 +0000

    corrected dataset paths

--- deeplake/integrations/mmseg/mmseg_.py
+++ deeplake/integrations/mmseg/mmseg_.py
@@ -103,7 +103,7 @@ Below is the example of the deeplake mmseg configuration:
 ...     train=dict(
 ...         pipeline=train_pipeline,
 ...         # Credentials for authentication. See documendataion for deeplake.load() for details
-...         deeplake_path="hub://activeloop/coco-train",
+...         deeplake_path="hub://activeloop/semantic-seg-train",
 ...          deeplake_credentials={
 ...             "token": TOKEN,
 ...             "creds": None,
@@ -122,7 +122,7 @@ Below is the example of the deeplake mmseg configuration:
 ...     # Parameters as the same as for train
 ...     val=dict(
 ...         pipeline=test_pipeline,
-...         deeplake_path="hub://activeloop/coco-val",
+...         deeplake_path="hub://activeloop/semantic-seg-val",
 ...         deeplake_credentials={
 ...             "token": TOKEN,
 ...             "creds": None,
