7ce005ebb94469260f30df08b33ff78b4a9b53b4
dgaloop <david.gyulnazaryan@activeloop.dev>
2024-07-30 21:50:07 +0400

    Pass token in test

--- deeplake/core/tests/test_serialize.py
+++ deeplake/core/tests/test_serialize.py
@@ -50,9 +50,9 @@ def test_chunkids_serialize():
 
 @pytest.mark.slow
 @pytest.mark.flaky
-def test_get_large_header():
+def test_get_large_header(hub_cloud_dev_token):
     # headers for videos in this dataset are larger than the 100 bytes originally fetched
     # ideally this test would just be calling `serialize.get_header_from_url` directly, but that requires all the URL buliding up logic that lives in the chunk engine.
     # So calling a larger codepath that includes `get_header_from_url`
-    ds = deeplake.load("hub://activeloop/hmdb51-train")
+    ds = deeplake.load("hub://activeloop/hmdb51-train", token=hub_cloud_dev_token)
     assert ds.videos[0].shape == (75, 240, 560, 3)
