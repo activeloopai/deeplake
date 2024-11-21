c80089d11f7b35e30babf9ccc514332fd91f049a
Sasun Hambardzumyan <151129343+khustup2@users.noreply.github.com>
2024-11-21 16:57:44 +0400

    v4.0.3 Release (#2992)

--- python/deeplake/__init__.py
+++ python/deeplake/__init__.py
@@ -6,7 +6,7 @@ import numpy
 import deeplake
 from ._deeplake import *
 
-__version__ = "4.0.2"
+__version__ = "4.0.3"
 
 __all__ = [
     "__version__",
@@ -34,6 +34,7 @@ __all__ = [
     "ColumnAlreadyExistsError",
     "ColumnDoesNotExistError",
     "InvalidColumnValueError",
+    "PushError",
     "GcsStorageProviderFailed",
     "History",
     "InvalidType",
