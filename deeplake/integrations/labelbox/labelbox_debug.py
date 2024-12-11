d7a6ed20d1796667db3d385206f6614cafe986e3
Tigran Yesayan <tigran.yesayan@activeloop.ai>
2024-12-10 20:22:43 -0500

    cleanup labelbox debug classes

--- deeplake/integrations/labelbox/labelbox_debug.py
+++ deeplake/integrations/labelbox/labelbox_debug.py
@@ -1,6 +1,10 @@
-import json
+# classes in this file are needed to debug the ontology and labelbox projects when there's no access to the labelbox workspace
 from deeplake.integrations.labelbox.labelbox_converter import labelbox_video_converter
 
+# helper classes to support same accessors as labelbox instances for accessing the ontology
+class ontology_list_for_debug(list):
+    def __call__(self):
+        return self
 
 class ontology_for_debug:
     def __init__(self, data):
@@ -11,15 +15,19 @@ class ontology_for_debug:
                 setattr(
                     self,
                     key,
-                    [
+                    ontology_list_for_debug([
                         ontology_for_debug(item) if isinstance(item, dict) else item
                         for item in value
-                    ],
+                    ]),
                 )
             else:
                 setattr(self, key, value)
 
+    def __call__(self):
+        return self
 
+# Creates ontology object from the final exported labelbox project.
+# This function shall replace `client.get_ontology(ontology_id)` in the converter script.
 def ontology_for_debug_from_json(projects, project_id):
 
     global_objects = {}
@@ -36,6 +44,9 @@ def ontology_for_debug_from_json(projects, project_id):
         d = {
             "feature_schema_id": classification["feature_schema_id"],
             "name": classification["name"],
+            "scope": {
+                "value": "index"
+            },
             "options": [],
         }
 
@@ -43,7 +54,7 @@ def ontology_for_debug_from_json(projects, project_id):
 
         # handle the rest of the tools if needed
         if "radio_answer" in classification:
-            d["class_type"] = "radio"
+            d["class_type"] = {"value": "radio"}
             option = {
                 "name": classification["radio_answer"]["name"],
                 "value": classification["radio_answer"]["value"],
@@ -53,7 +64,7 @@ def ontology_for_debug_from_json(projects, project_id):
             }
 
         if "checkbox_answers" in classification:
-            d["class_type"] = "checkbox"
+            d["class_type"] = {"value": "checkbox"}
             option = {
                 "name": classification["checkbox_answers"]["name"],
                 "value": classification["checkbox_answers"]["value"],
@@ -78,7 +89,9 @@ def ontology_for_debug_from_json(projects, project_id):
         tools[tool["feature_schema_id"]] = {
             "feature_schema_id": tool["feature_schema_id"],
             "name": tool["name"],
-            "tool": annotation_kind_map[tool["annotation_kind"]],
+            "tool": {
+                "value": annotation_kind_map[tool["annotation_kind"]]
+            },
         }
 
         classifications = []
@@ -112,100 +125,3 @@ def ontology_for_debug_from_json(projects, project_id):
     return ontology_for_debug(
         {"classifications": final_classifications, "tools": final_tools}
     )
-
-
-class labelbox_video_converter_debug(labelbox_video_converter):
-    def __init__(
-        self,
-        ontology,
-        converters,
-        project,
-        project_id,
-        dataset,
-        context,
-        metadata_generators=None,
-        group_mapping=None,
-    ):
-        super().__init__(
-            ontology,
-            converters,
-            project,
-            project_id,
-            dataset,
-            context,
-            metadata_generators,
-            group_mapping,
-        )
-
-    def register_tool_(self, tool, context, fix_grouping_only):
-        if tool.tool not in self.labelbox_type_converters_:
-            print("skip tool:", tool.tool)
-            return
-
-        prefered_name = tool.name
-
-        if tool.tool in self.group_mapping:
-            prefered_name = self.group_mapping[tool.tool]
-        else:
-            prefered_name = tool.name
-
-        should_group_with_classifications = len(tool.classifications) > 0
-        if should_group_with_classifications:
-            tool_name = prefered_name + "/" + prefered_name
-            if fix_grouping_only:
-                if tool.tool in self.group_mapping:
-                    self.groupped_tensor_overrides[tool.tool] = tool_name
-        else:
-            tool_name = prefered_name
-
-        for classification in tool.classifications:
-            self.register_classification_(
-                classification,
-                context,
-                fix_grouping_only=fix_grouping_only,
-                parent=prefered_name,
-            )
-
-        if fix_grouping_only:
-            return
-
-        if tool.tool in self.groupped_tensor_overrides:
-            tool_name = self.groupped_tensor_overrides[tool.tool]
-
-        self.labelbox_type_converters_[tool.tool](
-            tool, self, tool_name, context, tool.tool in self.group_mapping
-        )
-
-    def register_classification_(self, tool, context, fix_grouping_only, parent=""):
-        if tool.class_type not in self.labelbox_type_converters_:
-            return
-
-        if tool.class_type in self.group_mapping:
-            prefered_name = (parent + "/" if parent else "") + self.group_mapping[
-                tool.class_type
-            ]
-        else:
-            prefered_name = (parent + "/" if parent else "") + tool.name
-
-        if fix_grouping_only:
-            return
-
-        self.labelbox_type_converters_[tool.class_type](
-            tool,
-            self,
-            prefered_name,
-            context,
-            tool.class_type in self.group_mapping,
-        )
-
-    def register_ontology_(self, ontology, context, fix_grouping_only=True):
-        for tool in ontology.tools():
-            self.register_tool_(tool, context, fix_grouping_only=fix_grouping_only)
-
-        for classification in ontology.classifications():
-            self.register_classification_(
-                classification, context, fix_grouping_only=fix_grouping_only
-            )
-
-        if fix_grouping_only:
-            self.register_ontology_(ontology, context, fix_grouping_only=False)
